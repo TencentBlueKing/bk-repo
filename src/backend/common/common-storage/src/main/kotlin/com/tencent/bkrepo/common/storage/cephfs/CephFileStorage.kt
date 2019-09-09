2ca0617db437d2fe4a05ec68840f680b4396466a
carrypan <carrypan@tencent.com>
2019-09-09 21:41:52 +0800

    完成common-storage的抽象层代码

---
+++ src/backend/common/common-storage/src/main/kotlin/com/tencent/bkrepo/common/storage/cephfs/CephFileStorage.kt
@@ -0,0 +1,29 @@
+package com.tencent.bkrepo.common.storage.cephfs
+
+import com.tencent.bkrepo.common.storage.AbstractFileStorage
+import com.tencent.bkrepo.common.storage.strategy.LocateStrategy
+import java.io.InputStream
+
+/**
+ * CephFS文件存储
+ *
+ * @author: carrypan
+ * @date: 2019-09-09
+ */
+class CephFileStorage(locateStrategy: LocateStrategy) : AbstractFileStorage(locateStrategy) {
+    override fun store(filename: String, path: String, inputStream: InputStream) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun delete(filename: String, path: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun load(filename: String, path: String): InputStream {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun exist(filename: String, path: String): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
