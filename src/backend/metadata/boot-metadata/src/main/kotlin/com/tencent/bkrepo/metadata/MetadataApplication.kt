107e248dc80df4671e728e9e4a292c8f2e590507
carrypan <carrypan@tencent.com>
2019-09-10 20:43:16 +0800

    metadata服务接口设计

---
+++ src/backend/metadata/boot-metadata/src/main/kotlin/com/tencent/bkrepo/metadata/MetadataApplication.kt
@@ -0,0 +1,17 @@
+package com.tencent.bkrepo.metadata
+
+import com.tencent.bkrepo.common.service.MicroService
+import org.springframework.boot.runApplication
+
+/**
+ * 元数据微服务启动类
+ *
+ * @author: carrypan
+ * @date: 2019-09-10
+ */
+@MicroService
+class MetadataApplication
+
+fun main(args: Array<String>) {
+    runApplication<MetadataApplication>(*args)
+}
