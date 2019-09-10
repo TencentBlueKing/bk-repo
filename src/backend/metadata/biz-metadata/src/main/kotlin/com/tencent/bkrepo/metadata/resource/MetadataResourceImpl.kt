bb22967685b30d3830afa91ee5c69a848b960770
carrypan <carrypan@tencent.com>
2019-09-10 20:45:07 +0800

    metadata服务接口设计

--- src/backend/metadata/biz-metadata/src/main/kotlin/com/tencent/bkrepo/metadata/resource/MetadataResourceImpl.kt
+++ src/backend/metadata/biz-metadata/src/main/kotlin/com/tencent/bkrepo/metadata/resource/MetadataResourceImpl.kt
@@ -12,28 +12,28 @@ import org.springframework.web.bind.annotation.RestController
  * @date: 2019-09-10
  */
 @RestController
-class MetadataResourceImpl: MetadataResource {
+class MetadataResourceImpl : MetadataResource {
     override fun detail(id: String): Response<Metadata> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun list(resourceId: String): Response<List<Metadata>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun create(repository: Metadata): Response<Metadata> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun create(repository: List<Metadata>): Response<List<Metadata>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun update(id: String, repository: Metadata): Response<Boolean> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun delete(id: String): Response<Boolean> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 }
