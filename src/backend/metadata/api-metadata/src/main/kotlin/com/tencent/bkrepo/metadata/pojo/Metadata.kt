c11de29325363d1675e7aa3b2e2b6c6dd1dc19dc
carrypan <carrypan@tencent.com>
2019-09-10 20:33:27 +0800

    metadata服务接口设计

---
+++ src/backend/metadata/api-metadata/src/main/kotlin/com/tencent/bkrepo/metadata/pojo/Metadata.kt
@@ -0,0 +1,22 @@
+package com.tencent.bkrepo.metadata.pojo
+
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+
+/**
+ * 元数据信息
+ *
+ * @author: carrypan
+ * @date: 2019-09-10
+ */
+@ApiModel("元数据信息")
+data class Metadata(
+        @ApiModelProperty("元数据id")
+        val id: String,
+        @ApiModelProperty("元数据key")
+        val key: String,
+        @ApiModelProperty("元数据value")
+        val value: Any,
+        @ApiModelProperty("元数据所属资源id")
+        val resourceId: String
+)
