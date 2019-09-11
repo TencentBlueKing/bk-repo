359bb99e626a95fc0ff623e12ec2ff1bcda63bce
carrypan <carrypan@tencent.com>
2019-09-11 18:16:32 +0800

    配置动态刷新测试

--- src/backend/demo/biz-demo/src/main/kotlin/com/tencent/bkrepo/demo/resource/UserResourceImpl.kt
+++ src/backend/demo/biz-demo/src/main/kotlin/com/tencent/bkrepo/demo/resource/UserResourceImpl.kt
@@ -2,14 +2,35 @@ package com.tencent.bkrepo.demo.resource
 
 import com.tencent.bkrepo.demo.api.UserResource
 import com.tencent.bkrepo.demo.pojo.User
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.cloud.context.config.annotation.RefreshScope
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UserResourceImpl : UserResource {
+
+    @Autowired
+    lateinit var foo: Foo
+
 
     override fun sayHello(name: String) = "Hello, $name!"
 
     override fun sayHello(user: User): String {
         return "Hello, ${user.name}, you are ${user.age} years old!"
     }
+
+    @GetMapping
+    fun config() = foo.bar
 }
+
+
+@Component
+@ConfigurationProperties("foo")
+class Foo {
+    lateinit var bar: String
+}
+
