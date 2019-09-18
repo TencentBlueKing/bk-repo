78289bda61b8311bcf46a8d6d2ea4636e7fd503e
carrypan <carrypan@tencent.com>
2019-09-18 12:06:30 +0800

    增加大文件上传测试

--- src/backend/demo/boot-demo/src/test/kotlin/com/tencent/bkrepo/demo/InnerCosFileStorageTest.kt
+++ src/backend/demo/boot-demo/src/test/kotlin/com/tencent/bkrepo/demo/InnerCosFileStorageTest.kt
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.core.io.ClassPathResource
+import java.io.File
 
 /**
  *
@@ -25,7 +26,7 @@ class InnerCosFileStorageTest @Autowired constructor(val innerCosFileStorage: In
 
     @Test
     @DisplayName("文件操作集成测试")
-    fun storeFileTest() {
+    fun integrationTest() {
         // 准备文件
         val testFile = ClassPathResource(testFileName).file
         val originalContent = toString(testFile.inputStream())
@@ -48,5 +49,32 @@ class InnerCosFileStorageTest @Autowired constructor(val innerCosFileStorage: In
         assertFalse(innerCosFileStorage.exist(fileSha256))
 
     }
+
+    @Test
+    @DisplayName("文件上传测试")
+    fun storeFileTest() {
+
+        // 准备文件
+        val testFile = File("/Users/carrypan/Downloads/Visual_Paradigm_CE_16_0_20190861_OSX_WithJRE.dmg")
+        if(testFile.exists()) {
+            val fileSize = testFile.length() / 1024F / 1024F
+            println("文件大小: $fileSize MB")
+
+            // 计算sha256
+            var start = System.currentTimeMillis()
+            val fileSha256 = FileDigestUtils.fileSha256(listOf(testFile.inputStream()))
+            println("文件SHA256: $fileSha256, 计算耗时: ${(System.currentTimeMillis() - start) / 1000F}秒")
+
+            // 上传
+            start = System.currentTimeMillis()
+            innerCosFileStorage.store(fileSha256, testFile.inputStream())
+            val uploadConsume = (System.currentTimeMillis() - start) / 1000F
+            println("上传耗时: ${uploadConsume}秒")
+
+            println("上传平均速度: ${fileSize / uploadConsume} MB/S")
+        }
+
+    }
+
 
 }
