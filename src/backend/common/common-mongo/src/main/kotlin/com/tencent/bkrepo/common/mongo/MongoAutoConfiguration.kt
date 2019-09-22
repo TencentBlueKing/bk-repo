4d2350a92b1ccce4b7e9f324753b1195664c73b9
carrypan <carrypan@tencent.com>
2019-09-22 17:57:41 +0800

    开启MongoAuditing功能

--- src/backend/common/common-mongo/src/main/kotlin/com/tencent/bkrepo/common/mongo/MongoAutoConfiguration.kt
+++ src/backend/common/common-mongo/src/main/kotlin/com/tencent/bkrepo/common/mongo/MongoAutoConfiguration.kt
@@ -2,17 +2,21 @@ package com.tencent.bkrepo.common.mongo
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.MongoDbFactory
 import org.springframework.data.mongodb.MongoTransactionManager
+import org.springframework.data.mongodb.config.EnableMongoAuditing
 
 /**
  * mongodb 4.0+开始支持事物，但springboot data mongo为了兼容老版本不出错，默认不开启事物
  * 同时，mongodb 事物控制只能用与多副本集的情况，否则会报错，因此添加了enabled开关
  */
-@ConditionalOnProperty(value = ["spring.data.mongodb.transaction.enabled"], matchIfMissing = true)
+@Configuration
+@EnableMongoAuditing
 class MongoAutoConfiguration {
 
     @Bean
+    @ConditionalOnProperty(value = ["spring.data.mongodb.transaction.enabled"], matchIfMissing = true)
     fun transactionManager(dbFactory: MongoDbFactory): MongoTransactionManager {
         return MongoTransactionManager(dbFactory)
     }
