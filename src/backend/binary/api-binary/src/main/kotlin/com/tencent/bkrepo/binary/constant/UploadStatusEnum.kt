05592dba78ce48abe7696e14cc5c243b1941985d
carrypan <carrypan@tencent.com>
2019-09-30 19:59:13 +0800

    binary模型类设计

---
+++ src/backend/binary/api-binary/src/main/kotlin/com/tencent/bkrepo/binary/constant/UploadStatusEnum.kt
@@ -0,0 +1,13 @@
+package com.tencent.bkrepo.binary.constant
+
+/**
+ * 上传状态
+ *
+ * @author: carrypan
+ * @date: 2019-09-30
+ */
+enum class UploadStatusEnum {
+    UPLOADING,
+    COMPLETED,
+    ABORTED
+}
