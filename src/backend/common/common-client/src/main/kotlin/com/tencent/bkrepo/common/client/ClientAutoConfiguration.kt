e97ef0d89f6516b9866085e3b7e5fb28828be981
carrypan <carrypan@tencent.com>
2019-11-01 17:00:15 +0800

    优化feign配置，统一记录请求和响应的日志到api-log

--- src/backend/common/common-client/src/main/kotlin/com/tencent/bkrepo/common/client/ClientAutoConfiguration.kt
+++ src/backend/common/common-client/src/main/kotlin/com/tencent/bkrepo/common/client/ClientAutoConfiguration.kt
@@ -1,7 +1,9 @@
 package com.tencent.bkrepo.common.client
 
+import com.tencent.bkrepo.common.client.log.Slf4jFeignLogger
 import feign.RequestInterceptor
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.cloud.openfeign.FeignLoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.context.request.RequestContextHolder
@@ -31,5 +33,11 @@ class ClientAutoConfiguration {
                 requestTemplate.header("Cookie", cookieBuilder.toString()) // 设置cookie信息
             }
         }
+    }
+
+    @Bean
+    fun feignLoggerFactory(): FeignLoggerFactory {
+        val feignLogger = Slf4jFeignLogger()
+        return FeignLoggerFactory { feignLogger }
     }
 }
