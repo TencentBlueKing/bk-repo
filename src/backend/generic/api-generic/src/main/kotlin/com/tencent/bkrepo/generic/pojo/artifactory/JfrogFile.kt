bea0731f2713583dfde285947fa8e3e682711629
necrohuang <necrohuang@tencent.com>
2019-11-01 11:05:53 +0800

    clean artifactory api code

--- src/backend/generic/api-generic/src/main/kotlin/com/tencent/bkrepo/generic/pojo/artifactory/JfrogFile.kt
+++
@@ -1,9 +0,0 @@
-package com.tencent.bkrepo.generic.pojo.artifactory
-
-data class JfrogFile(
-    val uri: String,
-    val size: Long,
-    val lastModified: String,
-    val folder: Boolean,
-    val sha1: String = ""
-)
--- src/backend/generic/api-generic/src/main/kotlin/com/tencent/bkrepo/generic/pojo/artifactory/JfrogFilesData.kt
+++
@@ -1,7 +0,0 @@
-package com.tencent.bkrepo.generic.pojo.artifactory
-
-data class JfrogFilesData(
-    val uri: String,
-    val created: String,
-    val files: List<JfrogFile>
-)
