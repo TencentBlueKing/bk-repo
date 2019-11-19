09829cd0fd5c8e96ba7cb1f1c3ec88fe4010fe54
carrypan <carrypan@tencent.com>
2019-11-19 16:02:28 +0800

    metadata user接口使用artifact统一方式

--- src/backend/repository/api-repository/src/main/kotlin/com/tencent/bkrepo/repository/api/UserMetadataResource.kt
+++ src/backend/repository/api-repository/src/main/kotlin/com/tencent/bkrepo/repository/api/UserMetadataResource.kt
@@ -1,9 +1,11 @@
 package com.tencent.bkrepo.repository.api
 
-import com.tencent.bkrepo.common.api.annotation.WildcardParam
 import com.tencent.bkrepo.common.api.constant.AUTH_HEADER_USER_ID
 import com.tencent.bkrepo.common.api.constant.AUTH_HEADER_USER_ID_DEFAULT_VALUE
 import com.tencent.bkrepo.common.api.pojo.Response
+import com.tencent.bkrepo.common.artifact.locator.ArtifactLocation
+import com.tencent.bkrepo.common.artifact.locator.ArtifactLocator
+import com.tencent.bkrepo.common.artifact.locator.ArtifactLocator.Companion.ARTIFACT_LOCATE_URI
 import com.tencent.bkrepo.repository.constant.SERVICE_NAME
 import com.tencent.bkrepo.repository.pojo.metadata.UserMetadataDeleteRequest
 import com.tencent.bkrepo.repository.pojo.metadata.UserMetadataSaveRequest
@@ -13,7 +15,6 @@ import io.swagger.annotations.ApiParam
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
@@ -30,56 +31,35 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/user/metadata")
 interface UserMetadataResource {
     @ApiOperation("查询元数据列表")
-    @GetMapping("/{projectId}/{repoName}/**")
+    @GetMapping(ARTIFACT_LOCATE_URI)
     fun query(
         @ApiParam(value = "用户id", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @RequestHeader(AUTH_HEADER_USER_ID)
         userId: String,
-        @ApiParam(value = "所属项目", required = true)
-        @PathVariable
-        projectId: String,
-        @ApiParam(value = "仓库名称", required = true)
-        @PathVariable
-        repoName: String,
-        @ApiParam(hidden = true)
-        @WildcardParam
-        fullPath: String
+        @ArtifactLocator
+        artifactLocation: ArtifactLocation
     ): Response<Map<String, String>>
 
     @ApiOperation("创建/更新元数据列表")
-    @PostMapping("/{projectId}/{repoName}/**")
+    @PostMapping(ARTIFACT_LOCATE_URI)
     fun save(
         @ApiParam(value = "用户id", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @RequestHeader(AUTH_HEADER_USER_ID)
         userId: String,
-        @ApiParam(value = "所属项目", required = true)
-        @PathVariable
-        projectId: String,
-        @ApiParam(value = "仓库名称", required = true)
-        @PathVariable
-        repoName: String,
-        @ApiParam(hidden = true)
-        @WildcardParam
-        fullPath: String,
+        @ArtifactLocator
+        artifactLocation: ArtifactLocation,
         @RequestBody
         metadataSaveRequest: UserMetadataSaveRequest
     ): Response<Void>
 
     @ApiOperation("删除元数据")
-    @DeleteMapping("/{projectId}/{repoName}/**")
+    @DeleteMapping(ARTIFACT_LOCATE_URI)
     fun delete(
         @ApiParam(value = "用户id", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
         @RequestHeader(AUTH_HEADER_USER_ID)
         userId: String,
-        @ApiParam(value = "所属项目", required = true)
-        @PathVariable
-        projectId: String,
-        @ApiParam(value = "仓库名称", required = true)
-        @PathVariable
-        repoName: String,
-        @ApiParam(hidden = true)
-        @WildcardParam
-        fullPath: String,
+        @ArtifactLocator
+        artifactLocation: ArtifactLocation,
         @RequestBody
         metadataDeleteRequest: UserMetadataDeleteRequest
     ): Response<Void>
