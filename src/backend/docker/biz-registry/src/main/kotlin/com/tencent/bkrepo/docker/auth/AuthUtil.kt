a74c43484702cda8407e9914700bb367be8634fc
owen <owen@tencent.com>
2019-12-03 10:35:59 +0800

    commit auth util

--- src/backend/docker/biz-registry/src/main/kotlin/com/tencent/bkrepo/docker/auth/AuthUtil.kt
+++ src/backend/docker/biz-registry/src/main/kotlin/com/tencent/bkrepo/docker/auth/AuthUtil.kt
@@ -6,6 +6,7 @@ import com.tencent.bkrepo.docker.util.JwtUtil
 import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import java.text.SimpleDateFormat
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -17,6 +18,7 @@ class AuthUtil {
         try {
             val user = DockerClientAuthHandler.extractBasicAuth(request)
             val token = JwtUtil.sign(user.username , user.password)
+//            val issuedAt = SimpleDateFormat("yyyy/MM/dd")
             val issuedAt = "2009-11-10T23:00:00Z"
             val tokenUrl = String.format("{\"token\": \"%s\", \"expires_in\": 3600,\"issued_at\": \"%s\"}", token, issuedAt)
             return ResponseEntity.ok().header("Content-Type", "application/json").header("Docker-Distribution-Api-Version", "registry/2.0").body(tokenUrl)
