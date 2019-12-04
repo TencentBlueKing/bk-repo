d79ecbe93b6d9f762a11154ff2c2d32c308e543e
carrypan <carrypan@tencent.com>
2019-12-04 14:57:47 +0800

    移除download操作的事务注解

--- src/backend/generic/biz-generic/src/main/kotlin/com/tencent/bkrepo/generic/service/DownloadService.kt
+++ src/backend/generic/biz-generic/src/main/kotlin/com/tencent/bkrepo/generic/service/DownloadService.kt
@@ -7,7 +7,6 @@ import com.tencent.bkrepo.common.artifact.repository.RepositoryHolder
 import com.tencent.bkrepo.common.artifact.repository.context.ArtifactDownloadContext
 import com.tencent.bkrepo.generic.artifact.GenericArtifactInfo
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 /**
  * 通用文件下载服务类
@@ -19,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional
 class DownloadService {
 
     @Permission(ResourceType.REPO, PermissionAction.READ)
-    @Transactional(rollbackFor = [Throwable::class])
     fun download(artifactInfo: GenericArtifactInfo) {
         val context = ArtifactDownloadContext()
         val repository = RepositoryHolder.getRepository(context.repositoryInfo.category)
