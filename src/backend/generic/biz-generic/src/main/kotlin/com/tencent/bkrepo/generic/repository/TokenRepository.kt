37a236b4beebcc91c6881b4254475e143e4d6c28
owen <owen@tencent.com>
2019-12-10 18:05:19 +0800

    delete generic unversion files

--- src/backend/generic/biz-generic/src/main/kotlin/com/tencent/bkrepo/generic/repository/TokenRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.tencent.bkrepo.generic.repository
-
-import com.tencent.bkrepo.generic.model.TDownloadTokenRecord
-import org.springframework.data.mongodb.repository.MongoRepository
-
-interface DownloadTokenRepository : MongoRepository<TDownloadTokenRecord, String> {
-    fun findOneByToken(name: String): TDownloadTokenRecord?
-}
