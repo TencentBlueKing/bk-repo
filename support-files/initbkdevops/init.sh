3166ea1d04531f0b261e72b106d8c73fe75fd587
carrypan <carrypan@tencent.com>
2019-12-25 16:42:18 +0800

    更新init脚本

--- support-files/initbkdevops/init.sh
+++ support-files/initbkdevops/init.sh
@@ -5,9 +5,9 @@ user=$2
 authorization=$(echo $3 | base64)
 
 # 创建项目
-curl -X POST http://dev1.bkrepo.oa.com/api/repository/api/project -H "Authorization:BKREPO $authorization" -H "X-BKREPO-UID: $user" -H "Content-Type: application/json" -d '{"name": "'$project'", "displayName": "'$project'", "description": ""}'
+curl -X POST http://dev1.bkrepo.oa.com/api/repository/api/project -H "Authorization:Platform $authorization" -H "X-BKREPO-UID: $user" -H "Content-Type: application/json" -d '{"name": "'$project'", "displayName": "'$project'", "description": ""}'
 
 # 创建仓库
-curl -X POST http://dev1.bkrepo.oa.com/api/repository/api/repo -H "Authorization:BKREPO $authorization" -H "X-BKREPO-UID: $user" -H "Content-Type: application/json" -d '{"projectId": "'$project'", "name": "pipeline", "type":"GENERIC", "category":"LOCAL", "public":false, "configuration": {"type":"local"}, "description":""}'
-curl -X POST http://dev1.bkrepo.oa.com/api/repository/api/repo -H "Authorization:BKREPO $authorization" -H "X-BKREPO-UID: $user" -H "Content-Type: application/json" -d '{"projectId": "'$project'", "name": "custom", "type":"GENERIC", "category":"LOCAL", "public":false, "configuration": {"type":"local"}, "description":""}'
-curl -X POST http://dev1.bkrepo.oa.com/api/repository/api/repo -H "Authorization:BKREPO $authorization" -H "X-BKREPO-UID: $user" -H "Content-Type: application/json" -d '{"projectId": "'$project'", "name": "report", "type":"GENERIC", "category":"LOCAL", "public":false, "configuration": {"type":"local"}, "description":""}'
+curl -X POST http://dev1.bkrepo.oa.com/api/repository/api/repo -H "Authorization:Platform $authorization" -H "X-BKREPO-UID: $user" -H "Content-Type: application/json" -d '{"projectId": "'$project'", "name": "pipeline", "type":"GENERIC", "category":"LOCAL", "public":false, "configuration": {"type":"local"}, "description":""}'
+curl -X POST http://dev1.bkrepo.oa.com/api/repository/api/repo -H "Authorization:Platform $authorization" -H "X-BKREPO-UID: $user" -H "Content-Type: application/json" -d '{"projectId": "'$project'", "name": "custom", "type":"GENERIC", "category":"LOCAL", "public":false, "configuration": {"type":"local"}, "description":""}'
+curl -X POST http://dev1.bkrepo.oa.com/api/repository/api/repo -H "Authorization:Platform $authorization" -H "X-BKREPO-UID: $user" -H "Content-Type: application/json" -d '{"projectId": "'$project'", "name": "report", "type":"GENERIC", "category":"LOCAL", "public":false, "configuration": {"type":"local"}, "description":""}'
