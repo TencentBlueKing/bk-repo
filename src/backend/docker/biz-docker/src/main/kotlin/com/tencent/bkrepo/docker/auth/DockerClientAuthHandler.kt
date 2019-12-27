e3d2ae9329e98ca6fb3d18a65a0053f2a5548c6a
owen <owen@tencent.com>
2019-12-27 11:25:18 +0800

    add debug message

--- src/backend/docker/biz-docker/src/main/kotlin/com/tencent/bkrepo/docker/auth/DockerClientAuthHandler.kt
+++ src/backend/docker/biz-docker/src/main/kotlin/com/tencent/bkrepo/docker/auth/DockerClientAuthHandler.kt
@@ -72,7 +72,7 @@ class DockerClientAuthHandler(val userResource: ServiceUserResource) : ClientAut
         response.status = SC_UNAUTHORIZED
         response.setHeader("Docker-Distribution-Api-Version", "registry/2.0")
         val registryService = "bkrepo"
-        val scopeStr = "repository:samalba/my-app:pull,push"
+        val scopeStr = "repository:bkrepo/docker-local/tb:push,pull"
         response.setHeader(BASIC_AUTH_RESPONSE_HEADER, String.format("Bearer realm=\"%s\",service=\"%s\",scope=\"%s\"", authUrl, registryService,scopeStr) )
         response.contentType = MediaType.APPLICATION_JSON
         logger.info("aaaaaaaaaaaaaaaaaaaaaa ,{}",String.format("{\"errors\":[{\"code\":\"%s\",\"message\":\"%s\",\"detail\":\"%s\"}]}", "UNAUTHORIZED", "authentication required", "BAD_CREDENTIAL") )
