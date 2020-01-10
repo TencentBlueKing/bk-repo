d11338553d92d6d575bf18172ef1f7c026d8553e
owen <owen@tencent.com>
2020-01-10 12:01:26 +0800

    commit jwt

--- src/backend/docker/biz-docker/src/main/kotlin/com/tencent/bkrepo/docker/auth/AuthUtil.kt
+++ src/backend/docker/biz-docker/src/main/kotlin/com/tencent/bkrepo/docker/auth/AuthUtil.kt
@@ -15,7 +15,7 @@ class AuthUtil {
     fun authUser(request: HttpServletRequest, response: HttpServletResponse): ResponseEntity<Any> {
         try {
             val user = DockerClientAuthHandler.extractBasicAuth(request)
-            val token = JwtUtil.sign(user.username, user.password)
+            val token = JwtUtil.sign(user.username)
             val issuedAt = TimeUtils.getGMTTime()
             val tokenUrl = String.format("{\"token\": \"%s\", \"access_token\": \"%s\",\"issued_at\": \"%s\"}", token, token, issuedAt)
             return ResponseEntity.ok().header("Content-Type", "application/json").header("Docker-Distribution-Api-Version", "registry/2.0").body(tokenUrl)
