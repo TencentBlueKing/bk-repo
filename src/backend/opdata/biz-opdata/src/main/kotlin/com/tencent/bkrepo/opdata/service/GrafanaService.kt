6605f069124652b7ee6b7e51bab533ac8b252748
owen <owen@tencent.com>
2020-01-10 14:55:49 +0800

    remove project with no nodes

--- src/backend/opdata/biz-opdata/src/main/kotlin/com/tencent/bkrepo/opdata/service/GrafanaService.kt
+++ src/backend/opdata/biz-opdata/src/main/kotlin/com/tencent/bkrepo/opdata/service/GrafanaService.kt
@@ -87,7 +87,9 @@ class GrafanaService @Autowired constructor(
             val displaySize = totalSize / (1024 * 1024 * 1024)
             val data = listOf<Long>(displaySize, System.currentTimeMillis())
             val element = listOf<List<Long>>(data)
-            result.add(NodeResult(projectId, element))
+            if (displaySize != 0L) {
+                result.add(NodeResult(projectId, element))
+            }
         }
         return result
     }
