c6521810d9b450d3dcbac96d1a0d401a4bd16b93
weaving <>
2020-03-31 16:06:03 +0800

    message: isExists 改查metadata

--- src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/artifact/repository/HelmLocalRepository.kt
+++ src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/artifact/repository/HelmLocalRepository.kt
@@ -11,13 +11,23 @@ import com.tencent.bkrepo.common.artifact.repository.context.ArtifactSearchConte
 import com.tencent.bkrepo.common.artifact.repository.context.ArtifactUploadContext
 import com.tencent.bkrepo.common.artifact.repository.local.LocalRepository
 import com.tencent.bkrepo.common.artifact.util.HttpResponseUtils
+import com.tencent.bkrepo.common.query.model.PageLimit
+import com.tencent.bkrepo.common.query.model.QueryModel
+import com.tencent.bkrepo.common.query.model.Rule
+import com.tencent.bkrepo.common.query.model.Sort
 import com.tencent.bkrepo.common.service.util.HttpContextHolder
 import com.tencent.bkrepo.common.storage.util.FileDigestUtils
 import com.tencent.bkrepo.helm.constants.CHART_NOT_FOUND
+import com.tencent.bkrepo.helm.constants.CHART_VERSION_NOT_FOUND
+import com.tencent.bkrepo.helm.constants.EMPTY_CHART_OR_VERSION
+import com.tencent.bkrepo.helm.constants.EMPTY_NAME_OR_VERSION
+import com.tencent.bkrepo.helm.constants.ENTRIES
+import com.tencent.bkrepo.helm.constants.ERROR_NOT_FOUND
 import com.tencent.bkrepo.helm.constants.FULL_PATH
 import com.tencent.bkrepo.helm.constants.INDEX_CACHE_YAML
 import com.tencent.bkrepo.helm.constants.INDEX_YAML
 import com.tencent.bkrepo.helm.constants.INIT_STR
+import com.tencent.bkrepo.helm.constants.NO_CHART_NAME_FOUND
 import com.tencent.bkrepo.helm.utils.JsonUtil
 import com.tencent.bkrepo.repository.pojo.node.service.NodeCreateRequest
 import com.tencent.bkrepo.repository.pojo.node.service.NodeDeleteRequest
@@ -178,16 +188,47 @@ class HelmLocalRepository : LocalRepository() {
         val artifactInfo = context.artifactInfo
         val response = HttpContextHolder.getResponse()
         with(artifactInfo) {
-            val nodeDetail = nodeResource.detail(projectId, repoName, artifactUri).data
-            if (nodeDetail == null) {
-                response.status = HttpStatus.SC_NOT_FOUND
-            } else {
-                response.status = HttpStatus.SC_OK
+            val projectId = Rule.QueryRule("projectId", projectId)
+            val repoName = Rule.QueryRule("repoName", repoName)
+            val urlList = artifactUri.removePrefix("/").split("/").filter { it.isNotBlank() }
+            val rule:Rule? = when (urlList.size) {
+                // query with name
+                1 -> {
+                    val name = Rule.QueryRule("name", urlList[0])
+                    Rule.NestedRule(
+                        mutableListOf(repoName, projectId, name),
+                        Rule.NestedRule.RelationType.AND
+                    )
+                }
+                // query with name and version
+                2 -> {
+                    val name = Rule.QueryRule("name", urlList[0])
+                    val version = Rule.QueryRule("version", urlList[1])
+                    Rule.NestedRule(
+                        mutableListOf(repoName, projectId, name, version),
+                        Rule.NestedRule.RelationType.AND
+                    )
+                }
+                else -> {
+                    null
+                }
             }
+            rule?.let {
+                val queryModel = QueryModel(
+
+                page = PageLimit(0, 5),
+                sort = Sort(listOf("name"), Sort.Direction.ASC),
+                select = mutableListOf("projectId", "repoName", "fullPath", "metadata"),
+                rule = it
+                )if
+                val nodeList: List<Map<String, Any>>? = nodeResource.query(queryModel).data?.records
+                (nodeList.isNullOrEmpty()) HttpStatus.SC_NOT_FOUND
+            }
+
         }
     }
 
-    companion object {
+     companion object {
         val logger: Logger = LoggerFactory.getLogger(HelmLocalRepository::class.java)
     }
 }
