a75fe28b9282a7ce1bed3bd614478db4dc419e9b
charis <ccxwhy_cc@163.com>
2020-03-31 16:30:43 +0800

    helm index.yaml fresh增加日志

---
+++ src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/exception/HelmExceptionHandler.kt
@@ -0,0 +1,44 @@
+package com.tencent.bkrepo.helm.exception
+
+import com.tencent.bkrepo.common.api.constant.ANONYMOUS_USER
+import com.tencent.bkrepo.common.api.constant.USER_KEY
+import com.tencent.bkrepo.common.api.util.JsonUtils
+import com.tencent.bkrepo.common.artifact.exception.ArtifactExceptionHandler
+import com.tencent.bkrepo.common.service.util.HttpContextHolder
+import com.tencent.bkrepo.common.service.util.ResponseBuilder
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+/**
+ * 统一异常处理
+ */
+@RestControllerAdvice
+class HelmExceptionHandler {
+    @ExceptionHandler(HelmIndexFreshFailException::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handlerNpmClientAuthException(exception: HelmIndexFreshFailException) {
+        response(HttpStatus.INTERNAL_SERVER_ERROR, exception)
+    }
+
+    private fun response(status: HttpStatus, exception: HelmException) {
+        logHelmException(exception)
+        val responseObject = ResponseBuilder.fail(status.value(), exception.message)
+        val responseString = JsonUtils.objectMapper.writeValueAsString(responseObject)
+        val response = HttpContextHolder.getResponse()
+        response.contentType = "application/json; charset=utf-8"
+        response.writer.println(responseString)
+    }
+
+    private fun logHelmException(exception: HelmException) {
+        val userId = HttpContextHolder.getRequest().getAttribute(USER_KEY) ?: ANONYMOUS_USER
+        val uri = HttpContextHolder.getRequest().requestURI
+        logger.warn("User[$userId] access resource[$uri] failed[${exception.javaClass.simpleName}]: ${exception.message}")
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(ArtifactExceptionHandler::class.java)
+    }
+}
--- src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/service/ChartManipulationService.kt
+++ src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/service/ChartManipulationService.kt
@@ -14,17 +14,20 @@ import com.tencent.bkrepo.common.storage.util.FileDigestUtils
 import com.tencent.bkrepo.helm.artifact.HelmArtifactInfo
 import com.tencent.bkrepo.helm.constants.FULL_PATH
 import com.tencent.bkrepo.helm.constants.INDEX_CACHE_YAML
-import com.tencent.bkrepo.helm.constants.UPLOAD_ERROR_MAP
 import com.tencent.bkrepo.helm.constants.UPLOAD_SUCCESS_MAP
+import com.tencent.bkrepo.helm.exception.HelmIndexFreshFailException
 import com.tencent.bkrepo.helm.pojo.ChartEntity
 import com.tencent.bkrepo.helm.pojo.IndexEntity
 import com.tencent.bkrepo.helm.utils.PackDecompressorUtils
 import com.tencent.bkrepo.helm.utils.YamlUtils
 import com.tencent.bkrepo.repository.util.NodeUtils.FILE_SEPARATOR
 import org.apache.commons.fileupload.util.Streams
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.io.File
+import java.io.FileNotFoundException
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -44,7 +47,7 @@ class ChartManipulationService {
             val removeContext = ArtifactRemoveContext()
             removeContext.contextAttributes[FULL_PATH] = getFileFullPath(artifactFileMap)
             repository.remove(removeContext)
-            return UPLOAD_ERROR_MAP
+            throw HelmIndexFreshFailException("fresh $INDEX_CACHE_YAML file failed")
         }
         return UPLOAD_SUCCESS_MAP
     }
@@ -53,6 +56,7 @@ class ChartManipulationService {
         val inputStream = (artifactFileMap["chart"] as MultipartArtifactFile).getInputStream()
         val tempDir = System.getProperty("java.io.tmpdir")
         PackDecompressorUtils.unTarGZ(inputStream, tempDir)
+        logger.info("file " + getFileFullPath(artifactFileMap) + " unTar success!")
         val file = getUnTgzFile(artifactFileMap, tempDir)
         val chartEntity = YamlUtils.getObject<ChartEntity>(file)
         val indexEntity = getIndexYamlFile(chartEntity, artifactFileMap)
@@ -66,6 +70,7 @@ class ChartManipulationService {
         uploadContext.contextAttributes[FULL_PATH] = "$FILE_SEPARATOR$INDEX_CACHE_YAML"
         val uploadRepository = RepositoryHolder.getRepository(uploadContext.repositoryInfo.category)
         uploadRepository.upload(uploadContext)
+        logger.info("fresh $INDEX_CACHE_YAML success!")
     }
 
     private fun getIndexYamlFile(chartEntity: ChartEntity, artifactFileMap: ArtifactFileMap): IndexEntity {
@@ -73,11 +78,9 @@ class ChartManipulationService {
         val repository = RepositoryHolder.getRepository(context.repositoryInfo.category)
         context.contextAttributes[FULL_PATH] = "$FILE_SEPARATOR$INDEX_CACHE_YAML"
         val indexFile = repository.search(context) as File
+        logger.info("search $INDEX_CACHE_YAML success!")
         val indexEntity = YamlUtils.getObject<IndexEntity>(indexFile)
-        chartEntity.urls = listOf("charts${getFileFullPath(artifactFileMap)}")
-        val format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS+08:00")
-        chartEntity.created = LocalDateTime.now().format(format)
-        chartEntity.digest = artifactFileMap["chart"]?.getInputStream()?.let { FileDigestUtils.fileSha256(it) }
+        updateChartEntity(chartEntity, artifactFileMap)
         val chartName = chartEntity.name!!
         val isFirstChart = indexEntity.entries!!.containsKey(chartName)
         indexEntity.entries?.let {
@@ -90,13 +93,32 @@ class ChartManipulationService {
         return indexEntity
     }
 
+    private fun updateChartEntity(
+        chartEntity: ChartEntity,
+        artifactFileMap: ArtifactFileMap
+    ) {
+        chartEntity.urls = listOf("charts${getFileFullPath(artifactFileMap)}")
+        val format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS+08:00")
+        chartEntity.created = LocalDateTime.now().format(format)
+        chartEntity.digest = artifactFileMap["chart"]?.getInputStream()?.let { FileDigestUtils.fileSha256(it) }
+    }
+
     private fun getUnTgzFile(artifactFileMap: ArtifactFileMap, tempDir: String): File {
-        val name = getFileFullPath(artifactFileMap).trimStart('/').substringBeforeLast('-')
-        return File("$tempDir$name${FILE_SEPARATOR}Chart.yaml")
+        try {
+            val name = getFileFullPath(artifactFileMap).trimStart('/').substringBeforeLast('-')
+            return File("$tempDir$name${FILE_SEPARATOR}Chart.yaml")
+        } catch (e: FileNotFoundException) {
+            logger.error(e.message)
+            throw HelmIndexFreshFailException("file Chart.yaml nou found")
+        }
     }
 
     private fun getFileFullPath(artifactFileMap: ArtifactFileMap): String {
         val fileName = (artifactFileMap["chart"] as MultipartArtifactFile).getOriginalFilename()
         return FILE_SEPARATOR + fileName.substringAfterLast('/')
+    }
+
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(ChartManipulationService::class.java)
     }
 }
