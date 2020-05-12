3c593befb564cae7a50ac0f60046533cbf4f021b
carrypan <carrypan@tencent.com>
2020-05-12 15:39:28 +0800

    feature: 调整metric描述名称 #7

--- src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/metrics/ArtifactMetrics.kt
+++ src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/metrics/ArtifactMetrics.kt
@@ -18,19 +18,19 @@ class ArtifactMetrics: MeterBinder {
 
     override fun bindTo(meterRegistry: MeterRegistry) {
         Gauge.builder(ARTIFACT_UPLOADING_COUNT, uploadCount, { it.get().toDouble() })
-            .description("当前正在上传的构件数量")
+            .description("构件实时上传数")
             .register(meterRegistry)
 
         Gauge.builder(ARTIFACT_DOWNLOADING_COUNT, downloadCount, { it.get().toDouble() })
-            .description("当前正在下载的构件数量")
+            .description("构件实时下载数")
             .register(meterRegistry)
 
         Gauge.builder(ASYNC_TASK_ACTIVE_COUNT, taskAsyncExecutor.threadPoolExecutor, { it.activeCount.toDouble() })
-            .description("当前异步任务进行数量")
+            .description("异步任务实时数")
             .register(meterRegistry)
 
         Gauge.builder(ASYNC_TASK_QUEUE_SIZE, taskAsyncExecutor.threadPoolExecutor, { it.queue.size.toDouble() })
-            .description("当前异步任务队列大小")
+            .description("异步任务队列大小")
             .register(meterRegistry)
     }
 }
