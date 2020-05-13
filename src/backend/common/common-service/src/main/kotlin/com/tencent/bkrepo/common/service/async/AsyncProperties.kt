1b7ab23c11b6fafb2eb80628d43956561a5df1ed
charis <ccxwhy_cc@163.com>
2020-05-13 17:56:43 +0800

    fix: npm数据迁移的线程池可配置 #32

--- src/backend/common/common-service/src/main/kotlin/com/tencent/bkrepo/common/service/async/AsyncProperties.kt
+++ src/backend/common/common-service/src/main/kotlin/com/tencent/bkrepo/common/service/async/AsyncProperties.kt
@@ -4,9 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("async")
 data class AsyncProperties (
-    val corePoolSize: Int = 100,
-    val maxPoolSize: Int = 1000,
-    val queueCapacity: Int = 1000,
-    val keepAliveSeconds: Int = 60,
-    val threadNamePrefix: String = "async-executor-"
+    var corePoolSize: Int = 100,
+    var maxPoolSize: Int = 1000,
+    var queueCapacity: Int = 1000,
+    var keepAliveSeconds: Int = 60,
+    var threadNamePrefix: String = "async-executor-"
 )
