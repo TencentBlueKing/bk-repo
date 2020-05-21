881c66b6107ee78d80427c102ccb8407b27270f4
owenlxu <owenlxu@tencent.com>
2020-05-21 15:57:26 +0800

    refactor: modify default timeout settings #42

--- src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/service/ScheduleService.kt
+++ src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/service/ScheduleService.kt
@@ -30,7 +30,7 @@ class ScheduleService @Autowired constructor(
             .usingJobData(TASK_ID_KEY, task.id)
             .build()
         val trigger = createTrigger(task)
-        scheduler.scheduleJob(jobDetail, trigger)
+        //scheduler.scheduleJob(jobDetail, trigger)
         logger.info("Create full replication job success!")
     }
 
