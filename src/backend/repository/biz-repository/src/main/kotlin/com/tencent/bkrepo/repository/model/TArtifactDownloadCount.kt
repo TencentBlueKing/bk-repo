9b8c04fe9aac2e86396a02d31da443f998fb7900
charis <ccxwhy_cc@163.com>
2020-05-25 18:02:10 +0800

    feature: 新增构建下载量统计功能 #50

---
+++ src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/model/TArtifactDownloadCount.kt
@@ -0,0 +1,27 @@
+package com.tencent.bkrepo.repository.model
+
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.index.CompoundIndexes
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
+
+@Document("artifact_download_count")
+@CompoundIndexes(
+    CompoundIndex(
+        name = "projectId_repoName_artifact_download_count_idx",
+        def = "{'projectId': 1, 'repoName': 1, 'artifact': 1, 'version': 1, 'currentDate': 1}",
+        background = true,
+        unique = true
+    )
+)
+data class TArtifactDownloadCount (
+    var id: String? = null,
+    var projectId: String,
+    var repoName: String,
+    // artifact name
+    var artifact: String,
+    var version: String?,
+    // download count
+    var count: Int,
+    var currentDate: LocalDate
+)
