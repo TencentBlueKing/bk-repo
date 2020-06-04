447fcad21343456499eecfadcb786fd61e8f674a
carrypan <carrypan@tencent.com>
2020-06-04 15:10:19 +0800

    bugfix: 下载统计修改为异步方式 #65

--- src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/repository/local/LocalRepository.kt
+++ src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/repository/local/LocalRepository.kt
@@ -19,8 +19,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
-import org.springframework.scheduling.annotation.Async
+import java.util.concurrent.Executor
 import java.util.regex.Pattern
+import javax.annotation.Resource
 
 /**
  *
@@ -41,6 +42,9 @@ abstract class LocalRepository : AbstractArtifactRepository() {
     @Autowired
     lateinit var downloadStatisticsResource: DownloadStatisticsResource
 
+    @Resource
+    private lateinit var taskAsyncExecutor: Executor
+
     override fun onUpload(context: ArtifactUploadContext) {
         val nodeCreateRequest = getNodeCreateRequest(context)
         storageService.store(nodeCreateRequest.sha256!!, context.getArtifactFile(), context.storageCredentials)
@@ -59,17 +63,18 @@ abstract class LocalRepository : AbstractArtifactRepository() {
         }
     }
 
-    @Async
     open fun countDownloads(context: ArtifactDownloadContext) {
-        val artifactInfo = context.artifactInfo
-        downloadStatisticsResource.add(
-            DownloadStatisticsCreateRequest(
-                artifactInfo.projectId,
-                artifactInfo.repoName,
-                artifactInfo.artifact,
-                artifactInfo.version
+        taskAsyncExecutor.execute {
+            val artifactInfo = context.artifactInfo
+            downloadStatisticsResource.add(
+                DownloadStatisticsCreateRequest(
+                    artifactInfo.projectId,
+                    artifactInfo.repoName,
+                    artifactInfo.artifact,
+                    artifactInfo.version
+                )
             )
-        )
+        }
     }
 
     /**
