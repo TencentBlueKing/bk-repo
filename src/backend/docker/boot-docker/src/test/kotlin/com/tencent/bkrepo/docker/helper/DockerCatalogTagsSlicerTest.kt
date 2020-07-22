7a611eb8958227aa430643e942e667ee28bcc975
owenlxu <owenlxu@tencent.com>
2020-07-22 19:57:42 +0800

    refactor: refactor docker constants #123

--- src/backend/docker/boot-docker/src/test/kotlin/com/tencent/bkrepo/docker/helper/DockerCatalogTagsSlicerTest.kt
+++ src/backend/docker/boot-docker/src/test/kotlin/com/tencent/bkrepo/docker/helper/DockerCatalogTagsSlicerTest.kt
@@ -1,4 +1,32 @@
 package com.tencent.bkrepo.docker.helper
 
+import com.tencent.bkrepo.docker.helpers.DockerCatalogTagsSlicer
+import com.tencent.bkrepo.docker.helpers.DockerPaginationElementsHolder
+import com.tencent.bkrepo.docker.response.CatalogResponse
+import org.apache.commons.lang.StringUtils
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
 class DockerCatalogTagsSlicerTest {
+
+    @Test
+    @DisplayName("测试分页索引")
+    fun testSliceCatalog() {
+        val maxEntries = 3
+        val lastEntry = "apache"
+        val manifests = mapOf("nginx" to "v1", "linux" to "latest", "apache" to "v1", "java" to "v1")
+        val elementsHolder = DockerPaginationElementsHolder()
+
+        manifests.forEach {
+            if (StringUtils.isNotBlank(it.key)) {
+                elementsHolder.addElement(it.key)
+            }
+            DockerCatalogTagsSlicer.sliceCatalog(elementsHolder, maxEntries, lastEntry)
+        }
+
+        val catalogResponse = CatalogResponse(elementsHolder)
+        Assertions.assertEquals(catalogResponse.repositories.size, 1)
+    }
+
 }
