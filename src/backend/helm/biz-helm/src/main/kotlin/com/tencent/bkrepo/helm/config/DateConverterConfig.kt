4333a0a02d352c0403d179f18446a50805f3ca7b
weaving <>
2020-07-28 14:22:18 +0800

    fix: 修复codecc 检查问题:代码格式化，移除JsonUtil。#96

--- src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/config/DateConverterConfig.kt
+++ src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/config/DateConverterConfig.kt
@@ -15,14 +15,16 @@ class DateConverterConfig {
     @Bean
     fun localDateTimeConverter(): Converter<String, LocalDateTime> {
         return Converter {
-            source -> LocalDateTime.parse(source, DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT))
+            source ->
+            LocalDateTime.parse(source, DateTimeFormatter.ofPattern(DEFAULT_DATE_TIME_FORMAT))
         }
     }
 
     @Bean
     fun localDateConverter(): Converter<String, LocalDate> {
         return Converter {
-            source -> LocalDate.parse(source, DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT))
+            source ->
+            LocalDate.parse(source, DateTimeFormatter.ofPattern(DEFAULT_DATE_FORMAT))
         }
     }
 
