97f60defe2f4c8f45c430a453e469f5ca53c56b8
weaving <>
2020-08-19 23:58:05 +0800

    fix: rpm仓库移除com.sun.xml.internal.messaging.saaj.util.ByteInputStream类 #158

--- src/backend/rpm/biz-rpm/src/main/kotlin/com/tencent/bkrepo/rpm/util/redline/model/RpmXmlMetadataWithOldStream.kt
+++
@@ -1,9 +0,0 @@
-package com.tencent.bkrepo.rpm.util.redline.model
-
-import com.tencent.bkrepo.rpm.util.xStream.pojo.RpmXmlMetadata
-import java.io.InputStream
-
-data class RpmXmlMetadataWithOldStream(
-    val newRpmXmlMetadata: RpmXmlMetadata,
-    val OldPrimaryStream: InputStream
-)
