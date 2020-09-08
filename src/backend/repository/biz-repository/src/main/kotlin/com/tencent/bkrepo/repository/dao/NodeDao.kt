b7a760949c4a796d3ea73ae86c80c364a386ae79
carrypan <carrypan@tencent.com>
2020-09-08 11:34:16 +0800

    refactor: 节点查询优先根据folder排序 #165

--- src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/dao/NodeDao.kt
+++ src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/dao/NodeDao.kt
@@ -2,7 +2,7 @@ package com.tencent.bkrepo.repository.dao
 
 import com.tencent.bkrepo.common.mongo.dao.sharding.ShardingMongoDao
 import com.tencent.bkrepo.repository.model.TNode
-import com.tencent.bkrepo.repository.util.QueryHelper
+import com.tencent.bkrepo.repository.util.NodeQueryHelper
 import org.springframework.stereotype.Repository
 
 /**
@@ -14,6 +14,6 @@ class NodeDao : ShardingMongoDao<TNode>() {
      * 查询节点model
      */
     fun findNode(projectId: String, repoName: String, fullPath: String): TNode? {
-        return this.findOne(QueryHelper.nodeQuery(projectId, repoName, fullPath))
+        return this.findOne(NodeQueryHelper.nodeQuery(projectId, repoName, fullPath))
     }
 }
