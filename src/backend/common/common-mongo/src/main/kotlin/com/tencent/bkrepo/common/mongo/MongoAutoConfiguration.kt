f3d33a7a8c386eebef62303db0d6a1efe0a40c25
carrypan <carrypan@tencent.com>
2020-10-27 21:41:16 +0800

    refactor: spring-data-mongo 自动创建索引问题 #234

--- src/backend/common/common-mongo/src/main/kotlin/com/tencent/bkrepo/common/mongo/MongoAutoConfiguration.kt
+++ src/backend/common/common-mongo/src/main/kotlin/com/tencent/bkrepo/common/mongo/MongoAutoConfiguration.kt
@@ -22,6 +22,7 @@
 package com.tencent.bkrepo.common.mongo
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.mongo.MongoProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.PropertySource
@@ -51,13 +52,17 @@ class MongoAutoConfiguration {
      * remove _class
      */
     @Bean
-    fun mappingMongoConverter(mongoDatabaseFactory: MongoDatabaseFactory): MappingMongoConverter {
+    fun mappingMongoConverter(
+        mongoProperties: MongoProperties,
+        mongoDatabaseFactory: MongoDatabaseFactory
+    ): MappingMongoConverter {
         val dbRefResolver = DefaultDbRefResolver(mongoDatabaseFactory)
 
         val conversions = MongoCustomConversions(emptyList<Any>())
         val mappingContext = MongoMappingContext()
         mappingContext.setSimpleTypeHolder(conversions.simpleTypeHolder)
         mappingContext.afterPropertiesSet()
+        mappingContext.isAutoIndexCreation = mongoProperties.isAutoIndexCreation
 
         val converter = MappingMongoConverter(dbRefResolver, mappingContext)
         converter.setTypeMapper(DefaultMongoTypeMapper(null))
