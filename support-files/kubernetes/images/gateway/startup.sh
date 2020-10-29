4c36a1c189b040b565f09a90e49933f0f405d62b
carrypan <carrypan@tencent.com>
2020-10-29 18:24:23 +0800

    refactor: 优化容器化部署 #214

---
+++ support-files/kubernetes/images/gateway/startup.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+##初始化配置
+/data/workspace/render_tpl -u -m . -p /data/workspace /data/workspace/templates/gateway*
+/data/workspace/render_tpl -u -m . -p /data/workspace /data/workspace/frontend/repository/frontend#repository#index.html
+/data/workspace/render_tpl -u -m . -p /data/workspace /data/workspace/frontend/ui/frontend#ui#index.html
+
+##启动程序
+/usr/local/openresty/nginx/sbin/nginx
+tail -f /dev/null
