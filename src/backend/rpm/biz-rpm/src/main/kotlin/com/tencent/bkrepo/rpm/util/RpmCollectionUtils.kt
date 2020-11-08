faed937a9f3860c954ebeca702f847d71d5d1409
necrohuang <necrohuang@tencent.com>
2020-11-09 00:28:24 +0800

    Merge branch 'issue_235' into 'develop'

--- src/backend/rpm/biz-rpm/src/main/kotlin/com/tencent/bkrepo/rpm/util/RpmCollectionUtils.kt
+++ src/backend/rpm/biz-rpm/src/main/kotlin/com/tencent/bkrepo/rpm/util/RpmCollectionUtils.kt
@@ -3,56 +3,10 @@ package com.tencent.bkrepo.rpm.util
 import com.tencent.bkrepo.repository.pojo.node.NodeInfo
 
 object RpmCollectionUtils {
-
-    fun List<NodeInfo>.filterRpmCustom(set: MutableSet<String>, enabledFileLists: Boolean): List<NodeInfo> {
-        val resultList = mutableListOf<NodeInfo>()
-        try {
-            resultList.add(
-                this.first {
-                    it.metadata?.get("indexType") == "primary"
-                }
-            )
-            resultList.add(
-                this.first {
-                    it.metadata?.get("indexType") == "others"
-                }
-            )
-            if (enabledFileLists) {
-                resultList.add(
-                    this.first {
-                        it.metadata?.get("indexType") == "filelists"
-                    }
-                )
-            }
-        } catch (noSuchElementException: NoSuchElementException) {
-            // todo
-            // 仓库中还没有生成索引
-        }
-        val doubleSet = mutableSetOf<String>()
-        for (str in set) {
-            doubleSet.add(str)
-            doubleSet.add("${str}_gz")
-        }
-
-        for (str in doubleSet) {
-            try {
-                resultList.add(
-                    this.first {
-                        it.metadata?.get("indexName") == str
-                    }
-                )
-            } catch (noSuchElementException: NoSuchElementException) {
-                // todo
-                // 用户未上传对应分组文件
-            }
-        }
-        return resultList
-    }
-
     /**
      * 检查repodata 目录是否契合深度
      */
-    fun filterByDepth(repodataList: MutableList<String>, depth: Int): List<String> {
+    fun filterByDepth(repodataList: List<String>, depth: Int): List<String> {
         return repodataList.filter {
             it.removePrefix("/").removeSuffix("/").split("/").size == (depth.inc())
         }
