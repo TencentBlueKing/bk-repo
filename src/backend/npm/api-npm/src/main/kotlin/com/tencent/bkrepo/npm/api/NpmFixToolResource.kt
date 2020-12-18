b0bba5ff355daae031cb751bd43d766004447f79
陈灿 <charis@canway.net>
2020-12-18 16:26:26 +0800

    fix: 修复npm历史数据的元数据中dist里面增加packageSize字段 #281

--- src/backend/npm/api-npm/src/main/kotlin/com/tencent/bkrepo/npm/api/NpmFixToolResource.kt
+++ src/backend/npm/api-npm/src/main/kotlin/com/tencent/bkrepo/npm/api/NpmFixToolResource.kt
@@ -3,6 +3,7 @@ package com.tencent.bkrepo.npm.api
 import com.tencent.bkrepo.common.artifact.api.ArtifactPathVariable
 import com.tencent.bkrepo.npm.artifact.NpmArtifactInfo
 import com.tencent.bkrepo.npm.pojo.fixtool.DateTimeFormatResponse
+import com.tencent.bkrepo.npm.pojo.fixtool.PackageMetadataFixResponse
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,4 +17,10 @@ interface NpmFixToolResource {
         @ArtifactPathVariable artifactInfo: NpmArtifactInfo,
         pkgName: String
     ): DateTimeFormatResponse
+
+    @ApiOperation("修复历史数据dist对象中增加packageSize字段")
+    @GetMapping("/{projectId}/{repoName}/fixPackageSizeField")
+    fun fixPackageSizeField(
+        @ArtifactPathVariable artifactInfo: NpmArtifactInfo
+    ): PackageMetadataFixResponse
 }
