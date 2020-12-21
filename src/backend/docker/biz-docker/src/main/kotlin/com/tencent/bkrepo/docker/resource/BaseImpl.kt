d79d4f9fa8b4e2a6ea0460c7a252584e8259c88d
owenlxu <owenlxu@tencent.com>
2020-12-21 20:49:23 +0800

    fix: fix docker auth #274

--- src/backend/docker/biz-docker/src/main/kotlin/com/tencent/bkrepo/docker/resource/BaseImpl.kt
+++ src/backend/docker/biz-docker/src/main/kotlin/com/tencent/bkrepo/docker/resource/BaseImpl.kt
@@ -31,6 +31,8 @@
 
 package com.tencent.bkrepo.docker.resource
 
+import com.tencent.bkrepo.common.api.constant.ANONYMOUS_USER
+import com.tencent.bkrepo.common.security.exception.AuthenticationException
 import com.tencent.bkrepo.docker.api.Base
 import com.tencent.bkrepo.docker.constant.DOCKER_API_VERSION
 import com.tencent.bkrepo.docker.constant.DOCKER_HEADER_API_VERSION
@@ -43,6 +45,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class BaseImpl : Base {
     override fun ping(userId: String): DockerResponse {
+        if (userId == ANONYMOUS_USER) {
+            throw AuthenticationException()
+        }
         return ResponseEntity.ok().header(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).header(DOCKER_HEADER_API_VERSION, DOCKER_API_VERSION).body("{}")
     }
 }
