ec011fcd69e480948ff6c6d243417e6caf0a7d8a
carrypan <carrypan@tencent.com>
2021-01-29 15:20:18 +0800

    feature: repository增加包依赖关系管理 #320

--- src/backend/repository/api-repository/src/main/kotlin/com/tencent/bkrepo/repository/api/PackageDependentsClient.kt
+++ src/backend/repository/api-repository/src/main/kotlin/com/tencent/bkrepo/repository/api/PackageDependentsClient.kt
@@ -41,6 +41,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Primary
 @FeignClient(REPOSITORY_SERVICE_NAME, contextId = "PackageDependentsClient")
