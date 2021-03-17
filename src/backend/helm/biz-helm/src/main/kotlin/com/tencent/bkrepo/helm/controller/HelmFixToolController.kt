8ca7e7b7d7d9ff448002a7819b8bcd163e84763e
陈灿 <charis@canway.net>
2021-03-17 18:52:05 +0800

    fix: 修复helm的索引文件的制品的created字段时间不对的bug #305

--- src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/controller/HelmFixToolController.kt
+++ src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/controller/HelmFixToolController.kt
@@ -1,5 +1,9 @@
 package com.tencent.bkrepo.helm.controller
 
+import com.tencent.bkrepo.common.api.pojo.Response
+import com.tencent.bkrepo.common.artifact.api.ArtifactPathVariable
+import com.tencent.bkrepo.common.service.util.ResponseBuilder
+import com.tencent.bkrepo.helm.artifact.HelmArtifactInfo
 import com.tencent.bkrepo.helm.pojo.fixtool.PackageManagerResponse
 import com.tencent.bkrepo.helm.service.FixToolService
 import io.swagger.annotations.ApiOperation
@@ -14,5 +18,14 @@ class HelmFixToolController(
     @GetMapping("/ext/package/populate")
     fun fixPackageVersion(): List<PackageManagerResponse> {
         return fixToolService.fixPackageVersion()
+    }
+
+    @ApiOperation("修复index.yaml文件中的制品包创建时间问题")
+    @GetMapping("/ext/{projectId}/{repoName}/repairDateFormat")
+    fun repairPackageCreatedDate(
+        @ArtifactPathVariable artifactInfo: HelmArtifactInfo
+    ): Response<Void> {
+        fixToolService.repairPackageCreatedDate(artifactInfo)
+        return ResponseBuilder.success()
     }
 }
