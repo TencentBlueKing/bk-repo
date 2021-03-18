b682b4550a50cd655b0efd6bb19dfe943c48f97a
owenlxu <owenlxu@tencent.com>
2021-03-18 22:09:53 +0800

    feature : add bkrepo ticket auth #242

--- src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/service/local/UserServiceImpl.kt
+++ src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/service/local/UserServiceImpl.kt
@@ -266,6 +266,7 @@ class UserServiceImpl constructor(
             val id = IDUtil.genRandomId()
             val now = LocalDateTime.now()
             var expiredTime: LocalDateTime? = null
+            if (expiredAt.length == 0) expiredAt = null
             expiredAt?.let {
                 val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
                 expiredTime = LocalDateTime.parse(expiredAt, dateTimeFormatter)
