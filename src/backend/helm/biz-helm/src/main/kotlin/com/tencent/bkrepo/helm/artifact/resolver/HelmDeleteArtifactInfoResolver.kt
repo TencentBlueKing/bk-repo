3c3d0d5745db36837faf74e11817de8d7f12b153
陈灿 <charis@canway.net>
2021-09-22 15:13:57 +0800

    --story=858130378 fix: 解决helm仓库页面删除包的版本404异常

--- src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/artifact/resolver/HelmDeleteArtifactInfoResolver.kt
+++ src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/artifact/resolver/HelmDeleteArtifactInfoResolver.kt
@@ -5,6 +5,7 @@ import com.tencent.bkrepo.common.artifact.resolve.path.ArtifactInfoResolver
 import com.tencent.bkrepo.common.artifact.resolve.path.Resolver
 import com.tencent.bkrepo.common.artifact.util.PackageKeys
 import com.tencent.bkrepo.helm.constants.NAME
+import com.tencent.bkrepo.helm.constants.PACKAGE_KEY
 import com.tencent.bkrepo.helm.constants.VERSION
 import com.tencent.bkrepo.helm.pojo.artifact.HelmDeleteArtifactInfo
 import org.springframework.stereotype.Component
@@ -22,20 +23,30 @@ class HelmDeleteArtifactInfoResolver : ArtifactInfoResolver {
     ): ArtifactInfo {
         // 判断是客户端的请求还是页面发送的请求分别进行处理
         val requestURL = request.requestURL
-        // 页面删除包请求
-        return if (requestURL.contains("/ext/package/delete/$projectId/$repoName")) {
-            val packageKey = request.queryString.substringAfterLast("=")
-            HelmDeleteArtifactInfo(projectId, repoName, packageKey)
-        } else if (requestURL.contains("/ext/version/delete/$projectId/$repoName")) {
-            val parameters = request.queryString.split("&")
-            val packageKey = parameters.first().substringAfterLast("=")
-            val version = parameters.last().substringAfterLast("=")
-            HelmDeleteArtifactInfo(projectId, repoName, packageKey, version)
-        } else {
-            val attributes = request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE) as Map<*, *>
-            val id = attributes[NAME].toString().trim()
-            val version = attributes[VERSION].toString().trim()
-            HelmDeleteArtifactInfo(projectId, repoName, PackageKeys.ofHelm(id), version)
+        return when {
+            // 页面删除包请求
+            requestURL.contains(PACKAGE_DELETE_PREFIX) -> {
+                val packageKey = request.getParameter(PACKAGE_KEY)
+                HelmDeleteArtifactInfo(projectId, repoName, packageKey)
+            }
+            // 页面删除包版本请求
+            requestURL.contains(PACKAGE_VERSION_DELETE_PREFIX) -> {
+                val packageKey = request.getParameter(PACKAGE_KEY)
+                val version = request.getParameter(VERSION)
+                HelmDeleteArtifactInfo(projectId, repoName, packageKey, version)
+            }
+            else -> {
+                // 客户端请求删除版本
+                val attributes = request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE) as Map<*, *>
+                val id = attributes[NAME].toString().trim()
+                val version = attributes[VERSION].toString().trim()
+                HelmDeleteArtifactInfo(projectId, repoName, PackageKeys.ofHelm(id), version)
+            }
         }
+    }
+
+    companion object {
+        private const val PACKAGE_DELETE_PREFIX = "/ext/package/delete/"
+        private const val PACKAGE_VERSION_DELETE_PREFIX = "/ext/version/delete/"
     }
 }
