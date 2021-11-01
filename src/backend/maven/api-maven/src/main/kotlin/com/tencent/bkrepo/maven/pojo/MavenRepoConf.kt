5b151a56f1f73ec9857572c24e0241428dce3ccc
weaving <weaving@canway.net>
2021-11-01 10:19:00 +0800

    -- story=868988315 chore: 移除无用代码

--- src/backend/maven/api-maven/src/main/kotlin/com/tencent/bkrepo/maven/pojo/MavenRepoConf.kt
+++ src/backend/maven/api-maven/src/main/kotlin/com/tencent/bkrepo/maven/pojo/MavenRepoConf.kt
@@ -5,8 +5,8 @@ data class MavenRepoConf(
     val mavenSnapshotVersionBehavior: Int?,
     /**
      * The maximum number of unique snapshots of a single artifact to store.
-    Once the number of snapshots exceeds this setting, older versions are removed.
-    A value of 0 (default) indicates there is no limit, and unique snapshots are not cleaned up.
+     Once the number of snapshots exceeds this setting, older versions are removed.
+     A value of 0 (default) indicates there is no limit, and unique snapshots are not cleaned up.
      */
     val maxUniqueSnapshots: Int?
 )
