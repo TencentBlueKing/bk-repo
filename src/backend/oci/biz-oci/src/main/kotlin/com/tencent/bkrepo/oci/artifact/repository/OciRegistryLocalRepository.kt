595f1c99e73d35d2ff7b559c4e4a9cdeb8dfa494
陈灿 <charis@canway.net>
2021-11-06 17:48:51 +0800

    --story=868909633: Repo支持OCI(包含了镜像和chart)框架搭建开发

---
+++ src/backend/oci/biz-oci/src/main/kotlin/com/tencent/bkrepo/oci/artifact/repository/OciRegistryLocalRepository.kt
@@ -0,0 +1,5 @@
+package com.tencent.bkrepo.oci.artifact.repository
+
+import com.tencent.bkrepo.common.artifact.repository.local.LocalRepository
+
+class OciRegistryLocalRepository : LocalRepository()
