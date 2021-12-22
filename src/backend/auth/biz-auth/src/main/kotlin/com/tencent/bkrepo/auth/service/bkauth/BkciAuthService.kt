801a1f00a350dbb5bc935ef9e9e206f238c91897
zacyanliu <zacyanliu@tencent.com>
2021-12-22 11:06:34 +0800

    Revert "#578 http请求异常日志修改"
    This reverts commit a5563c2e4e7e87a3b3665869033f29ba2df3f684.

--- src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/service/bkauth/BkciAuthService.kt
+++ src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/service/bkauth/BkciAuthService.kt
@@ -87,7 +87,7 @@ class BkciAuthService @Autowired constructor(
             resourcePermissionCache.put(cacheKey, responseObject.data)
             responseObject.data
         } catch (exception: Exception) {
-            logger.error("validateProjectUsers url is $url, error: ", exception)
+            logger.error("validateProjectUsers error:", exception)
             false
         }
     }
