8f1dd7886ab248b5c2120aa0fea094c801f2933f
weaving <weaving@canway.net>
2022-01-05 15:43:28 +0800

    -- story=858172692 chore: 代码格式化

--- src/backend/nuget/biz-nuget/src/main/kotlin/com/tencent/bkrepo/nuget/handler/NugetPackageHandler.kt
+++ src/backend/nuget/biz-nuget/src/main/kotlin/com/tencent/bkrepo/nuget/handler/NugetPackageHandler.kt
@@ -24,7 +24,8 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import java.util.*
+import java.util.StringJoiner
+import kotlin.collections.HashSet
 
 @Component
 class NugetPackageHandler {
