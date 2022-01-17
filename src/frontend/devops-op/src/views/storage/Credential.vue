5c08cecf224513eb1841f3d39363414b2beb4082
kunlongli <kunlongli@tencent.com>
2022-01-17 22:36:22 +0800

    --story=871193677 feat:实现删除凭据

--- src/frontend/devops-op/src/views/storage/Credential.vue
+++ src/frontend/devops-op/src/views/storage/Credential.vue
@@ -64,7 +64,7 @@
   </div>
 </template>
 <script>
-import { credentials } from '@/api/storage'
+import { credentials, deleteCredential } from '@/api/storage'
 import CreateCredentialDialog from '@/views/storage/components/CreateCredentialDialog'
 
 export default {
@@ -94,7 +94,21 @@ export default {
       console.log('edit')
     },
     handleDelete(index, credential) {
-      console.log('delete')
+      this.$confirm(`是否确定删除${credential.key}`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        deleteCredential(credential.key).then(_ => {
+          this.credentials.splice(index, 1)
+          this.$message.success('删除成功')
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消'
+        })
+      })
     },
     expireDays(expireDays) {
       return parseInt(expireDays) <= 0 ? '永久' : expireDays
