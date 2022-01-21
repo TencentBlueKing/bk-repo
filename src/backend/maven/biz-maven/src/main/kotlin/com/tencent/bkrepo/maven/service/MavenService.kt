270391a4eb05565b71c68649fb350c83edc7366f
zacyanliu <zacyanliu@tencent.com>
2022-01-21 21:07:10 +0800

    --bug=96421709 优化异常处理类

--- src/backend/maven/biz-maven/src/main/kotlin/com/tencent/bkrepo/maven/service/MavenService.kt
+++ src/backend/maven/biz-maven/src/main/kotlin/com/tencent/bkrepo/maven/service/MavenService.kt
@@ -35,7 +35,6 @@ import com.tencent.bkrepo.auth.pojo.enums.PermissionAction
 import com.tencent.bkrepo.auth.pojo.enums.ResourceType
 import com.tencent.bkrepo.common.artifact.api.ArtifactFile
 import com.tencent.bkrepo.common.artifact.constant.PARAM_DOWNLOAD
-import com.tencent.bkrepo.common.artifact.exception.NodeNotFoundException
 import com.tencent.bkrepo.common.artifact.repository.context.ArtifactContextHolder
 import com.tencent.bkrepo.common.artifact.repository.context.ArtifactDownloadContext
 import com.tencent.bkrepo.common.artifact.repository.context.ArtifactQueryContext
@@ -46,7 +45,6 @@ import com.tencent.bkrepo.common.artifact.view.ViewModelService
 import com.tencent.bkrepo.common.security.permission.Permission
 import com.tencent.bkrepo.common.service.util.HttpContextHolder
 import com.tencent.bkrepo.maven.artifact.MavenArtifactInfo
-import com.tencent.bkrepo.maven.exception.MavenArtifactFormatException
 import com.tencent.bkrepo.maven.exception.MavenArtifactNotFoundException
 import com.tencent.bkrepo.repository.api.NodeClient
 import com.tencent.bkrepo.repository.pojo.list.HeaderItem
@@ -57,7 +55,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import org.springframework.web.HttpMediaTypeException
 
 @Service
 class MavenService(
@@ -82,22 +79,16 @@ class MavenService(
     fun dependency(mavenArtifactInfo: MavenArtifactInfo) {
         // 为了兼容jfrog，当查询到目录时，会展示当前目录下所有子项，而不是直接报错
         with(mavenArtifactInfo) {
-            try {
-                val node = nodeClient.getNodeDetail(projectId, repoName, getArtifactFullPath()).data
-                    ?: throw NodeNotFoundException(getArtifactFullPath())
-                val download = HttpContextHolder.getRequest().getParameter(PARAM_DOWNLOAD)?.toBoolean() ?: false
-                if (node.folder && !download) {
-                    logger.info("The folder: ${getArtifactFullPath()} will be displayed...")
-                    renderListView(node, this)
-                } else {
-                    logger.info("The dependency file: ${getArtifactFullPath()} will be downloaded... ")
-                    val context = ArtifactDownloadContext()
-                    ArtifactContextHolder.getRepository().download(context)
-                }
-            } catch (e: NodeNotFoundException) {
-                throw MavenArtifactNotFoundException(e.message!!)
-            } catch (e: HttpMediaTypeException) {
-                throw MavenArtifactFormatException(e.message.toString())
+            val node = nodeClient.getNodeDetail(projectId, repoName, getArtifactFullPath()).data
+                ?: throw MavenArtifactNotFoundException("Artifact ${getArtifactFullPath()} could not find..")
+            val download = HttpContextHolder.getRequest().getParameter(PARAM_DOWNLOAD)?.toBoolean() ?: false
+            if (node.folder && !download) {
+                logger.info("The folder: ${getArtifactFullPath()} will be displayed...")
+                renderListView(node, this)
+            } else {
+                logger.info("The dependency file: ${getArtifactFullPath()} will be downloaded... ")
+                val context = ArtifactDownloadContext()
+                ArtifactContextHolder.getRepository().download(context)
             }
         }
     }
