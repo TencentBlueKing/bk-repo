8e128fb18f34f814cc37d2133d36495a27f356d4
zacyanliu <zacyanliu@tencent.com>
2022-01-21 15:21:18 +0800

    --bug=96415151 maven上传aar后缀文件无法更新meatdata文件

--- src/backend/maven/api-maven/src/main/kotlin/com/tencent/bkrepo/maven/Constant.kt
+++ src/backend/maven/api-maven/src/main/kotlin/com/tencent/bkrepo/maven/Constant.kt
@@ -6,7 +6,7 @@ const val MAX_UNIQUE_SNAPSHOTS = "MAX_UNIQUE_SNAPSHOTS"
 const val SNAPSHOT_SUFFIX = "-SNAPSHOT"
 
 const val PACKAGE_SUFFIX_REGEX =
-    "(.+)\\.(jar|war|tar|ear|ejb|rar|msi|rpm|tar\\.bz2|tar\\.gz|tar\\.xz|tbz|zip|pom)\$"
+    "(.+)\\.(jar|war|tar|ear|ejb|rar|msi|aar|rpm|tar\\.bz2|tar\\.gz|tar\\.xz|tbz|zip|pom)\$"
 
 const val ARTIFACT_FORMAT = "^%s-%s-?(SNAPSHOT|[0-9]{8}\\.[0-9]{6}-[0-9]+)?-?(.+)?.%s\$"
 
