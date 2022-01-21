3a043e39d947ca636257a03091cb1c6701167899
kunlongli <kunlongli@tencent.com>
2022-01-21 19:54:40 +0800

    --story=871193799 feat: 添加存储凭据接口文档注解

--- src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/controller/user/UserStorageCredentialsController.kt
+++ src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/controller/user/UserStorageCredentialsController.kt
@@ -45,6 +45,8 @@ import com.tencent.bkrepo.repository.message.RepositoryMessageCode
 import com.tencent.bkrepo.repository.pojo.credendials.StorageCredentialsCreateRequest
 import com.tencent.bkrepo.repository.pojo.credendials.StorageCredentialsUpdateRequest
 import com.tencent.bkrepo.repository.service.repo.StorageCredentialService
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -56,6 +58,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Api("存储凭据管理")
 @Principal(PrincipalType.ADMIN)
 @RestController
 @RequestMapping("/api/storage/credentials")
@@ -63,6 +66,7 @@ class UserStorageCredentialsController(
     private val storageCredentialService: StorageCredentialService
 ) {
 
+    @ApiOperation("创建凭据")
     @PostMapping
     fun create(
         @RequestAttribute userId: String,
@@ -72,6 +76,7 @@ class UserStorageCredentialsController(
         return ResponseBuilder.buildTyped(createdCredential)
     }
 
+    @ApiOperation("更新凭据")
     @PutMapping("/{credentialsKey}")
     fun update(
         @RequestAttribute userId: String,
@@ -83,6 +88,7 @@ class UserStorageCredentialsController(
         return ResponseBuilder.buildTyped(updatedCredentials)
     }
 
+    @ApiOperation("获取凭据列表")
     @GetMapping
     fun list(@RequestParam("region", required = false) region: String?): Response<List<StorageCredentials>> {
         val storageCredentialsList = storageCredentialService.list(region).map {
@@ -95,17 +101,22 @@ class UserStorageCredentialsController(
         return ResponseBuilder.buildTyped(storageCredentialsList)
     }
 
+    @ApiOperation("获取默认凭据")
     @GetMapping("/default")
     fun default(): Response<StorageCredentials> {
         return ResponseBuilder.buildTyped(mask(storageCredentialService.default()))
     }
 
+    @ApiOperation("删除凭据")
     @DeleteMapping("/{credentialKey}")
     fun delete(@PathVariable("credentialKey") credentialKey: String): Response<Void> {
         storageCredentialService.delete(credentialKey)
         return ResponseBuilder.success()
     }
 
+    /**
+     * 隐藏敏感信息
+     */
     private fun mask(storageCredentials: StorageCredentials): StorageCredentials {
         if (storageCredentials is InnerCosCredentials) {
             return storageCredentials.copy(secretId = "*", secretKey = "*")
