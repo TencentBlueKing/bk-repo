ae6f72d3784dafe45016dd250525c9d3e44eb68b
owenlxu <owenlxu@tencent.com>
2022-02-10 11:38:32 +0800

    --story=871973679 feat:社区版新的版本包

--- support-files/kubernetes/images/build.sh
+++ support-files/kubernetes/images/build.sh
@@ -17,7 +17,7 @@ PUSH=0
 REGISTRY=docker.io/bkrepo
 USERNAME=
 PASSWORD=
-BACKENDS=(repository auth generic docker helm npm pypi)
+BACKENDS=(repository auth generic docker helm npm pypi replication)
 
 cd $(dirname $0)
 WORKING_DIR=$(pwd)
