06b129892d4fe84ee634377ea3dbe620e4f6051c
zacyanliu <zacyanliu@tencent.com>
2022-02-24 17:41:56 +0800

    --story=871511829 还原删除的文件

--- src/backend/generic/biz-generic/src/main/kotlin/com/tencent/bkrepo/generic/artifact/configuration/AutoIndexRepositorySettings.kt
+++ src/backend/generic/biz-generic/src/main/kotlin/com/tencent/bkrepo/generic/artifact/configuration/AutoIndexRepositorySettings.kt
@@ -1,7 +1,7 @@
 /*
  * Tencent is pleased to support the open source community by making BK-CI 蓝鲸持续集成平台 available.
  *
- * Copyright (C) 2019 THL A29 Limited, a Tencent company.  All rights reserved.
+ * Copyright (C) 2022 THL A29 Limited, a Tencent company.  All rights reserved.
  *
  * BK-CI 蓝鲸持续集成平台 is licensed under the MIT license.
  *
@@ -29,6 +29,9 @@ package com.tencent.bkrepo.generic.artifact.configuration
 
 import com.tencent.bkrepo.common.artifact.pojo.configuration.RepositoryConfiguration
 
+/**
+ * 自动创建索引配置
+ */
 data class AutoIndexRepositorySettings(
     /**
      * 是否启用自动创建目录索引功能
