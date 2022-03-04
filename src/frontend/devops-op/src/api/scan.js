747f1c7c6843affa8f5add8ceb17db05ea92a7e3
kunlongli <kunlongli@tencent.com>
2022-03-04 12:04:10 +0800

    --story=872007241 feat:添加扫描按钮

---
+++ src/frontend/devops-op/src/api/scan.js
@@ -0,0 +1,41 @@
+import request from '@/utils/request'
+
+const PREFIX_SCAN = '/scanner/api/scan'
+const PREFIX_SCANNER = '/scanner/api/scanners'
+
+export function scanners() {
+  return request({
+    url: `${PREFIX_SCANNER}`,
+    method: 'get'
+  })
+}
+
+export function scan(scanner, projectId, repoName, path) {
+  return request({
+    url: `${PREFIX_SCAN}`,
+    method: 'post',
+    data: {
+      scanner: scanner,
+      rule: {
+        relation: 'AND',
+        rules: [
+          {
+            'field': 'projectId',
+            'value': projectId,
+            'operation': 'EQ'
+          },
+          {
+            'field': 'repoName',
+            'value': repoName,
+            'operation': 'EQ'
+          },
+          {
+            'field': 'path',
+            'value': path,
+            'operation': 'PREFIX'
+          }
+        ]
+      }
+    }
+  })
+}
