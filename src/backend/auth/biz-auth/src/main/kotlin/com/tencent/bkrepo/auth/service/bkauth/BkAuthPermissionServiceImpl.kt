b16e919f5b62a129f3e9003bcb088951b768b23c
owenlxu <owenlxu@tencent.com>
2022-03-10 20:40:13 +0800

    --story=872673361 删除auth中匿名校验

--- src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/service/bkauth/BkAuthPermissionServiceImpl.kt
+++ src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/service/bkauth/BkAuthPermissionServiceImpl.kt
@@ -180,16 +180,10 @@ class BkAuthPermissionServiceImpl constructor(
 
     override fun checkPermission(request: CheckPermissionRequest): Boolean {
 
-        // devops匿名访问请求处理
-        if (matchAnonymousCond(request.appId, request.uid)) {
-            logger.warn("devops anonymous pass [$request] ")
-            return true
-        }
-
         // bcs或bkrepo账号
         if (matchBcsOrRepoCond(request.appId)) return super.checkPermission(request) || checkDevopsPermission(request)
 
-        // devops实名访问请求处理
+        // devops账号
         if (matchDevopsCond(request.appId)) return checkDevopsPermission(request)
 
         // 非devops体系
@@ -215,9 +209,6 @@ class BkAuthPermissionServiceImpl constructor(
         return devopsAppIdList.contains(appId)
     }
 
-    private fun matchAnonymousCond(appId: String?, uid: String): Boolean {
-        return appId == bkAuthConfig.devopsAppId && uid == ANONYMOUS_USER && bkAuthConfig.devopsAllowAnonymous
-    }
 
     companion object {
         private val logger = LoggerFactory.getLogger(BkAuthPermissionServiceImpl::class.java)
