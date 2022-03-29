32c2e31df491299a9b394795b112a94b7c382b2d
kunlongli <kunlongli@tencent.com>
2022-03-29 16:22:56 +0800

    --story=872941501 feat: 实现停止扫描接口

--- src/backend/common/common-scanner/src/main/kotlin/com/tencent/bkrepo/common/scanner/pojo/scanner/SubScanTaskStatus.kt
+++ src/backend/common/common-scanner/src/main/kotlin/com/tencent/bkrepo/common/scanner/pojo/scanner/SubScanTaskStatus.kt
@@ -50,7 +50,7 @@ enum class SubScanTaskStatus {
          * 判断[status]是否是已结束的状态
          */
         fun finishedStatus(status: SubScanTaskStatus): Boolean {
-            return status == TIMEOUT || status == FAILED || status == SUCCESS
+            return status == TIMEOUT || status == FAILED || status == STOP || status == SUCCESS
         }
 
         /**
