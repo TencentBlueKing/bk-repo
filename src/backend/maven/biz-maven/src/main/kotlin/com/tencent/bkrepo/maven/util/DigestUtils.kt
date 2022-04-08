e2d4d2c86ba68264e48ecf9694534bd4fb8845ca
zacyanliu <zacyanliu@tencent.com>
2022-04-08 16:03:24 +0800

    --bug=98075987 maven上传会报500， 3种报错，1 唯一键冲突 2创建packageversion报错 3 java.lang.ArrayIndexOutOfBoundsException: null

--- src/backend/maven/biz-maven/src/main/kotlin/com/tencent/bkrepo/maven/util/DigestUtils.kt
+++ src/backend/maven/biz-maven/src/main/kotlin/com/tencent/bkrepo/maven/util/DigestUtils.kt
@@ -7,8 +7,11 @@ object DigestUtils {
     private val sha512Digest = MessageDigest.getInstance("SHA-512")
 
     fun sha512(buffer: ByteArray, offset: Int, length: Int): String {
-        sha512Digest.update(buffer, offset, length)
-        return hexToString(sha512Digest.digest(), 128)
+        // MessageDigest是thread unsafe的，解决concurrent时会报java.lang.ArrayIndexOutOfBoundsException: null
+        synchronized(sha512Digest) {
+            sha512Digest.update(buffer, offset, length)
+            return hexToString(sha512Digest.digest(), 128)
+        }
     }
 
     private fun hexToString(byteArray: ByteArray, length: Int): String {
