371306939394d94c286ab00020318273a8e71919
felixncheng <felixncheng@tencent.com>
2022-04-21 20:28:59 +0800

    --story=871796569 优化增量上传代码

--- src/backend/generic/biz-generic/src/main/kotlin/com/tencent/bkrepo/generic/service/DeltaSyncService.kt
+++ src/backend/generic/biz-generic/src/main/kotlin/com/tencent/bkrepo/generic/service/DeltaSyncService.kt
@@ -17,7 +17,6 @@ import com.tencent.bkrepo.common.artifact.repository.context.ArtifactDownloadCon
 import com.tencent.bkrepo.common.artifact.repository.core.ArtifactService
 import com.tencent.bkrepo.common.artifact.resolve.file.ArtifactFileFactory
 import com.tencent.bkrepo.common.artifact.resolve.file.chunk.ChunkedFileOutputStream
-import com.tencent.bkrepo.common.artifact.stream.ArtifactInputStream
 import com.tencent.bkrepo.common.artifact.stream.FileArtifactInputStream
 import com.tencent.bkrepo.common.artifact.util.http.IOExceptionUtils
 import com.tencent.bkrepo.common.bksync.BkSync
@@ -49,10 +48,10 @@ import org.springframework.http.MediaType
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.stereotype.Service
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter
-import java.io.BufferedOutputStream
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.io.InputStream
+import java.io.OutputStream
 import java.time.LocalDateTime
 import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.math.ceil
@@ -69,7 +68,7 @@ class DeltaSyncService(
     val nodeClient: NodeClient,
     val signFileDao: SignFileDao,
     val repositoryClient: RepositoryClient,
-    val storageProperties: StorageProperties,
+    storageProperties: StorageProperties,
     val taskExecutor: ThreadPoolTaskScheduler
 ) : ArtifactService() {
 
@@ -107,29 +106,37 @@ class DeltaSyncService(
             val chunkedArtifactFile = ArtifactFileFactory.buildChunked()
             val chunkedFileOutputStream = ChunkedFileOutputStream(chunkedArtifactFile)
             val outputStream = CompositeOutputStream(chunkedFileOutputStream, response.outputStream).buffered()
-            outputStream.use {
-                val artifactInputStream = storageManager.loadArtifactInputStream(node, storageCredentials)
-                    ?: throw ArtifactNotFoundException("file[$sha256] not found in ${storageCredentials?.key}")
-                val nanoTime = measureNanoTime {
-                    val bkSync = BkSync(blockSize)
-                    generateSignFile(artifactInputStream, bkSync, outputStream)
+            val artifactInputStream = storageManager.loadArtifactInputStream(node, storageCredentials)?.buffered()
+                ?: throw ArtifactNotFoundException("file[$sha256] not found in ${storageCredentials?.key}")
+            val nanoTime = measureNanoTime {
+                try {
+                    generateSignFile(artifactInputStream, outputStream)
+                } finally {
+                    // chunkedFileOutputStream close 会删除掉临时文件，
+                    // 由于用了buffered stream,所以这里需要flush,而且不能使用close
+                    outputStream.flush()
+                    artifactInputStream.close()
                 }
-                val throughput = Throughput(chunkedArtifactFile.getSize(), nanoTime)
-                logger.info("Success to generate artifact sign file [${node.fullPath}], $throughput.")
-                outputStream.flush()
-                chunkedArtifactFile.finish()
-                saveSignFile(node, chunkedArtifactFile)
             }
+            chunkedArtifactFile.finish()
+            val throughput = Throughput(chunkedArtifactFile.getSize(), nanoTime)
+            logger.info("Success to generate artifact sign file [${node.fullPath}], $throughput.")
+            saveSignFile(node, chunkedArtifactFile)
         }
     }
 
+    /**
+     * 生成sign file
+     * @param inputStream 需要签名的文件输入流
+     * @param outputStream sign file的输出流
+     * */
     private fun generateSignFile(
-        artifactInputStream: ArtifactInputStream,
-        bkSync: BkSync,
-        outputStream: BufferedOutputStream
+        inputStream: InputStream,
+        outputStream: OutputStream
     ) {
         try {
-            artifactInputStream.buffered().use { bkSync.checksum(artifactInputStream, outputStream) }
+            val bkSync = BkSync(blockSize)
+            bkSync.checksum(inputStream, outputStream)
         } catch (exception: IOException) {
             if (IOExceptionUtils.isClientBroken(exception)) {
                 val message = exception.message.orEmpty()
@@ -158,48 +165,19 @@ class DeltaSyncService(
                     )
                 }
             }
-            val hasCompleted = AtomicBoolean(false)
-            val clientError = AtomicBoolean(false)
-            val contentLength = request.contentLengthLong
-            val emitter = SseEmitter(patchTimeout)
-            emitter.onCompletion { hasCompleted.set(true) }
             val counterInputStream = CounterInputStream(deltaFile.getInputStream())
             val blockInputStream = getBlockInputStream(node, storageCredentials)
-            val file = ArtifactFileFactory.buildBkSync(blockInputStream, counterInputStream, blockSize)
-            val uploadSha256 = HeaderUtils.getHeader(HEADER_SHA256)
-            val uploadMd5 = HeaderUtils.getHeader(HEADER_MD5)
-            val expires = HeaderUtils.getLongHeader(HEADER_EXPIRES)
-            val repository = ArtifactContextHolder.getRepository(RepositoryCategory.LOCAL)
-            val metadata = (repository as GenericLocalRepository).resolveMetadata(request)
-            val reportAction = { reportProcess(clientError, hasCompleted, counterInputStream, contentLength, emitter) }
-            val ackFuture = taskExecutor.scheduleWithFixedDelay(reportAction, HEART_BEAT)
+            val emitter = SseEmitter(patchTimeout)
+            val patchContext = buildPatchContext(counterInputStream, emitter, this, blockInputStream)
+            emitter.onCompletion { patchContext.hasCompleted.set(true) }
+            val reportAction = { reportProcess(patchContext) }
+            val ackFuture = taskExecutor.scheduleWithFixedDelay(reportAction, HEART_BEAT_INTERVAL)
             taskExecutor.execute {
                 try {
-                    verifyCheckSum(uploadMd5, uploadSha256, file)
-                    val nodeCreateRequest =
-                        buildPatchNewNodeCreateRequest(
-                            repositoryDetail,
-                            artifactInfo,
-                            userId,
-                            file,
-                            expires,
-                            overwrite,
-                            metadata
-                        )
-                    val nodeDetail = storageManager.storeArtifactFile(nodeCreateRequest, file, storageCredentials)
-                    val event = SseEmitter.event().name(PATCH_EVENT_TYPE_DATA)
-                        .data(nodeDetail, MediaType.APPLICATION_JSON)
-                    emitter.send(event)
-                    emitter.complete()
-                } catch (e: ErrorCodeException) {
-                    val msg = SseEmitter.event().name(PATCH_EVENT_TYPE_ERROR)
-                        .data(e.message.orEmpty())
-                    emitter.send(msg)
-                    emitter.complete()
-                } catch (e: Exception) {
-                    emitter.completeWithError(e)
+                    doPatch(patchContext)
                 } finally {
                     blockInputStream.close()
+                    counterInputStream.close()
                     ackFuture.cancel(true)
                 }
             }
@@ -208,40 +186,69 @@ class DeltaSyncService(
     }
 
     /**
+     * 执行patch
+     * @param patchContext patch上下文
+     * */
+    private fun doPatch(patchContext: PatchContext) {
+        with(patchContext) {
+            try {
+                verifyCheckSum(uploadMd5, uploadSha256, file)
+                val nodeCreateRequest =
+                    buildPatchNewNodeCreateRequest(
+                        repositoryDetail,
+                        artifactInfo,
+                        userId,
+                        file,
+                        expires,
+                        overwrite,
+                        metadata
+                    )
+                val nodeDetail = storageManager
+                    .storeArtifactFile(nodeCreateRequest, file, repositoryDetail.storageCredentials)
+                val event = SseEmitter.event().name(PATCH_EVENT_TYPE_DATA)
+                    .data(nodeDetail, MediaType.APPLICATION_JSON)
+                emitter.send(event)
+                emitter.complete()
+            } catch (e: ErrorCodeException) {
+                val msg = SseEmitter.event()
+                    .name(PATCH_EVENT_TYPE_ERROR)
+                    .data(e.message.orEmpty())
+                emitter.send(msg)
+                emitter.complete()
+            } catch (e: Exception) {
+                emitter.completeWithError(e)
+            }
+        }
+    }
+
+    /**
      * 上报进度
-     * @param clientError 记录是否发生客户端错误
-     * @param hasCompleted 记录是否完成
-     * @param inputStream 可计数的输入流
-     * @param contentLength 增量文件总长度
-     * @param emitter 发送器
      * */
     private fun reportProcess(
-        clientError: AtomicBoolean,
-        hasCompleted: AtomicBoolean,
-        inputStream: CounterInputStream,
-        contentLength: Long,
-        emitter: SseEmitter
+        patchContext: PatchContext
     ) {
-        try {
-            if (clientError.get()) {
-                logger.info("Client has error,ignore report process.")
-                return
+        with(patchContext) {
+            try {
+                if (clientError.get()) {
+                    logger.info("Client has error,ignore report process.")
+                    return
+                }
+                if (!hasCompleted.get()) {
+                    val process = String.format("%.2f", (counterInputStream.count.toFloat() / contentLength) * 100)
+                    val msg = "Current process $process%."
+                    val event = SseEmitter.event().name(PATCH_EVENT_TYPE_INFO).data(msg, MediaType.TEXT_PLAIN)
+                    emitter.send(event)
+                    logger.info(msg)
+                } else {
+                    logger.info("Patch has already completed.")
+                }
+            } catch (e: IOException) {
+                if (IOExceptionUtils.isClientBroken(e)) {
+                    clientError.set(true)
+                    return
+                }
+                logger.error("Send sse event failed.", e)
             }
-            if (!hasCompleted.get()) {
-                val process = String.format("%.2f", (inputStream.count.toFloat() / contentLength) * 100)
-                val msg = "Current process $process%."
-                val event = SseEmitter.event().name(PATCH_EVENT_TYPE_INFO).data(msg, MediaType.TEXT_PLAIN)
-                emitter.send(event)
-                logger.info(msg)
-            } else {
-                logger.info("Patch has already completed.")
-            }
-        } catch (e: IOException) {
-            if (IOExceptionUtils.isClientBroken(e)) {
-                clientError.set(true)
-                return
-            }
-            logger.error("Send sse event failed.", e)
         }
     }
 
@@ -284,6 +291,39 @@ class DeltaSyncService(
             overwrite = overwrite,
             metadata = metadata
         )
+    }
+
+    /**
+     * 构建Patch上下文
+     * @param emitter sse发送器
+     * @param context 当前上下文
+     * @param blockInputStream 增量同步使用的块输入流
+     * */
+    private fun buildPatchContext(
+        counterInputStream: CounterInputStream,
+        emitter: SseEmitter,
+        context: ArtifactContext,
+        blockInputStream: BlockInputStream
+    ): PatchContext {
+        with(context) {
+            val repository = ArtifactContextHolder.getRepository(RepositoryCategory.LOCAL) as GenericLocalRepository
+            val file = ArtifactFileFactory.buildBkSync(blockInputStream, counterInputStream, blockSize)
+            return PatchContext(
+                uploadSha256 = HeaderUtils.getHeader(HEADER_SHA256),
+                uploadMd5 = HeaderUtils.getHeader(HEADER_MD5),
+                expires = HeaderUtils.getLongHeader(HEADER_EXPIRES),
+                metadata = repository.resolveMetadata(request),
+                contentLength = request.contentLengthLong,
+                emitter = emitter,
+                counterInputStream = counterInputStream,
+                overwrite = HeaderUtils.getBooleanHeader(HEADER_OVERWRITE),
+                artifactInfo = artifactInfo,
+                repositoryDetail = repositoryDetail,
+                file = file,
+                blockInputStream = blockInputStream,
+                userId = SecurityUtils.getUserId()
+            )
+        }
     }
 
     /**
@@ -375,7 +415,29 @@ class DeltaSyncService(
                 }
             }
         }
+
+        override fun close() {
+            inputStream.close()
+        }
     }
+
+    private data class PatchContext(
+        val hasCompleted: AtomicBoolean = AtomicBoolean(false),
+        val clientError: AtomicBoolean = AtomicBoolean(false),
+        val contentLength: Long,
+        val counterInputStream: CounterInputStream,
+        val emitter: SseEmitter,
+        val uploadSha256: String?,
+        val uploadMd5: String?,
+        val expires: Long,
+        val metadata: Map<String, String>,
+        val repositoryDetail: RepositoryDetail,
+        val userId: String,
+        val artifactInfo: ArtifactInfo,
+        val file: ArtifactFile,
+        val overwrite: Boolean,
+        val blockInputStream: BlockInputStream
+    )
 
     companion object {
         private val logger = LoggerFactory.getLogger(DeltaSyncService::class.java)
@@ -385,6 +447,6 @@ class DeltaSyncService(
         private const val PATCH_EVENT_TYPE_DATA = "DATA"
 
         // 3s patch 回复心跳时间，保持连接存活
-        private const val HEART_BEAT = 3000L
+        private const val HEART_BEAT_INTERVAL = 3000L
     }
 }
