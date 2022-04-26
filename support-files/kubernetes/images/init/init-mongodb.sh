ed505bd87e0a56a520307d40f1597b8bd083ae5d
owenlxu <owenlxu@tencent.com>
2022-04-26 20:13:12 +0800

    --story=873762101 feat: 新增加bcs账号配置

--- support-files/kubernetes/images/init/init-mongodb.sh
+++ support-files/kubernetes/images/init/init-mongodb.sh
@@ -11,7 +11,7 @@ bcs_token=$BK_REPO_BCS_TOKEN
 bcs_access_key=$BK_REPO_BCS_ACCESSKEY
 bcs_secret_key=$BK_REPO_BCS_SECRETKEY
 
-if [ $bcs_token -ne "" ]; then
+if [ "$bcs_token" -ne "" ]; then
     sed -i "s/609f9939e6944c5c8a842d88acf85edc/$bcs_access_key/g" init-data-ext.js
     sed -i "s/e041dd34cd89466648a9b196150f75/$bcs_secret_key/g" init-data-ext.js
     mongo $BK_REPO_MONGODB_URI init-data-ext.js
