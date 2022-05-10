81f80346fe191b24ccabecb16b23a9e2a634c81f
陈炯明 <eugene@canway.net>
2022-05-10 14:06:10 +0800

    --story=97376229 feature: 制品定时清理功能（代码同步，入口已屏蔽）

--- src/frontend/devops-repository/src/views/repoConfig/index.vue
+++ src/frontend/devops-repository/src/views/repoConfig/index.vue
@@ -80,6 +80,9 @@
             <bk-tab-panel render-directive="if" v-if="showProxyConfigTab" name="proxyConfig" :label="$t('proxyConfig')">
                 <proxy-config :base-data="repoBaseInfo" @refresh="getRepoInfoHandler"></proxy-config>
             </bk-tab-panel>
+            <!-- <bk-tab-panel name="cleanConfig" label="清理设置">
+                <clean-config :base-data="repoBaseInfo" @refresh="getRepoInfoHandler"></clean-config>
+            </bk-tab-panel> -->
             <!-- <bk-tab-panel render-directive="if" name="permissionConfig" :label="$t('permissionConfig')">
                 <permission-config></permission-config>
             </bk-tab-panel> -->
@@ -89,11 +92,16 @@
 <script>
     import CardRadioGroup from '@repository/components/CardRadioGroup'
     import proxyConfig from '@repository/views/repoConfig/proxyConfig'
+    // import cleanConfig from '@repository/views/repoConfig/cleanConfig'
     // import permissionConfig from './permissionConfig'
     import { mapState, mapActions } from 'vuex'
     export default {
         name: 'repoConfig',
-        components: { CardRadioGroup, proxyConfig },
+        components: {
+            CardRadioGroup,
+            proxyConfig
+            // cleanConfig
+        },
         data () {
             const filenameRule = [
                 {
