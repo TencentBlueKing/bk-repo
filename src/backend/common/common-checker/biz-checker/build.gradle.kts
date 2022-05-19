67b4f9def7763a4fe38cc919c113e92dbdde58e2
carroll <790359300@qq.com>
2022-05-19 09:23:39 +0800

    --story=09899823 制品扫描dependency-check版本回退

--- src/backend/common/common-checker/biz-checker/build.gradle.kts
+++ src/backend/common/common-checker/biz-checker/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api(project(":common:common-checker:api-checker"))
-    api("net.canway:dependency-check-simple:0.2.6") {
+    api("net.canway:dependency-check-simple:0.2.3") {
         exclude(group = "junit", module = "junit")
     }
 }
