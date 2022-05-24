426d6571bae61d0c10cd83871a6594fd1a448dc7
felixncheng <felixncheng@tencent.com>
2022-05-24 10:34:12 +0800

    --story=874447531 代码检查格式修改

--- src/backend/common/common-storage/storage-service/src/main/kotlin/com/tencent/bkrepo/common/storage/innercos/response/handler/PutObjectResponseHandler.kt
+++ src/backend/common/common-storage/storage-service/src/main/kotlin/com/tencent/bkrepo/common/storage/innercos/response/handler/PutObjectResponseHandler.kt
@@ -36,7 +36,7 @@ import com.tencent.bkrepo.common.storage.innercos.http.HttpResponseHandler
 import com.tencent.bkrepo.common.storage.innercos.response.PutObjectResponse
 import okhttp3.Response
 
-class PutObjectResponseHandler() : HttpResponseHandler<PutObjectResponse>() {
+class PutObjectResponseHandler : HttpResponseHandler<PutObjectResponse>() {
     override fun handle(response: Response): PutObjectResponse {
         val eTag = response.header(Headers.ETAG)!!.trim('"')
         return PutObjectResponse(eTag)
