a6ca58dc01874868c8bc2717863fd3b40b9c48fd
felixncheng <felixncheng@tencent.com>
2022-06-14 15:01:13 +0800

    --story=874241967 分片下载，通知增加通知最后一块内容。

--- src/backend/common/common-api/src/main/kotlin/com/tencent/bkrepo/common/api/stream/ChunkedFutureInputStream.kt
+++ src/backend/common/common-api/src/main/kotlin/com/tencent/bkrepo/common/api/stream/ChunkedFutureInputStream.kt
@@ -28,22 +28,26 @@ class ChunkedFutureInputStream<T>(
 
     override fun read(): Int {
         val read = cursor.read()
-        if (read == -1 && iterator.hasNext()) {
+        if (read == -1) {
             cursor.close()
             notify(currentFuture)
-            move2next()
-            return cursor.read()
+            if (iterator.hasNext()) {
+                move2next()
+                return cursor.read()
+            }
         }
         return read
     }
 
     override fun read(b: ByteArray, off: Int, len: Int): Int {
         val read = cursor.read(b, off, len)
-        if (read == -1 && iterator.hasNext()) {
+        if (read == -1) {
             cursor.close()
             notify(currentFuture)
-            move2next()
-            return cursor.read(b, off, len)
+            if (iterator.hasNext()) {
+                move2next()
+                return cursor.read(b, off, len)
+            }
         }
         return read
     }
