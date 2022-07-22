f5be9f95f54cfcb4a1eebc7286ba362fda036fcb
owenlxu <owenlxu@tencent.com>
2022-07-22 18:30:58 +0800

    --story=876042975 bkrepo对接蓝鲸用户管理

--- src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/pojo/enums/UserSource.kt
+++ src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/pojo/enums/UserSource.kt
@@ -3,5 +3,5 @@ package com.tencent.bkrepo.auth.pojo.enums;
 enum class UserSource(val value: String) {
     PIPELINE("pipeline"),
     PAAS("paas"),
-    REPO("repo")
+    REPO("repo");
 }
--- src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/service/bkiam/BkiamPermissionServiceImpl.kt
+++ src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/service/bkiam/BkiamPermissionServiceImpl.kt
@@ -41,8 +41,6 @@ import com.tencent.bkrepo.auth.repository.RoleRepository
 import com.tencent.bkrepo.auth.repository.UserRepository
 import com.tencent.bkrepo.auth.service.local.PermissionServiceImpl
 import com.tencent.bkrepo.common.api.constant.StringPool
-import com.tencent.bkrepo.repository.api.ProjectClient
-import com.tencent.bkrepo.repository.api.RepositoryClient
 import org.slf4j.LoggerFactory
 import org.springframework.data.mongodb.core.MongoTemplate
 
