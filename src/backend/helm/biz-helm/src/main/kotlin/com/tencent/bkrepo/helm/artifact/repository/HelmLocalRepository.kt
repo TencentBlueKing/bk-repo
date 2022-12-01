cc419178fef65f12f625470493eb7a067795fb08
yaoxuwan <yaoxuwan@tencent.com>
2022-12-01 11:12:57 +0800

    fix: 下载sign文件错误 #101

--- src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/artifact/repository/HelmLocalRepository.kt
+++ src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/artifact/repository/HelmLocalRepository.kt
@@ -169,7 +169,7 @@ class HelmLocalRepository(
 
     override fun onDownload(context: ArtifactDownloadContext): ArtifactResource? {
         val fullPath = context.getStringAttribute(FULL_PATH)!!
-        val node = ArtifactContextHolder.getNodeDetail(fullPath)
+        val node = ArtifactContextHolder.getNodeDetail(fullPath = fullPath)
         node?.let {
             node.metadata[NAME]?.let { context.putAttribute(NAME, it) }
             node.metadata[VERSION]?.let { context.putAttribute(VERSION, it) }
