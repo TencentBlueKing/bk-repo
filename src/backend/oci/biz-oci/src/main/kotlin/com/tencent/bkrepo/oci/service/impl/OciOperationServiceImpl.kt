4d1d122b33a28a3b335d3fb67a93aae1560b3536
owenlxu <owenlxu@tencent.com>
2023-02-13 22:15:48 +0800

    fix: oci增加强制协议访问 #211

--- src/backend/oci/biz-oci/src/main/kotlin/com/tencent/bkrepo/oci/service/impl/OciOperationServiceImpl.kt
+++ src/backend/oci/biz-oci/src/main/kotlin/com/tencent/bkrepo/oci/service/impl/OciOperationServiceImpl.kt
@@ -905,6 +905,7 @@ class OciOperationServiceImpl(
     }
 
     override fun getReturnDomain(request: HttpServletRequest): String {
+        logger.info("oci ociProperties %s",ociProperties.toString())
         return OciResponseUtils.getResponseURI(
             request = request,
             enableHttps = ociProperties.https
