5690e8ee9be2facb0fa85275d7e50e564f02f45b
yaoxuwan <yaoxuwan@tencent.com>
2023-03-07 21:53:26 +0800

    feat:增加Commit Edge组网方式 #224

--- src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/interceptor/EdgeNodeUploadInterceptor.kt
+++ src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/interceptor/EdgeNodeUploadInterceptor.kt
@@ -1,6 +1,7 @@
 package com.tencent.bkrepo.common.artifact.interceptor
 
 import com.tencent.bkrepo.common.api.constant.HttpStatus
+import com.tencent.bkrepo.common.api.pojo.ClusterArchitecture
 import com.tencent.bkrepo.common.api.pojo.ClusterNodeType
 import com.tencent.bkrepo.common.api.util.JsonUtils
 import com.tencent.bkrepo.common.artifact.pojo.RepositoryType
@@ -17,8 +18,8 @@ import javax.servlet.http.HttpServletResponse
  * */
 class EdgeNodeUploadInterceptor(private val clusterProperties: ClusterProperties) : HandlerInterceptor {
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        // edge节点只允许非具体仓库服务和get、head请求通过
-        if (clusterProperties.role == ClusterNodeType.EDGE && isRegistryUploadRequest(request)) {
+        // proxy组网edge节点只允许非具体仓库服务和get、head请求通过
+        if (isProxyEdge() && isRegistryUploadRequest(request)) {
             response.status = HttpStatus.BAD_REQUEST.value
             val ret = ResponseBuilder.fail(HttpStatus.BAD_REQUEST.value, "Deployment on edge node is not support.")
             val retStr = JsonUtils.objectMapper.writeValueAsString(ret)
@@ -27,6 +28,9 @@ class EdgeNodeUploadInterceptor(private val clusterProperties: ClusterProperties
         }
         return super.preHandle(request, response, handler)
     }
+
+    private fun isProxyEdge() = clusterProperties.role == ClusterNodeType.EDGE &&
+        clusterProperties.architecture == ClusterArchitecture.PROXY
 
     /**
      * 判断是否为依赖源的上传请求。
