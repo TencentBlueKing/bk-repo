d148218187af522d91d9603ceb20651f646a5d8c
kunlongli <kunlongli@tencent.com>
2023-03-08 18:03:18 +0800

    feat: 支持Maven包通过CommitEdge同步 #224

--- src/backend/maven/biz-maven/src/main/kotlin/com/tencent/bkrepo/maven/dao/MavenMetadataDao.kt
+++ src/backend/maven/biz-maven/src/main/kotlin/com/tencent/bkrepo/maven/dao/MavenMetadataDao.kt
@@ -2,7 +2,56 @@ package com.tencent.bkrepo.maven.dao
 
 import com.tencent.bkrepo.common.mongo.dao.simple.SimpleMongoDao
 import com.tencent.bkrepo.maven.model.TMavenMetadataRecord
+import com.tencent.bkrepo.maven.pojo.metadata.MavenMetadataRequest
+import org.springframework.data.mongodb.core.FindAndModifyOptions
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
 import org.springframework.stereotype.Repository
 
 @Repository
-class MavenMetadataDao : SimpleMongoDao<TMavenMetadataRecord>()
+class MavenMetadataDao : SimpleMongoDao<TMavenMetadataRecord>() {
+
+    fun findAndModify(
+        request: MavenMetadataRequest,
+        incBuildNo: Boolean,
+        upsert: Boolean,
+        returnNew: Boolean
+    ): TMavenMetadataRecord? {
+        val query = Query(buildCriteria(request))
+        val options = FindAndModifyOptions().upsert(upsert).returnNew(returnNew)
+        val update = Update().set(TMavenMetadataRecord::timestamp.name, request.timestamp)
+        if (incBuildNo) {
+            update.inc(TMavenMetadataRecord::buildNo.name)
+        } else {
+            update.set(TMavenMetadataRecord::buildNo.name, request.buildNo ?: 0)
+        }
+
+        return determineMongoTemplate().findAndModify(
+            query, update, options, TMavenMetadataRecord::class.java
+        )
+    }
+
+    fun delete(request: MavenMetadataRequest) {
+        remove(Query(buildCriteria(request)))
+    }
+
+    private fun buildCriteria(request: MavenMetadataRequest): Criteria {
+        with(request) {
+            val criteria = Criteria
+                .where(TMavenMetadataRecord::projectId.name).`is`(projectId)
+                .and(TMavenMetadataRecord::repoName.name).`is`(repoName)
+                .and(TMavenMetadataRecord::groupId.name).`is`(groupId)
+                .and(TMavenMetadataRecord::artifactId.name).`is`(artifactId)
+                .and(TMavenMetadataRecord::version.name).`is`(version)
+            if (classifier == null) {
+                criteria.and(TMavenMetadataRecord::classifier.name).exists(false)
+            } else {
+                criteria.and(TMavenMetadataRecord::classifier.name).`is`(classifier)
+            }
+            extension?.let { criteria.and(TMavenMetadataRecord::extension.name).`is`(it) }
+            timestamp?.let { criteria.and(TMavenMetadataRecord::timestamp.name).`is`(it) }
+            return criteria
+        }
+    }
+}
