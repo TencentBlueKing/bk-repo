2d6173f1c705170f38f04b5641e9ba7d0a830327
yaoxuwan <yaoxuwan@tencent.com>
2023-03-20 16:46:22 +0800

    feat:增加Commit Edge组网方式 #224 插件创建仓库时，仅校验项目成员权限

--- src/backend/repository/api-repository/src/main/kotlin/com/tencent/bkrepo/repository/pojo/repo/UserRepoCreateRequest.kt
+++ src/backend/repository/api-repository/src/main/kotlin/com/tencent/bkrepo/repository/pojo/repo/UserRepoCreateRequest.kt
@@ -59,5 +59,7 @@ data class UserRepoCreateRequest(
     @ApiModelProperty("存储凭证key", required = false)
     val storageCredentialsKey: String? = null,
     @ApiModelProperty("仓库配额", required = false)
-    val quota: Long? = null
+    val quota: Long? = null,
+    @ApiModelProperty("来自插件的请求", required = false)
+    val pluginRequest: Boolean = false
 ) : RepoRequest
