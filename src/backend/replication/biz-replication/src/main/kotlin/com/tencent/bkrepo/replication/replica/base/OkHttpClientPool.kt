8a6202514ca7b30f7be3df89f9de120ab7474416
zacYL <zacyanliu@tencent.com>
2023-03-23 20:48:59 +0800

    bugfix: 禁掉内部同步进度采集拦截器 TencentBlueKing#301

--- src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/replica/base/OkHttpClientPool.kt
+++ src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/replica/base/OkHttpClientPool.kt
@@ -31,7 +31,7 @@ object OkHttpClientPool {
                     it
                 )
             }
-            builder.addNetworkInterceptor(ProgressInterceptor())
+//            builder.addNetworkInterceptor(ProgressInterceptor())
             builder.build()
         }
     }
