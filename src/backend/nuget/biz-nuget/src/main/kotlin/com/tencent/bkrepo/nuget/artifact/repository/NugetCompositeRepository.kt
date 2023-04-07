53851c127740e29aa0aa41494e48b8d4b9d772b2
scorpion <scplsy@gmail.com>
2023-04-07 11:24:37 +0800

    feature: 新增NuGet组合仓库 #235

---
+++ src/backend/nuget/biz-nuget/src/main/kotlin/com/tencent/bkrepo/nuget/artifact/repository/NugetCompositeRepository.kt
@@ -0,0 +1,46 @@
+package com.tencent.bkrepo.nuget.artifact.repository
+
+import com.tencent.bkrepo.common.artifact.repository.composite.CompositeRepository
+import com.tencent.bkrepo.nuget.artifact.NugetArtifactInfo
+import com.tencent.bkrepo.nuget.pojo.artifact.NugetRegistrationArtifactInfo
+import com.tencent.bkrepo.repository.api.ProxyChannelClient
+import org.springframework.context.annotation.Primary
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+
+@Component
+@Primary
+class NugetCompositeRepository(
+    private val localRepository: NugetLocalRepository,
+    remoteRepository: NugetRemoteRepository,
+    proxyChannelClient: ProxyChannelClient
+) : CompositeRepository(localRepository, remoteRepository, proxyChannelClient), NugetRepository {
+
+    override fun feed(artifactInfo: NugetArtifactInfo): ResponseEntity<Any> {
+        return localRepository.feed(artifactInfo)
+    }
+
+    override fun registrationIndex(
+        artifactInfo: NugetRegistrationArtifactInfo,
+        registrationPath: String,
+        isSemver2Endpoint: Boolean
+    ): ResponseEntity<Any> {
+        return localRepository.registrationIndex(artifactInfo, registrationPath, isSemver2Endpoint)
+    }
+
+    override fun registrationPage(
+        artifactInfo: NugetRegistrationArtifactInfo,
+        registrationPath: String,
+        isSemver2Endpoint: Boolean
+    ): ResponseEntity<Any> {
+        return localRepository.registrationPage(artifactInfo, registrationPath, isSemver2Endpoint)
+    }
+
+    override fun registrationLeaf(
+        artifactInfo: NugetRegistrationArtifactInfo,
+        registrationPath: String,
+        isSemver2Endpoint: Boolean
+    ): ResponseEntity<Any> {
+        return localRepository.registrationLeaf(artifactInfo, registrationPath, isSemver2Endpoint)
+    }
+}
