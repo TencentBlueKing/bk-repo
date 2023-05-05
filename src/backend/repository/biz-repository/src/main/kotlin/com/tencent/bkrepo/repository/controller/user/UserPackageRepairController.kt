424066c4c48b3e9b79fbc703152d95863ba3f669
ericwu <ericwu@canway.net>
2023-05-05 12:25:08 +0000

    feature: 提供接口重新计算并修正所有制品包的版本数量 #459
    (cherry picked from commit d3ef7b9f0a9af21cbae3456150b5f54f2edf9b3b)

--- src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/controller/user/UserPackageRepairController.kt
+++ src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/controller/user/UserPackageRepairController.kt
@@ -5,6 +5,7 @@ import com.tencent.bkrepo.common.service.util.ResponseBuilder
 import com.tencent.bkrepo.repository.service.packages.PackageRepairService
 import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,6 +17,13 @@ class UserPackageRepairController(
     @GetMapping("/version/history/repair")
     fun repairHistoryVersion(): Response<Void> {
         packageRepairService.repairHistoryVersion()
+        return ResponseBuilder.success()
+    }
+
+    @ApiOperation("修正包的版本数")
+    @PutMapping("/package/version/recount")
+    fun repairVersionCount(): Response<Void> {
+        packageRepairService.repairVersionCount()
         return ResponseBuilder.success()
     }
 }
