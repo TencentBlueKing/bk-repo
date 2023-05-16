157f4a6df621c93a9a8f4266be12103a6bc16ba0
kunlongli <kunlongli@tencent.com>
2023-05-16 18:17:51 +0800

    feat: 设置执行Node遍历统计任务时仅查询部分字段 #559

--- src/backend/job/biz-job/src/main/kotlin/com/tencent/bkrepo/job/batch/node/NodeStatCompositeMongoDbBatchJob.kt
+++ src/backend/job/biz-job/src/main/kotlin/com/tencent/bkrepo/job/batch/node/NodeStatCompositeMongoDbBatchJob.kt
@@ -34,16 +34,46 @@ class NodeStatCompositeMongoDbBatchJob(
     }
 
     data class Node(private val map: Map<String, Any?>) {
-        private val defaultMap = map.withDefault { null }
+        // 需要通过@JvmField注解将Kotlin backing-field直接作为Java field使用，MongoDbBatchJob中才能解析出需要查询的字段
+        @JvmField
+        val id: String
 
-        val id: String by defaultMap
-        val folder: Boolean by defaultMap
-        val path: String by defaultMap
-        val fullPath: String by defaultMap
-        val name: String by defaultMap
-        val size: Long by defaultMap
-        val deleted: Date? by defaultMap
-        val projectId: String by defaultMap
-        val repoName: String by defaultMap
+        @JvmField
+        val folder: Boolean
+
+        @JvmField
+        val path: String
+
+        @JvmField
+        val fullPath: String
+
+        @JvmField
+        val name: String
+
+        @JvmField
+        val size: Long
+
+        @JvmField
+        val deleted: Date?
+
+        @JvmField
+        val projectId: String
+
+        @JvmField
+        val repoName: String
+
+        init {
+            id = map[Node::id.name] as String
+            folder = map[Node::folder.name] as Boolean
+            path = map[Node::path.name] as String
+            fullPath = map[Node::fullPath.name] as String
+            name = map[Node::name.name] as String
+            size = map[Node::size.name] as Long
+            // 查询出的deleted默认为Date类型
+            deleted = map[Node::deleted.name] as Date?
+            projectId = map[Node::projectId.name] as String
+            repoName = map[Node::repoName.name] as String
+        }
+
     }
 }
