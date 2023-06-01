35e8583342e98e1bbf0307efc04bcc688a373b0b
zacYL <100330102+zacYL@users.noreply.github.com>
2023-06-01 10:32:25 +0800

    feat: 打印任务具体执行详情 #706 (#707)
    * feat: 打印任务具体执行详情 #706
    
    * feat: 代码检查问题修复 #706

--- src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/replica/base/AbstractReplicaService.kt
+++ src/backend/replication/biz-replication/src/main/kotlin/com/tencent/bkrepo/replication/replica/base/AbstractReplicaService.kt
@@ -36,7 +36,6 @@ import com.tencent.bkrepo.replication.pojo.metrics.ReplicationRecord
 import com.tencent.bkrepo.replication.pojo.record.ExecutionResult
 import com.tencent.bkrepo.replication.pojo.record.ExecutionStatus
 import com.tencent.bkrepo.replication.pojo.record.request.RecordDetailInitialRequest
-import com.tencent.bkrepo.replication.pojo.request.ReplicaType
 import com.tencent.bkrepo.replication.pojo.task.ReplicaTaskInfo
 import com.tencent.bkrepo.replication.pojo.task.objects.PackageConstraint
 import com.tencent.bkrepo.replication.pojo.task.objects.PathConstraint
@@ -319,7 +318,6 @@ abstract class AbstractReplicaService(
         errorReason: String? = null,
         record: ReplicationRecord
     ) {
-        if (task.replicaType != ReplicaType.RUN_ONCE) return
         logger.info(
             toJson(
                 convertToReplicationRecordDetailMetricsRecord(
