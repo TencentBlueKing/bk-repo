a85e4109425de51054ae62ab5a28fe8684b25849
gujunling <gujunq1998@163.com>
2023-06-15 11:55:54 +0800

    fix: 创建虚拟仓库时选择的存储库传参需要添加projectId，解决历史数据可以跨项目选择仓库的兼容性问题

--- src/frontend/devops-repository/src/views/repoList/createRepoDialog.vue
+++ src/frontend/devops-repository/src/views/repoList/createRepoDialog.vue
@@ -583,7 +583,8 @@
                     body.configuration.repositoryList = this.repoBaseInfo.virtualStoreList.map(item => {
                         return {
                             name: item.name,
-                            category: item.category
+                            category: item.category,
+                            projectId: item.projectId
                         }
                     })
                     body.configuration.deploymentRepo = this.repoBaseInfo.deploymentRepo || ''
