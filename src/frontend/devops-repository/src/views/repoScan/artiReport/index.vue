eb2084babf27b17cf477465ea2ec38d8e8bd9e5b
gujunling <gujunq1998@163.com>
2023-06-21 14:49:22 +0800

    fix: 制品分析的扫描记录详情页在页面大小发生变化时页面样式异常 #837

--- src/frontend/devops-repository/src/views/repoScan/artiReport/index.vue
+++ src/frontend/devops-repository/src/views/repoScan/artiReport/index.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="container">
         <artifact-info :scan-types="scanTypes" :subtask-overview="subtaskOverview"></artifact-info>
-        <bk-tab type="unborder-card" style="width: 100%;height: 100%">
+        <bk-tab type="unborder-card" class="arti-tab">
             <bk-tab-panel v-for="(panel, index) in panels" v-bind="panel" :key="index" style="height: 100%">
                 <component
                     @rescan="rescan"
                     :subtask-overview="subtaskOverview"
                     :project-id="projectId"
                     :view-type="viewType"
-                    :is="panel.component" style="height: 100%"></component>
+                    :is="panel.component"
+                    style="height: 100%">
+                </component>
             </bk-tab-panel>
         </bk-tab>
     </div>
@@ -134,6 +136,10 @@
     ::v-deep .bk-tab-section {
         height: 100%;
         padding-right: 0;
+    }
+    .arti-tab{
+        width: calc(100% - 334px);
+        height: 100%;
     }
 }
 </style>
