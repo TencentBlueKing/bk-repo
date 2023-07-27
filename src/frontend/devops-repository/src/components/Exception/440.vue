c88cf4379e0c1f2571496c6a1b21bd2dd0b64635
v_wuqdwu <v_wuqdwu@tencent.com>
2023-07-27 17:24:05 +0800

    feat:440页面提示优化  #1008

--- src/frontend/devops-repository/src/components/Exception/440.vue
+++ src/frontend/devops-repository/src/components/Exception/440.vue
@@ -2,7 +2,7 @@
     <div class="empty-data-container flex-center" :class="{ 'hidden': isLoading }" style="background-color: white">
         <div class="flex-column flex-center">
             <img width="480" height="240" style="float: left;margin-right: 3px" src="/ui/440.svg" />
-            <span class="mt5 empty-data-title">{{ $t('noBusinessTip') }}</span>
+            <span class="mt5 empty-data-title">{{ $t('noBusinessSubTip') }}</span>
             <span class="mt5 empty-data-subtitle">{{ $route.params.msg }}</span>
         </div>
     </div>
