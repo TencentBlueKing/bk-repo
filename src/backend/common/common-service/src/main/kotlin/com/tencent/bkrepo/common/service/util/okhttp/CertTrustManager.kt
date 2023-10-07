d9dabeb94023d72ba0f4f9562a58e4d06fd13e0e
zacYL <zacyanliu@tencent.com>
2023-10-07 18:19:19 +0800

    bug: 当证书过期或者无效时默认信任所有证书 #1216

--- src/backend/common/common-service/src/main/kotlin/com/tencent/bkrepo/common/service/util/okhttp/CertTrustManager.kt
+++ src/backend/common/common-service/src/main/kotlin/com/tencent/bkrepo/common/service/util/okhttp/CertTrustManager.kt
@@ -82,6 +82,14 @@ object CertTrustManager {
         check(trustManagers.size == 1) { "Unexpected default trust managers size: ${trustManagers.size}" }
         val firstTrustManager = trustManagers.first()
         check(firstTrustManager is X509TrustManager) { "Unexpected default trust managers:$firstTrustManager" }
-        return firstTrustManager
+        return try {
+            // 校验传入的证书是否过期或者无效
+            firstTrustManager.acceptedIssuers.forEach {
+                it.checkValidity()
+            }
+            firstTrustManager
+        } catch (e: Exception) {
+            disableValidationTrustManager
+        }
     }
 }
