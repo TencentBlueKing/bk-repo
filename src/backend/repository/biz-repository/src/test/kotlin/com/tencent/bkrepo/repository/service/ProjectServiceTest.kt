3e51c06406c64cf58369a31a7fe3ffc07135649b
kunlongli <kunlongli@tencent.com>
2023-10-10 14:32:55 +0800

    feat: 增加项目元数据字段 #1055

--- src/backend/repository/biz-repository/src/test/kotlin/com/tencent/bkrepo/repository/service/ProjectServiceTest.kt
+++ src/backend/repository/biz-repository/src/test/kotlin/com/tencent/bkrepo/repository/service/ProjectServiceTest.kt
@@ -38,7 +38,10 @@ import com.tencent.bkrepo.repository.UT_PROJECT_ID
 import com.tencent.bkrepo.repository.UT_USER
 import com.tencent.bkrepo.repository.dao.ProjectDao
 import com.tencent.bkrepo.repository.pojo.project.ProjectCreateRequest
+import com.tencent.bkrepo.repository.pojo.project.ProjectMetadata
+import com.tencent.bkrepo.repository.pojo.project.ProjectUpdateRequest
 import com.tencent.bkrepo.repository.service.repo.ProjectService
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
@@ -72,6 +75,43 @@ class ProjectServiceTest @Autowired constructor(
     fun `test create project`() {
         val request = ProjectCreateRequest(UT_PROJECT_ID, UT_PROJECT_DISPLAY, UT_PROJECT_DESC, true, UT_USER)
         projectService.createProject(request)
+    }
+
+    @Test
+    @DisplayName("测试更新项目")
+    fun `test update project`() {
+        // create project
+        val metadata = ProjectMetadata(key = "k", value = "v")
+        val request = ProjectCreateRequest(
+            UT_PROJECT_ID,
+            UT_PROJECT_DISPLAY,
+            UT_PROJECT_DESC,
+            true,
+            UT_USER,
+            listOf(metadata)
+        )
+        val projectInfo = projectService.createProject(request)
+        Assertions.assertEquals(UT_PROJECT_ID, projectInfo.name)
+        Assertions.assertEquals(1, projectInfo.metadata.size)
+
+        // update project
+        val newDisplayName = "$UT_PROJECT_DISPLAY new"
+        val newDes = "$UT_PROJECT_DESC new"
+        val newMetadata = listOf(ProjectMetadata(key = "newKey", value = "newVal"))
+        val updateRequest = ProjectUpdateRequest(
+            displayName = newDisplayName,
+            description = newDes,
+            metadata = newMetadata,
+        )
+        projectService.updateProject(UT_PROJECT_ID, updateRequest)
+        val newProject = projectService.getProjectInfo(UT_PROJECT_ID)
+        Assertions.assertNotNull(newProject)
+        Assertions.assertEquals(newDisplayName, newProject!!.displayName)
+        Assertions.assertEquals(newDes, newProject.description)
+        Assertions.assertEquals(newMetadata.size, newProject.metadata.size)
+        for(i in newMetadata.indices) {
+            Assertions.assertEquals(newMetadata[i], newProject.metadata[i])
+        }
     }
 
     @Test
