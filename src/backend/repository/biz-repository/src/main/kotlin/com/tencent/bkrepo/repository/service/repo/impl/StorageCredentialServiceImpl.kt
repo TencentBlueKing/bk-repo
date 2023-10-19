a712144f918538ddd39ac27568f70ad612f70aca
yaoxuwan <yaoxuwan@tencent.com>
2023-10-19 14:54:06 +0800

    feat: 缓存文件过期时间配置改为Duration #1302

--- src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/service/repo/impl/StorageCredentialServiceImpl.kt
+++ src/backend/repository/biz-repository/src/main/kotlin/com/tencent/bkrepo/repository/service/repo/impl/StorageCredentialServiceImpl.kt
@@ -96,7 +96,7 @@ class StorageCredentialServiceImpl(
         storageCredentials.apply {
             cache = cache.copy(
                 loadCacheFirst = request.credentials.cache.loadCacheFirst,
-                expireDays = request.credentials.cache.expireDays
+                expireDuration = request.credentials.cache.expireDuration
             )
             upload = upload.copy(localPath = request.credentials.upload.localPath)
             credentialUpdaters[StorageCredentialsUpdater.name(this::class.java)]?.update(this, request)
