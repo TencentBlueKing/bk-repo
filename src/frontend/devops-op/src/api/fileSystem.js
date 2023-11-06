3a9fa09cbc23e39a35078c02cc473495a3334751
lannoy0523 <46735290+lannoy0523@users.noreply.github.com>
2023-11-06 16:07:27 +0800

    feat:admin增加文件系统客户端管理 #1357 (#1376)
    * feat:admin增加文件系统客户端管理 #1357
    
    * feat:admin增加文件系统客户端管理 #1357
    
    * feat:admin增加文件系统客户端管理 #1357

---
+++ src/frontend/devops-op/src/api/fileSystem.js
@@ -0,0 +1,17 @@
+import request from '@/utils/request'
+
+export const DEFAULT_PAGE_SIZE = 10
+const PREFIX = '/opdata/api/fs-client'
+
+export function queryFileSystemClient(projectId, repoName, pageNumber) {
+  return request({
+    url: `${PREFIX}/list/`,
+    method: 'get',
+    params: {
+      pageNumber: pageNumber,
+      pageSize: DEFAULT_PAGE_SIZE,
+      projectId: projectId,
+      repoName: repoName
+    }
+  })
+}
