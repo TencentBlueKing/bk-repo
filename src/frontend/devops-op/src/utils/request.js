1106e796607373b54ed82471ae19cce00228636f
lannoy0523 <46735290+lannoy0523@users.noreply.github.com>
2023-11-20 14:38:46 +0800

    bug:opdata 仓库大小统计页面问题修复 #1455 (#1459)

--- src/frontend/devops-op/src/utils/request.js
+++ src/frontend/devops-op/src/utils/request.js
@@ -6,7 +6,7 @@ import store from '@/store'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  timeout: 1000 * 60 * 5 // request timeout
 })
 
 // request interceptor
