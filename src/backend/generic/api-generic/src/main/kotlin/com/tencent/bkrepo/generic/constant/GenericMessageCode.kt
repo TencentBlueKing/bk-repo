b29c403b0b0b151b735781e356ab49e7e37ece30
kunlongli <16629885+cnlkl@users.noreply.github.com>
2023-11-20 14:41:31 +0800

    feat: 支持Generic Remote仓库下载 #1400 (#1406)
    * feat: 支持Generic Remote仓库下载 #1400
    
    * feat: 支持Generic Remote仓库使用平台账号认证 #1400
    
    * feat: 支持搜索Generic Remote仓库 #1400

--- src/backend/generic/api-generic/src/main/kotlin/com/tencent/bkrepo/generic/constant/GenericMessageCode.kt
+++ src/backend/generic/api-generic/src/main/kotlin/com/tencent/bkrepo/generic/constant/GenericMessageCode.kt
@@ -41,7 +41,8 @@ enum class GenericMessageCode(private val businessCode: Int, private val key: St
     LIST_DIR_NOT_ALLOWED(2, "generic.dir.not-allowed"),
     SIGN_FILE_NOT_FOUND(3, "generic.delta.sign-file.notfound"),
     NODE_DATA_HAS_CHANGED(4, "generic.node.data.has.changed"),
-    DOWNLOAD_DIR_NOT_ALLOWED(5, "generic.download.dir.not-allowed");
+    DOWNLOAD_DIR_NOT_ALLOWED(5, "generic.download.dir.not-allowed"),
+    ARTIFACT_SEARCH_FAILED(6, "generic.artifact.query.failed");
 
     override fun getBusinessCode() = businessCode
     override fun getKey() = key
