cc1838e1f4450c196790f5f441072b8b2560a7d4
zacYL <100330102+zacYL@users.noreply.github.com>
2023-11-30 15:43:54 +0800

    feat: 修复 mongotemplate 加载报错 #1509 (#1510)

--- src/backend/job/biz-job/src/main/kotlin/com/tencent/bkrepo/job/batch/node/FolderStatChildJob.kt
+++ src/backend/job/biz-job/src/main/kotlin/com/tencent/bkrepo/job/batch/node/FolderStatChildJob.kt
@@ -47,6 +47,7 @@ import com.tencent.bkrepo.job.config.properties.CompositeJobProperties
 import com.tencent.bkrepo.job.config.properties.NodeStatCompositeMongoDbBatchJobProperties
 import com.tencent.bkrepo.job.pojo.FolderInfo
 import org.springframework.data.mongodb.core.BulkOperations.BulkMode
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Update
@@ -65,8 +66,9 @@ import kotlin.text.toLongOrNull as toLongOrNull1
  */
 class FolderStatChildJob(
     val properties: CompositeJobProperties,
-    private val redisTemplate: RedisTemplate<String, String>
-) : ChildMongoDbBatchJob<NodeStatCompositeMongoDbBatchJob.Node>(properties) {
+    private val mongoTemplate: MongoTemplate,
+    private val redisTemplate: RedisTemplate<String, String>,
+    ) : ChildMongoDbBatchJob<NodeStatCompositeMongoDbBatchJob.Node>() {
 
     override fun onParentJobStart(context: ChildJobContext) {
         require(context is FolderChildContext)
