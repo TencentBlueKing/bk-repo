825b330522bcba116644b73e164cba39ceae3d9f
谷俊崎[junqigu] <junqigu@canway.net>
2024-01-25 16:03:32 +0800

    feat: 分发计划执行日志弹窗相关界面展示优化 #1675

--- src/frontend/devops-repository/src/views/planManage/planLog.vue
+++ src/frontend/devops-repository/src/views/planManage/planLog.vue
@@ -1,57 +1,75 @@
 <template>
-    <bk-sideslider :is-show.sync="showSideslider" :quick-close="true" :width="850" :title="$t('planLogTitle', { name: planData.name })">
-        <template #content>
-            <div class="plan-detail-container" v-bkloading="{ isLoading }">
-                <bk-radio-group
-                    v-if="planData.replicaType !== 'REAL_TIME'"
-                    class="mt10 pr20"
-                    style="text-align: right;"
-                    v-model="status"
-                    @change="handlerPaginationChange()">
-                    <bk-radio class="ml50" value="">{{ $t('total') }}</bk-radio>
-                    <bk-radio class="ml50" value="SUCCESS">{{ $t('asyncPlanStatusEnum.SUCCESS') }}</bk-radio>
-                    <bk-radio class="ml50" value="FAILED">{{ $t('asyncPlanStatusEnum.FAILED') }}</bk-radio>
-                </bk-radio-group>
-                <bk-table
-                    class="mt10"
-                    :height="planData.replicaType !== 'REAL_TIME' ? 'calc(100% - 90px)' : 'calc(100% - 60px)'"
-                    :data="logList"
-                    :outer-border="false"
-                    :row-border="false"
-                    :row-style="{ cursor: 'pointer' }"
-                    size="small"
-                    @row-click="showLogDetailHandler">
-                    <bk-table-column type="index" :label="$t('NO')" width="70"></bk-table-column>
-                    <bk-table-column :label="$t('runningStatus')" width="110">
-                        <template #default="{ row }">
-                            <span class="repo-tag" :class="row.status">{{$t(`asyncPlanStatusEnum.${row.status}`) || $t('notExecuted')}}</span>
-                        </template>
-                    </bk-table-column>
-                    <bk-table-column :label="$t('startExecutionTime')" width="150">
-                        <template #default="{ row }">{{formatDate(row.startTime)}}</template>
-                    </bk-table-column>
-                    <bk-table-column v-if="planData.replicaType !== 'REAL_TIME'" :label="$t('endExecutionTime')" width="150">
-                        <template #default="{ row }">{{formatDate(row.endTime)}}</template>
-                    </bk-table-column>
-                    <bk-table-column :label="$t('note')" show-overflow-tooltip>
-                        <template #default="{ row }">{{row.errorReason || '/'}}</template>
-                    </bk-table-column>
-                </bk-table>
-                <bk-pagination
-                    class="p10"
-                    size="small"
-                    align="right"
-                    show-total-count
-                    @change="current => handlerPaginationChange({ current })"
-                    @limit-change="limit => handlerPaginationChange({ limit })"
-                    :current.sync="pagination.current"
-                    :limit="pagination.limit"
-                    :count="pagination.count"
-                    :limit-list="pagination.limitList">
-                </bk-pagination>
-            </div>
-        </template>
-    </bk-sideslider>
+    <div>
+        <bk-sideslider :is-show.sync="showSideslider" :quick-close="true" :width="600" :title="$t('planLogTitle', { name: planData.name })">
+            <template #content>
+                <div class="plan-detail-container" v-bkloading="{ isLoading }">
+                    <bk-radio-group
+                        v-if="planData.replicaType !== 'REAL_TIME'"
+                        class="mt10 pr20"
+                        style="text-align: right;"
+                        v-model="status"
+                        @change="handlerPaginationChange()">
+                        <bk-radio class="ml50" value="">{{ $t('total') }}</bk-radio>
+                        <bk-radio class="ml50" value="SUCCESS">{{ $t('asyncPlanStatusEnum.SUCCESS') }}</bk-radio>
+                        <bk-radio class="ml50" value="FAILED">{{ $t('asyncPlanStatusEnum.FAILED') }}</bk-radio>
+                    </bk-radio-group>
+                    <bk-table
+                        class="mt10"
+                        :height="planData.replicaType !== 'REAL_TIME' ? 'calc(100% - 90px)' : 'calc(100% - 60px)'"
+                        :data="logList"
+                        :outer-border="false"
+                        :row-border="false"
+                        :row-style="{ cursor: 'pointer' }"
+                        size="small"
+                        :empty-text="$t('planLogEmptyTitle')"
+                        @row-click="showLogDetailHandler">
+                        <bk-table-column type="index" :label="$t('NO')" width="70"></bk-table-column>
+                        <bk-table-column :label="$t('runningStatus')" width="110">
+                            <template #default="{ row }">
+                                <span class="repo-tag" :class="row.status">{{$t(`asyncPlanStatusEnum.${row.status}`) || $t('notExecuted')}}</span>
+                            </template>
+                        </bk-table-column>
+                        <bk-table-column :label="$t('startExecutionTime')" width="150">
+                            <template #default="{ row }">{{formatDate(row.startTime)}}</template>
+                        </bk-table-column>
+                        <bk-table-column v-if="planData.replicaType !== 'REAL_TIME'" :label="$t('endExecutionTime')" width="150">
+                            <template #default="{ row }">{{formatDate(row.endTime)}}</template>
+                        </bk-table-column>
+                        <bk-table-column :label="$t('planLogErrorReason')" width="120">
+                            <template #default="{ row }">
+                                <bk-button v-if="row.errorReason" style="width:80%;" text theme="primary" @click.stop="openErrorDetail(row)">{{$t('view')}}</bk-button>
+                                <span v-else>/</span>
+                            </template>
+                        </bk-table-column>
+                    </bk-table>
+                    <bk-pagination
+                        class="p10"
+                        size="small"
+                        align="right"
+                        show-total-count
+                        @change="current => handlerPaginationChange({ current })"
+                        @limit-change="limit => handlerPaginationChange({ limit })"
+                        :current.sync="pagination.current"
+                        :limit="pagination.limit"
+                        :count="pagination.count"
+                        :limit-list="pagination.limitList">
+                    </bk-pagination>
+                </div>
+            </template>
+        </bk-sideslider>
+        <canway-dialog
+            v-model="errorDialog.show"
+            width="450"
+            height-num="380"
+            :title="$t('planLogErrorReason')"
+            :show-footer="false"
+            @cancel="closeErrorDialog">
+            <div class="error-dialog-info">{{errorDialog.reason}}</div>
+            <template #footer>
+                <bk-button @click="closeErrorDialog">{{$t('close')}}</bk-button>
+            </template>
+        </canway-dialog>
+    </div>
 </template>
 <script>
     import { formatDate } from '@repository/utils'
@@ -81,6 +99,10 @@
                     current: 1,
                     limit: 20,
                     limitList: [10, 20, 40]
+                },
+                errorDialog: {
+                    show: false,
+                    reason: ''
                 }
             }
         },
@@ -144,6 +166,20 @@
                         offsetY: 50
                     })
                 }
+            },
+            openErrorDetail (row) {
+                // 因为后端接口涉及改动较大，且不好实现，后端在分发异常的时候错误信息会始终携带指定字符串 “部分数据同步失败”，所以产品同学说前端针对这几个指定字符串的国际化做特殊处理
+                const errorReason = (this.currentLanguage === 'zh-cn' ? '部分数据同步失败' : 'Partial data synchronization failed') + row.errorReason.split('部分数据同步失败')?.[1] || ''
+                this.errorDialog = {
+                    show: true,
+                    reason: errorReason
+                }
+            },
+            closeErrorDialog () {
+                this.errorDialog = {
+                    show: false,
+                    reason: ''
+                }
             }
         }
     }
@@ -151,5 +187,10 @@
 <style lang="scss" scoped>
 .plan-detail-container {
     height: 100%;
+}
+.error-dialog-info{
+    max-height: 250px;
+    overflow-y: auto;
+    word-break: break-all;
 }
 </style>
