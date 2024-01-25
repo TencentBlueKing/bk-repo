0283c29ecd6703f73c1dfcfdd037f1a62045b7eb
谷俊崎[junqigu] <junqigu@canway.net>
2024-01-25 15:04:14 +0800

    feat: 制品分发的日志详情页中添加同步数量的展示并修改相关列表数据展示 #1675

--- src/frontend/devops-repository/src/views/planManage/logDetail.vue
+++ src/frontend/devops-repository/src/views/planManage/logDetail.vue
@@ -1,6 +1,22 @@
 <template>
     <main class="log-detail-container" v-bkloading="{ isLoading }">
         <div class="mr20 mt10 log-package-seach flex-align-center">
+            <div v-if="showOverviewCount" class="flex-align-center ml20" style="margin-right:auto;">
+                <div v-for="(value,key) in countList" :key="key" class="mr30 ">
+                    <span>
+                        {{$t(`planLogEnum.${key}`)}}
+                    </span>
+                    <span>: </span>
+                    <span class="log-count"
+                        v-bk-tooltips="{
+                            content: $t(`planLogEnum.${key}`) + ': ' + value,
+                            placements: ['top'],
+                            disabled: ((value + '').length || 0) < 6
+                        }">
+                        {{value}}
+                    </span>
+                </div>
+            </div>
             <bk-search-select
                 class="search-group"
                 clearable
@@ -40,28 +56,34 @@
                     <span class="ml5">{{ row.localRepoName }}</span>
                 </template>
             </bk-table-column>
+            <bk-table-column :label="$t('artifactName')" show-overflow-tooltip>
+                <template #default="{ row }">{{ row.artifactName || row.packageKey || row.path || '/' }}</template>
+            </bk-table-column>
+            <bk-table-column :label="$t('version')" show-overflow-tooltip>
+                <template #default="{ row }">{{ row.version || (row.versions || ['/']).join('、') }}</template>
+            </bk-table-column>
             <bk-table-column :label="$t('syncStatus')" width="90">
                 <template #default="{ row }">
                     <div class="status-sign" :class="row.status" :data-name="$t(`asyncPlanStatusEnum.${row.status}`) || $t('notExecuted')"></div>
                 </template>
             </bk-table-column>
-            <template v-if="logDetail.replicaType === 'REAL_TIME' || logDetail.replicaObjectType !== 'REPOSITORY'">
-                <bk-table-column :label="$t('artifactName') + '/' + $t('filePath')" show-overflow-tooltip>
-                    <template #default="{ row }">{{ row.packageKey || row.path || '/' }}</template>
-                </bk-table-column>
-                <bk-table-column :label="$t('version')" show-overflow-tooltip>
-                    <template #default="{ row }">{{ (row.versions || ['/']).join('、') }}</template>
-                </bk-table-column>
-            </template>
+            <bk-table-column :label="$t('conflictStrategy')" width="150">
+                <template #default="{ row }">
+                    <span>{{row.conflictStrategy ? $t(`conflictStrategyEnum.${row.conflictStrategy}`) : '/'}}</span>
+                </template>
+            </bk-table-column>
             <bk-table-column :label="$t('startTime')" width="150">
                 <template #default="{ row }">{{formatDate(row.startTime)}}</template>
             </bk-table-column>
             <bk-table-column :label="$t('endTime')" width="150">
                 <template #default="{ row }">{{formatDate(row.endTime)}}</template>
             </bk-table-column>
-            <bk-table-column :label="$t('successNum')" prop="success"></bk-table-column>
-            <bk-table-column :label="$t('skipNum')" prop="skip"></bk-table-column>
-            <bk-table-column :label="$t('failNum')" prop="failed"></bk-table-column>
+            <!-- 历史数据因为统计数量不存在，需要显示之前的成功数量等字段 -->
+            <template v-if="!showOverviewCount">
+                <bk-table-column :label="$t('successNum')" prop="success"></bk-table-column>
+                <bk-table-column :label="$t('skipNum')" prop="skip"></bk-table-column>
+                <bk-table-column :label="$t('failNum')" prop="failed"></bk-table-column>
+            </template>
             <bk-table-column :label="$t('note')" show-overflow-tooltip>
                 <template #default="{ row }">{{row.errorReason || '/'}}</template>
             </bk-table-column>
@@ -83,12 +105,13 @@
 <script>
     import { mapGetters, mapActions } from 'vuex'
     import { formatDate } from '@repository/utils'
-    import { asyncPlanStatusEnum } from '@repository/store/publicEnum'
+    import { asyncPlanStatusEnum, planLogEnum } from '@repository/store/publicEnum'
     export default {
         name: 'logDetail',
         data () {
             return {
                 asyncPlanStatusEnum,
+                planLogEnum,
                 isLoading: false,
                 logDetail: {},
                 pkgList: [],
@@ -99,7 +122,16 @@
                     current: 1,
                     limit: 20,
                     limitList: [10, 20, 40]
-                }
+                },
+                // 分发计划日志详情的同步数量
+                countList: {
+                    total: 0,
+                    success: 0,
+                    failed: 0,
+                    conflict: 0
+                },
+                // 是否需要显示日志详情的同步数量相关字段
+                showOverviewCount: false
             }
         },
         computed: {
@@ -111,8 +143,7 @@
                 return [
                     { name: this.$t('nodeName'), id: 'clusterName' },
                     { name: this.$t('repoName'), id: 'repoName' },
-                    ...(this.logDetail.replicaObjectType === 'PACKAGE' ? [{ name: this.$t('artifactName'), id: 'packageName' }] : []),
-                    ...(this.logDetail.replicaObjectType === 'PATH' ? [{ name: this.$t('filePath'), id: 'path' }] : [])
+                    { name: this.$t('artifactName'), id: 'artifactName' }
                 ]
             }
         },
@@ -132,10 +163,11 @@
                     })
                 }
             })
+            this.getPlanLogCountList()
         },
         methods: {
             formatDate,
-            ...mapActions(['getPlanLogDetail', 'getPlanLogPackageList']),
+            ...mapActions(['getPlanLogDetail', 'getPlanLogPackageList', 'getPlanLogDetailOverview']),
             handlerPaginationChange ({ current = 1, limit = this.pagination.limit } = {}) {
                 this.pagination.current = current
                 this.pagination.limit = limit
@@ -182,6 +214,19 @@
                     target[item.id] = item.values[0].id
                     return target
                 }, {})
+            },
+            // 获取当前分发计划执行记录中同步数量相关参数展示数据
+            getPlanLogCountList () {
+                this.getPlanLogDetailOverview({ id: this.logId }).then((res) => {
+                    // 只有在后端接口返回的数据有值时才需要展示，后端接口没有返回时则表明可能是历史数据，历史数据不展示
+                    if (res) {
+                        this.showOverviewCount = true
+                        this.countList = {
+                            total: (res?.success || 0) + (res?.failed || 0),
+                            ...res
+                        }
+                    }
+                })
             }
         }
     }
@@ -195,6 +240,16 @@
         .search-group {
             min-width: 250px;
         }
+    }
+    .log-count {
+        font-size: 14px;
+        font-weight: bold;
+        max-width: 50px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        display: inline-block;
+        vertical-align: middle;
     }
 }
 </style>
