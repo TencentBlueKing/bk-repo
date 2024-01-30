d03bb97d92a1765427dc3a6a1f7309ece43f5e85
kunlongli <kunlongli@tencent.com>
2024-01-30 21:47:19 +0800

    feat: 支持导出一段时间内的项目流量使用数据 #1744

--- src/backend/common/common-operate/operate-service/src/main/kotlin/com/tencent/bkrepo/common/operate/service/dao/ProjectUsageStatisticsDao.kt
+++ src/backend/common/common-operate/operate-service/src/main/kotlin/com/tencent/bkrepo/common/operate/service/dao/ProjectUsageStatisticsDao.kt
@@ -38,6 +38,7 @@ import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Update
 import org.springframework.data.mongodb.core.query.gte
 import org.springframework.data.mongodb.core.query.isEqualTo
+import org.springframework.data.mongodb.core.query.lt
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -89,14 +90,22 @@ class ProjectUsageStatisticsDao : SimpleMongoDao<TProjectUsageStatistics>() {
     }
 
     fun findBetween(start: Long, end: Long): List<TProjectUsageStatistics> {
-        val criteria = TProjectUsageStatistics::start.gte(start).and(TProjectUsageStatistics::start.name).lt(end)
+        val criteria = Criteria().andOperator(
+            TProjectUsageStatistics::start.gte(start),
+            TProjectUsageStatistics::start.lt(start),
+        )
         return find(Query(criteria))
     }
 
     fun delete(start: Long?, end: Long): DeleteResult {
-        val criteria = Criteria()
-        start?.let { criteria.and(TProjectUsageStatistics::start.name).gte(it) }
-        criteria.and(TProjectUsageStatistics::start.name).lt(end)
+        val criteria = if (start == null) {
+            TProjectUsageStatistics::start.lt(end)
+        } else {
+            Criteria().andOperator(
+                TProjectUsageStatistics::start.gte(start),
+                TProjectUsageStatistics::start.lt(end),
+            )
+        }
         return remove(Query(criteria))
     }
 }
