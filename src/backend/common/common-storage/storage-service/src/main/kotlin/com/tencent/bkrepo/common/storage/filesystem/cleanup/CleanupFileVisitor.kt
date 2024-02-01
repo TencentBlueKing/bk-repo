9aefb15e8ba30b2b28c291d57c6f500155de385f
zacyanliu <zacyanliu@tencent.com>
2024-02-01 17:56:41 +0800

    feat: 代码调整 #1707

--- src/backend/common/common-storage/storage-service/src/main/kotlin/com/tencent/bkrepo/common/storage/filesystem/cleanup/CleanupFileVisitor.kt
+++ src/backend/common/common-storage/storage-service/src/main/kotlin/com/tencent/bkrepo/common/storage/filesystem/cleanup/CleanupFileVisitor.kt
@@ -106,20 +106,24 @@ class CleanupFileVisitor(
         }
         // 由于支持删除上传路径，所以这里即使是空目录，也需要判断过期时间。
         if (fileExpireResolver.isExpired(dirPath.toFile())) {
-            Files.newDirectoryStream(dirPath).use {
-                if (!it.iterator().hasNext()) {
-                    try {
-                        Files.delete(dirPath)
-                    }  catch (e: DirectoryNotEmptyException) {
-                        logger.warn("Directory [$dirPath] is not empty!")
-                    }
-                    logger.info("Clean up folder[$dirPath].")
-                    result.cleanupFolder += 1
-                }
-            }
+            deleteEmptyFolder(dirPath)
         }
         result.totalFolder += 1
         return FileVisitResult.CONTINUE
+    }
+
+    private fun deleteEmptyFolder(dirPath: Path) {
+        Files.newDirectoryStream(dirPath).use {
+            if (!it.iterator().hasNext()) {
+                try {
+                    Files.delete(dirPath)
+                    logger.info("Clean up folder[$dirPath].")
+                    result.cleanupFolder += 1
+                }  catch (ignore: DirectoryNotEmptyException) {
+                    logger.warn("Directory [$dirPath] is not empty!")
+                }
+            }
+        }
     }
 
     override fun needWalk(): Boolean {
