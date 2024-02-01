e47da6298af51842e33275ccbb5a1bfb1c9c681c
zacyanliu <zacyanliu@tencent.com>
2024-02-01 11:09:13 +0800

    feat: 去除多余引用 #1685

--- src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/controller/api/HelmFixToolController.kt
+++ src/backend/helm/biz-helm/src/main/kotlin/com/tencent/bkrepo/helm/controller/api/HelmFixToolController.kt
@@ -42,7 +42,6 @@ import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestAttribute
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
