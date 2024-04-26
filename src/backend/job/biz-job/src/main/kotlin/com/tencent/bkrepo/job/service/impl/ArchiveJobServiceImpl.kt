f1e79158b2d621114b8295821c8d5de5145515ce
felixncheng <felixncheng@tencent.com>
2024-04-26 15:14:42 +0800

    feat: 归档文件支持按项目取回  #2079

--- src/backend/job/biz-job/src/main/kotlin/com/tencent/bkrepo/job/service/impl/ArchiveJobServiceImpl.kt
+++ src/backend/job/biz-job/src/main/kotlin/com/tencent/bkrepo/job/service/impl/ArchiveJobServiceImpl.kt
@@ -1,6 +1,9 @@
 package com.tencent.bkrepo.job.service.impl
 
+import com.tencent.bkrepo.archive.api.ArchiveClient
 import com.tencent.bkrepo.archive.constant.ArchiveStorageClass
+import com.tencent.bkrepo.archive.request.ArchiveFileRequest
+import com.tencent.bkrepo.archive.request.UncompressFileRequest
 import com.tencent.bkrepo.common.mongo.dao.util.sharding.HashShardingUtils
 import com.tencent.bkrepo.fs.server.constant.FAKE_SHA256
 import com.tencent.bkrepo.job.BATCH_SIZE
@@ -8,9 +11,11 @@ import com.tencent.bkrepo.job.SHARDING_COUNT
 import com.tencent.bkrepo.job.batch.context.NodeContext
 import com.tencent.bkrepo.job.batch.task.archive.IdleNodeArchiveJob
 import com.tencent.bkrepo.job.batch.task.archive.IdleNodeArchiveJob.Companion.COLLECTION_NAME_PREFIX
+import com.tencent.bkrepo.job.batch.utils.NodeCommonUtils
+import com.tencent.bkrepo.job.batch.utils.RepositoryCommonUtils
 import com.tencent.bkrepo.job.service.ArchiveJobService
+import com.tencent.bkrepo.repository.constant.SYSTEM_USER
 import org.slf4j.LoggerFactory
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.isEqualTo
@@ -21,7 +26,7 @@ import java.time.LocalDateTime
 @Service
 class ArchiveJobServiceImpl(
     private val archiveJob: IdleNodeArchiveJob,
-    val reactiveMongoTemplate: ReactiveMongoTemplate,
+    private val archiveClient: ArchiveClient,
 ) : ArchiveJobService {
     override fun archive(projectId: String, key: String, days: Int, storageClass: ArchiveStorageClass) {
         val now = LocalDateTime.now()
@@ -37,15 +42,59 @@ class ArchiveJobServiceImpl(
                     Criteria.where("lastAccessDate").isEqualTo(null),
                     Criteria.where("lastAccessDate").lt(cutoffTime),
                 ),
-        ).cursorBatchSize(BATCH_SIZE)
+        )
         val index = HashShardingUtils.shardingSequenceFor(projectId, SHARDING_COUNT)
         val collectionName = COLLECTION_NAME_PREFIX.plus(index)
         val context = NodeContext()
-        reactiveMongoTemplate.find(query, IdleNodeArchiveJob.Node::class.java, collectionName)
-            .doFinally { logger.info(context.toString()) }
-            .subscribe {
-                archiveJob.archiveNode(it, context, storageClass, key, days)
+        NodeCommonUtils.findByCollectionAsync(query, BATCH_SIZE, collectionName) {
+            val node = archiveJob.mapToEntity(it)
+            archiveJob.archiveNode(node, context, storageClass, key, days)
+        }.subscribe {
+            logger.info("Success to archive project[$projectId], $context")
+        }
+    }
+
+    override fun restore(projectId: String) {
+        val query = Query.query(
+            Criteria.where("folder").isEqualTo(false)
+                .and("deleted").isEqualTo(null)
+                .and("sha256").ne(FAKE_SHA256)
+                .and("projectId").isEqualTo(projectId).orOperator(
+                    Criteria.where("archived").isEqualTo(true),
+                    Criteria.where("compressed").isEqualTo(true),
+                ),
+        )
+        val index = HashShardingUtils.shardingSequenceFor(projectId, SHARDING_COUNT)
+        val collectionName = COLLECTION_NAME_PREFIX.plus(index)
+        val context = NodeContext()
+        NodeCommonUtils.findByCollectionAsync(query, BATCH_SIZE, collectionName) {
+            val node = archiveJob.mapToEntity(it)
+            val repoName = node.repoName
+            val fullPath = node.fullPath
+            val sha256 = node.sha256
+            try {
+                val repo = RepositoryCommonUtils.getRepositoryDetail(projectId, repoName)
+                val credentialKey = repo.storageCredentials?.key
+                if (it["archived"].toString() == "true") {
+                    val req = ArchiveFileRequest(sha256, credentialKey, SYSTEM_USER)
+                    archiveClient.restore(req)
+                } else {
+                    val req = UncompressFileRequest(sha256, credentialKey, SYSTEM_USER)
+                    archiveClient.uncompress(req)
+                }
+                context.count.incrementAndGet()
+                context.size.addAndGet(node.size)
+                context.success.incrementAndGet()
+                logger.info("Restore node $projectId/$repoName$fullPath($sha256)")
+            } catch (e: Exception) {
+                context.failed.incrementAndGet()
+                logger.error("Restore node error $projectId/$repoName$fullPath($sha256)", e)
+            } finally {
+                context.total.incrementAndGet()
             }
+        }.subscribe {
+            logger.info("Success to restore project[$projectId], $context")
+        }
     }
 
     companion object {
