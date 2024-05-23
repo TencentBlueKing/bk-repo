b1e7beb93c433714186ae6e04bb18f33ae936dc3
lannoy0523 <935275025@qq.com>
2024-05-23 11:24:18 +0800

    feat：目录权限新增配置---禁止根目录权限 #2166

--- src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/controller/user/RootDirectoryPermissionController.kt
+++ src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/controller/user/RootDirectoryPermissionController.kt
@@ -43,8 +43,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestAttribute
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.PathVariable
 import java.time.LocalDateTime
 
 @RestController
