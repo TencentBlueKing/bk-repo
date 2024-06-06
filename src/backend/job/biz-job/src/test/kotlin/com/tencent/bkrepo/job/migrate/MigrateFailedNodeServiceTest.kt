aa4529478eb16c05603548838a0861a847ec89f6
kunlongli <kunlongli@tencent.com>
2024-06-06 21:57:02 +0800

    bug: 增加自动修复迁移失败node单元测试 #2246

--- src/backend/job/biz-job/src/test/kotlin/com/tencent/bkrepo/job/migrate/MigrateFailedNodeServiceTest.kt
+++ src/backend/job/biz-job/src/test/kotlin/com/tencent/bkrepo/job/migrate/MigrateFailedNodeServiceTest.kt
@@ -14,7 +14,6 @@ import com.tencent.bkrepo.job.migrate.strategy.MigrateFailedNodeAutoFixStrategy
 import com.tencent.bkrepo.job.migrate.strategy.MigrateFailedNodeFixer
 import com.tencent.bkrepo.job.migrate.utils.MigrateTestUtils.insertFailedNode
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
