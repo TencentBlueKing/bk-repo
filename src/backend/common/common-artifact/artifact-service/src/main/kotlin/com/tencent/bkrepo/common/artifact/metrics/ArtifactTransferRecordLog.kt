a6f8d3cec119cf13afe961d3cd163e9f7d2d4845
zacyanliu <zacyanliu@tencent.com>
2024-07-02 10:44:21 +0800

    feat: 打印字段增加 #2233

--- src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/metrics/ArtifactTransferRecordLog.kt
+++ src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/metrics/ArtifactTransferRecordLog.kt
@@ -44,6 +44,8 @@ class ArtifactTransferRecordLog(
     val clientIp: String = record.clientIp
     val project: String = record.project
     val repoName: String = record.repoName
+    val agent: String = record.agent
+    val fullPath: String = record.fullPath
     val service: String? = commonTag["service"]
     val instance: String? = commonTag["instance"]
     val host: String? = commonTag["host"]
