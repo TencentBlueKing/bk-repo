b5c078e97d15f9457a0c5a58335b458c51535d27
owenlxu <owenlxu@tencent.com>
2024-07-16 16:39:58 +0800

    feat: 制品库支持访问控制 #2347

--- src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/service/local/PermissionServiceImpl.kt
+++ src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/service/local/PermissionServiceImpl.kt
@@ -470,9 +470,9 @@ open class PermissionServiceImpl constructor(
         roles: Set<String>,
         requestSource: String?
     ): Boolean {
-        logger.info("check user in access deny group [$projectId, $repoName, $roles]")
+        logger.info("check user in access deny group [$projectId, $repoName]")
         // 仅校验repo下的请求
-        if (repoName == null) return true
+        if (repoName == null) return false
         if (requestSource != null && requestSource == DEVX_ACCESS_FROM_OFFICE) {
             val result = repoAuthConfigDao.findOneByProjectRepo(projectId, repoName) ?: return false
             if (result.officeDenyGroupSet == null) return false
