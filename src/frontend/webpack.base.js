6988b1c843f92d572c282ad9ff06ff59be223ed4
lannoy0523 <935275025@qq.com>
2024-09-09 14:37:59 +0800

    bug:修复前端组件漏洞 #2538

--- src/frontend/webpack.base.js
+++ src/frontend/webpack.base.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const ESLintPlugin = require('eslint-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
@@ -53,17 +54,6 @@ module.exports = ({ entry, publicPath, dist, port = 8080, argv, env }) => {
                         },
                         'sass-loader'
                     ]
-                },
-                {
-                    test: /\.(js|vue)$/,
-                    loader: 'eslint-loader',
-                    enforce: 'pre',
-                    include: [path.resolve(__dirname, 'devops-repository/src'), path.resolve('src')],
-                    exclude: [/node_modules/],
-                    options: {
-                        fix: true,
-                        formatter: require('eslint-friendly-formatter')
-                    }
                 }
             ]
         },
@@ -75,8 +65,28 @@ module.exports = ({ entry, publicPath, dist, port = 8080, argv, env }) => {
                 ignoreOrder: true
             }),
             new CopyWebpackPlugin({
-                patterns: [{ from: path.join(__dirname, 'locale', dist), to: buildDist }],
+                patterns: [{ from: path.join(__dirname, 'locale'), to: buildDist }],
                 options: { concurrency: 100 }
+            }),
+            new ESLintPlugin({
+                // 指定要检查的文件扩展名
+                extensions: ['js', 'vue'],
+                // 指定要检查的目录
+                context: path.resolve(__dirname, 'devops-repository/src'), // 主要上下文目录
+                // 其他选项
+                fix: true, // 自动修复问题
+                formatter: require('eslint-friendly-formatter'), // 使用友好的格式化程序
+                // 你可以添加更多选项，例如：
+                emitWarning: true, // 如果有 ESLint 警告，是否在控制台中显示
+                failOnError: false, // 如果有错误，是否使构建失败
+                failOnWarning: false, // 如果有警告，是否使构建失败
+                // 使用 include 选项来指定多个路径
+                files: [
+                    path.resolve(__dirname, 'devops-repository/src/**/*.js'),
+                    path.resolve(__dirname, 'devops-repository/src/**/*.vue'),
+                    path.resolve(__dirname, 'src/**/*.js'),
+                    path.resolve(__dirname, 'src/**/*.vue')
+                ]
             })
         ],
         optimization: {
