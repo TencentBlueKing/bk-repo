895e1c6a22767b4b988cd3f3541d16ded2b35804
kunlongli <kunlongli@tencent.com>
2024-10-16 18:12:29 +0800

    feat: 支持动态增加需要向量化访问日志的项目 #2651

--- src/backend/job/biz-job/src/main/kotlin/com/tencent/bkrepo/job/controller/user/UserEmbeddingController.kt
+++ src/backend/job/biz-job/src/main/kotlin/com/tencent/bkrepo/job/controller/user/UserEmbeddingController.kt
@@ -1,5 +1,7 @@
 package com.tencent.bkrepo.job.controller.user
 
+import com.tencent.bkrepo.common.api.exception.ErrorCodeException
+import com.tencent.bkrepo.common.api.message.CommonMessageCode
 import com.tencent.bkrepo.common.security.permission.Principal
 import com.tencent.bkrepo.common.security.permission.PrincipalType
 import com.tencent.bkrepo.job.batch.task.cache.preload.ArtifactAccessLogEmbeddingJob
@@ -16,6 +18,9 @@ class UserEmbeddingController(
 ) {
     @PostMapping("/project/{projectId}")
     fun embed(@PathVariable projectId: String) {
+        if (artifactAccessLogEmbeddingJob == null) {
+            throw ErrorCodeException(CommonMessageCode.SYSTEM_ERROR, "unsupported operation")
+        }
         artifactAccessLogEmbeddingJob?.embedAccessLog(projectId)
     }
 }
