b4eb20bae3471f627316dc152b2ba1948d792d3c
liuliaozhong <liuliaozhong@canway.net>
2024-12-26 16:31:13 +0800

    feat: 文件在线预览支持更多的文件类型 #2765
    * feat: 文件在线预览支持更多的文件类型 #2765
    
    文件预览后端服务基础实现，为页面提供接口服务
    1.一个全新的preview服务，已实现文件的下载，可以下载制品库文件、远程文件.
    2.office文件转换实现.
    3.水印接口.
    4.缓存与异常处理.
    5.k8s部署
    
    * feat: 文件在线预览支持更多的文件类型 #2765
    
    代码规范
    
    * feat: 文件在线预览支持更多的文件类型 #2765
    
    1.调整接口逻辑，预览接口返回流
    2.远程文件保存在自定义仓库，bkrepo文件保存在原仓库新建一份供预览
    3.redis缓存更改为mongodb存储
    4.增加预览类型支持，code、txt、picture等
    5.公共处理逻辑提取到抽象类中
    
    * feat: 文件在线预览支持更多的文件类型 #2765
    
    1.代码规范
    2.构建镜像报错
    
    * feat: 文件在线预览支持更多的文件类型 #2765
    
    1.代码规范之方法嵌套太深
    2.指定默认的officeHome
    
    * feat: 文件在线预览支持更多的文件类型 #2765
    
    1.office插件启动失败问题
    
    * feat: 文件在线预览支持更多的文件类型 #2765
    
    1.路径中文乱码
    2.office转换pdf乱码
    
    * feat: 文件在线预览支持更多的文件类型 #2765
    
    1.指定构建preview的dockerfile文件
    
    * feat: 文件在线预览支持更多的文件类型 #2765
    
    1.review意见修改
    
    * feat: 文件在线预览支持更多的文件类型 #2765
    
    1.代码规范，方法嵌套太深

---
+++ src/backend/preview/biz-preview/src/main/kotlin/com/tencent/bkrepo/preview/service/FileTransferService.kt
@@ -0,0 +1,176 @@
+/*
+ * Tencent is pleased to support the open source community by making BK-CI 蓝鲸持续集成平台 available.
+ *
+ * Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * BK-CI 蓝鲸持续集成平台 is licensed under the MIT license.
+ *
+ * A copy of the MIT License is included in this file.
+ *
+ *
+ * Terms of the MIT License:
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.tencent.bkrepo.preview.service
+
+import com.tencent.bkrepo.common.artifact.api.ArtifactInfo
+import com.tencent.bkrepo.common.artifact.constant.ARTIFACT_INFO_KEY
+import com.tencent.bkrepo.common.artifact.constant.REPO_KEY
+import com.tencent.bkrepo.common.artifact.pojo.RepositoryCategory
+import com.tencent.bkrepo.common.artifact.pojo.RepositoryType
+import com.tencent.bkrepo.common.artifact.repository.context.ArtifactContextHolder
+import com.tencent.bkrepo.common.artifact.repository.context.ArtifactDownloadContext
+import com.tencent.bkrepo.common.artifact.repository.context.ArtifactUploadContext
+import com.tencent.bkrepo.common.artifact.repository.core.ArtifactService
+import com.tencent.bkrepo.common.artifact.resolve.file.ArtifactFileFactory
+import com.tencent.bkrepo.common.metadata.service.repo.RepositoryService
+import com.tencent.bkrepo.common.service.util.HttpContextHolder
+import com.tencent.bkrepo.preview.config.configuration.PreviewConfig
+import com.tencent.bkrepo.preview.constant.PREVIEW_ARTIFACT_TO_FILE
+import com.tencent.bkrepo.preview.constant.PREVIEW_NODE_DETAIL
+import com.tencent.bkrepo.preview.constant.PREVIEW_TMP_FILE_SAVE_PATH
+import com.tencent.bkrepo.preview.constant.PreviewMessageCode
+import com.tencent.bkrepo.preview.exception.PreviewNotFoundException
+import com.tencent.bkrepo.preview.pojo.DownloadResult
+import com.tencent.bkrepo.preview.pojo.FileAttribute
+import com.tencent.bkrepo.preview.pojo.cache.PreviewFileCacheInfo
+import com.tencent.bkrepo.preview.utils.DownloadUtils
+import com.tencent.bkrepo.repository.pojo.node.NodeDetail
+import javax.servlet.http.HttpServletRequest
+import org.springframework.stereotype.Component
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.UUID
+
+@Component
+class FileTransferService(
+    private val config: PreviewConfig,
+    private val downloadUtils: DownloadUtils,
+    private val repositoryService: RepositoryService
+) : ArtifactService() {
+
+    /**
+     * 输出文件内容
+     */
+    fun sendFileAsResponse(fileAttribute: FileAttribute, previewFileCacheInfo: PreviewFileCacheInfo) {
+        val artifactInfo = ArtifactInfo(previewFileCacheInfo.projectId,
+            previewFileCacheInfo.repoName,
+            previewFileCacheInfo.fullPath
+        )
+        setFileTransferAttribute(artifactInfo, fileAttribute, false)
+        val node = ArtifactContextHolder.getNodeDetail(artifactInfo)
+        val context = ArtifactDownloadContext()
+        if (node == null && context.repositoryDetail.category == RepositoryCategory.LOCAL) {
+            throw PreviewNotFoundException(PreviewMessageCode.PREVIEW_NODE_NOT_FOUND,
+                "${artifactInfo.projectId}|${artifactInfo.repoName}|${artifactInfo.getArtifactFullPath()}")
+        }
+        repository.download(context)
+    }
+
+    fun download(fileAttribute: FileAttribute): DownloadResult? {
+        var result: DownloadResult? = if (fileAttribute.storageType == 1) {
+            downloadUtils.downLoad(fileAttribute, config)
+        } else {
+            downloadFromRepo(fileAttribute)
+        }
+        fileAttribute.originFilePath = result?.filePath
+        return result
+    }
+
+    fun downloadFromRepo(fileAttribute: FileAttribute):DownloadResult? {
+        val artifactInfo = ArtifactInfo(fileAttribute.projectId!!,
+            fileAttribute.repoName!!,
+            fileAttribute.artifactUri!!
+        )
+        setFileTransferAttribute(artifactInfo, fileAttribute, true)
+        val node = ArtifactContextHolder.getNodeDetail(artifactInfo)
+        val context = ArtifactDownloadContext()
+        if (node == null && context.repositoryDetail.category == RepositoryCategory.LOCAL) {
+            throw PreviewNotFoundException(PreviewMessageCode.PREVIEW_NODE_NOT_FOUND,
+                "${artifactInfo.projectId}|${artifactInfo.repoName}|${artifactInfo.getArtifactFullPath()}")
+        }
+        val result = DownloadResult()
+        try {
+            repository.download(context)
+            val request: HttpServletRequest = HttpContextHolder.getRequest()
+            result.filePath = request.getAttribute(PREVIEW_TMP_FILE_SAVE_PATH)?.toString()
+            result.md5 = node!!.md5
+            result.size = node.size
+        } catch (e: Exception) {
+            result.apply {
+                code = DownloadResult.CODE_FAIL
+                msg = "Download Faile from bkrepo,$e"
+            }
+        }
+
+        return result
+    }
+
+    fun upload(fileAttribute: FileAttribute, sourcePath: String): NodeDetail{
+        val file = File(sourcePath)
+        require(file.exists()) { "The file does not exist, $sourcePath" }
+        // 准备要上传的信息,如果是bkrepo文件，预览文件保存在原仓库，否则保存在自定义仓库中
+        val projectId = if (fileAttribute.storageType == 0) fileAttribute.projectId else config.projectId
+        val repoName = if (fileAttribute.storageType == 0) fileAttribute.repoName else config.repoName
+        val artifactInfo = ArtifactInfo(projectId!!, repoName!!, buildArtifactUri(fileAttribute))
+        setFileTransferAttribute(artifactInfo, fileAttribute, true)
+        val artifactFile = ArtifactFileFactory.build(file.inputStream(), file.length())
+        val context = ArtifactUploadContext(artifactFile)
+
+        repository.upload(context)
+
+        val nodeDetail: NodeDetail = context.getAttribute(PREVIEW_NODE_DETAIL)!!
+        return nodeDetail
+    }
+
+    /**
+     * 把project、repo等信息设置到request域，上传、下载要用
+     */
+    private fun setFileTransferAttribute(artifactInfo: ArtifactInfo,
+                                         fileAttribute: FileAttribute,
+                                         toFile: Boolean) {
+        val request: HttpServletRequest = HttpContextHolder.getRequest()
+        val repoDetail =repositoryService.getRepoDetail(artifactInfo.projectId,
+            artifactInfo.repoName,
+            RepositoryType.GENERIC.name
+        ) ?: throw PreviewNotFoundException(PreviewMessageCode.PREVIEW_REPO_NOT_FOUND,
+            "${artifactInfo.projectId}|${artifactInfo.repoName}")
+        request.setAttribute(ARTIFACT_INFO_KEY, artifactInfo)
+        request.setAttribute(REPO_KEY, repoDetail)
+
+        val fileTmpPath = downloadUtils.getRelFilePath(fileAttribute.fileName, fileAttribute.suffix!!, config.fileDir)
+        request.setAttribute(PREVIEW_TMP_FILE_SAVE_PATH, fileTmpPath)
+        request.setAttribute(PREVIEW_ARTIFACT_TO_FILE, toFile)
+    }
+
+    /**
+     * 制品uri：/date/uuid/fileName
+     */
+    private fun buildArtifactUri(fileAttribute: FileAttribute): String {
+        val date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+        return if (fileAttribute.storageType == 0)
+            "/convert/$date/${UUID.randomUUID()}/${fileAttribute.convertFileName}"
+        else
+            "/$date/${UUID.randomUUID()}/${fileAttribute.convertFileName}"
+    }
+
+}
---
+++ src/backend/preview/biz-preview/src/main/kotlin/com/tencent/bkrepo/preview/service/impl/OfficeFilePreviewImpl.kt
@@ -0,0 +1,159 @@
+/*
+ * Tencent is pleased to support the open source community by making BK-CI 蓝鲸持续集成平台 available.
+ *
+ * Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+ *
+ * BK-CI 蓝鲸持续集成平台 is licensed under the MIT license.
+ *
+ * A copy of the MIT License is included in this file.
+ *
+ *
+ * Terms of the MIT License:
+ * ---------------------------------------------------
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.tencent.bkrepo.preview.service.impl
+
+import com.tencent.bkrepo.common.metadata.service.node.NodeService
+import com.tencent.bkrepo.preview.config.configuration.PreviewConfig
+import com.tencent.bkrepo.preview.constant.PreviewMessageCode
+import com.tencent.bkrepo.preview.exception.PreviewSystemException
+import com.tencent.bkrepo.preview.pojo.DownloadResult
+import com.tencent.bkrepo.preview.pojo.FileAttribute
+import com.tencent.bkrepo.preview.service.FileTransferService
+import com.tencent.bkrepo.preview.service.OfficeToPdfService
+import com.tencent.bkrepo.preview.service.cache.impl.PreviewFileCacheServiceImpl
+import com.tencent.bkrepo.preview.utils.EncodingDetects
+import org.jodconverter.core.office.OfficeException
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.nio.charset.StandardCharsets
+
+/**
+ * 处理office文件
+ */
+@Service
+class OfficeFilePreviewImpl(
+    private val config: PreviewConfig,
+    private val officeToPdfService: OfficeToPdfService,
+    private val fileTransferService: FileTransferService,
+    private val previewFileCacheService: PreviewFileCacheServiceImpl,
+    private val nodeService: NodeService
+) : AbstractFilePreview(
+    config,
+    fileTransferService,
+    previewFileCacheService,
+    nodeService
+) {
+    /**
+     * 转换文件，比如docx转成pdf
+     */
+    override fun processFileConversion(downloadResult: DownloadResult,
+                                       fileAttribute: FileAttribute,
+                                       outFilePath: String?): String {
+        val filePath = downloadResult.filePath!!
+        var finalFilePath: String
+        try {
+            finalFilePath = if (isNeedConvert(fileAttribute)) {
+                officeToPdfService.openOfficeToPDF(filePath, outFilePath!!, fileAttribute)
+                outFilePath
+            } else {
+                filePath
+            }
+        } catch (e: OfficeException) {
+            logger.error("Failed to convert office file", e)
+            throw PreviewSystemException(PreviewMessageCode.PREVIEW_FIlE_CONVERT_ERROR, fileAttribute.fileName!!)
+        }
+        fileAttribute.finalFilePath = finalFilePath
+        return finalFilePath
+    }
+
+    /**
+     * 对转换后的文件进行操作(改变编码方式)
+     */
+    override fun processFileContent(fileAttribute: FileAttribute) {
+        if (fileAttribute.isHtmlView && !fileAttribute.suffix.equals("csv")) {
+            changeFileEncoding(fileAttribute.finalFilePath!!)
+        }
+    }
+
+    private fun isNeedConvert(fileAttribute: FileAttribute): Boolean {
+        val suffix = fileAttribute.suffix
+        return suffix!!.lowercase() !in listOf("xlsx", "csv")
+    }
+
+    /**
+     * 把文件改成utf8编码
+     */
+    private fun changeFileEncoding(outFilePath: String) {
+        try {
+            // 获取文件的编码方式
+            val charset = EncodingDetects.getJavaEncode(outFilePath)
+            // 读取文件内容并替换 charset
+            val fileContent = readFileWithEncoding(outFilePath, charset!!)
+            // 添加额外的 HTML 控制头
+            val updatedContent = addHtmlHeaders(fileContent)
+            // 重新写入文件，采用 UTF-8 编码
+            writeFileWithUtf8(outFilePath, updatedContent)
+        } catch (e: IOException) {
+            logger.error("Error changing the encoding of the file", e)
+        }
+    }
+
+    private fun readFileWithEncoding(outFilePath: String, charset: String): String {
+        val sb = StringBuilder()
+        FileInputStream(outFilePath).use { inputStream ->
+            BufferedReader(InputStreamReader(inputStream, charset)).use { reader ->
+                var line: String?
+                while (reader.readLine().also { line = it } != null) {
+                    sb.append(line?.replace("charset=gb2312", "charset=utf-8") ?: line)
+                }
+            }
+        }
+        return sb.toString()
+    }
+
+    private fun addHtmlHeaders(content: String): String {
+        val sb = StringBuilder(content)
+        sb.append("<script src=\"js/jquery-3.6.1.min.js\" type=\"text/javascript\"></script>")
+        sb.append("<script src=\"excel/excel.header.js\" type=\"text/javascript\"></script>")
+        sb.append("<link rel=\"stylesheet\" href=\"excel/excel.css\">")
+        return sb.toString()
+    }
+
+    private fun writeFileWithUtf8(outFilePath: String, content: String) {
+        FileOutputStream(outFilePath).use { fos ->
+            BufferedWriter(OutputStreamWriter(fos, StandardCharsets.UTF_8)).use { writer ->
+                writer.write(content)
+            }
+        }
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(OfficeFilePreviewImpl::class.java)
+    }
+}
