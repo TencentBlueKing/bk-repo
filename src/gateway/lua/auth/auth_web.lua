5a6dc81dbf4349135b1f6ccbe65a2ba200827cc1
owenlxu <owenlxu@tencent.com>
2025-02-11 15:54:39 +0800

    feat: 支持移动网关校验 #725

--- src/gateway/lua/auth/auth_web.lua
+++ src/gateway/lua/auth/auth_web.lua
@@ -53,7 +53,20 @@ elseif config.auth_mode == "" or config.auth_mode == "token" then
     token = bk_token
 elseif config.auth_mode == "ticket" then
     local bk_ticket = cookieUtil:get_cookie("bk_ticket")
-    if bk_ticket == nil then
+    if bk_ticket ~= nil then
+        username = oauthUtil:verify_ticket(bk_ticket, "ticket")
+        token = bk_ticket
+        return
+    end
+    -- 校验移动网关登录态
+    if config.mobileSiteToken ~= nil and config.mobileSiteToken ~= "" then
+        local mobile_user = oauthUtil:verify_mobile_gateway()
+        if mobile_user == nil then
+            ngx.exit(401)
+            return
+        end
+        username = mobile_user
+    else
         bk_ticket = ngx.var.http_x_devops_bk_ticket
         if bk_ticket == nil then
             bk_ticket = urlUtil:parseUrl(ngx.var.request_uri)["x-devops-bk-ticket"]
@@ -62,9 +75,9 @@ elseif config.auth_mode == "ticket" then
             ngx.exit(401)
             return
         end
+        username = oauthUtil:verify_ticket(bk_ticket, "ticket")
+        token = bk_ticket
     end
-    username = oauthUtil:verify_ticket(bk_ticket, "ticket")
-    token = bk_ticket
 elseif config.auth_mode == "odc" then
     local bk_token = cookieUtil:get_cookie("bk_token")
     if bk_token == nil then
