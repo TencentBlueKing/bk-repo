afbee5edb43bade9d57bebaa78a9a6f81c0c926b
owenlxu <owenlxu@tencent.com>
2025-05-09 16:12:04 +0800

    feat: init-mongodb 脚本支持tls认证 #3147

--- support-files/kubernetes/images/init/init-mongodb.sh
+++ support-files/kubernetes/images/init/init-mongodb.sh
@@ -8,6 +8,15 @@ sed -i "s/5f4dcc3b5aa765d61d8327deb882cf99/$password_md5/g" init-data-tenant.js
 access_key=$BK_REPO_ACCESSKEY
 secret_key=$BK_REPO_SECRETKEY
 multi_tenant=$BK_REPO_ENABLE_MULTI_TENANT_MODE
+tls_enable=$BK_REPO_ENABLE_MONGODB_TLS
+tls_path=$BK_REPO_MONGODB_TLS_PATH
+bcs_access_key=$BK_REPO_BCS_ACCESSKEY
+bcs_secret_key=$BK_REPO_BCS_SECRETKEY
+
+mongo_args="--ipv6"
+if [ "$tls_enable" = "true" ] ; then
+    mongo_args="$mongo_args --tls --tlsCAFile $tls_path --tlsAllowInvalidHostnames"
+fi
 
 if [ "$access_key" != "" ] && [ "$secret_key" != "" ]; then
     sed -i "s/18b61c9c-901b-4ea3-89c3-1f74be944b66/$access_key/g" init-data.js
@@ -16,23 +25,17 @@ if [ "$access_key" != "" ] && [ "$secret_key" != "" ]; then
     sed -i "s/Us8ZGDXPqk86cwMukYABQqCZLAkM3K/$secret_key/g" init-data-tenant.js
 fi
 
+if [ "$multi_tenant" != "true" ]; then
+    mongo $mongo_args $BK_REPO_MONGODB_URI init-data.js
+else
+    mongo $mongo_args $BK_REPO_MONGODB_URI init-data-tenant.js
+fi
 
-bcs_access_key=$BK_REPO_BCS_ACCESSKEY
-bcs_secret_key=$BK_REPO_BCS_SECRETKEY
+
 
 if [ "$bcs_access_key" != "" ] && [ "$bcs_secret_key" != "" ]; then
      sed -i "s/609f9939e6944c5c8a842d88acf85edc/$bcs_access_key/g" init-data-ext.js
      sed -i "s/e041dd34cd89466648a9b196150f75/$bcs_secret_key/g" init-data-ext.js
-     if [ "$multi_tenant" != "true" ]; then
-       mongo --ipv6 $BK_REPO_MONGODB_URI init-data.js && mongo --ipv6 $BK_REPO_MONGODB_URI init-data-ext.js
-     else
-       mongo --ipv6 $BK_REPO_MONGODB_URI init-data-tenant.js && mongo --ipv6 $BK_REPO_MONGODB_URI init-data-ext.js
-     fi
-else
-  if [ "$multi_tenant" != "true" ]; then
-      mongo --ipv6 $BK_REPO_MONGODB_URI init-data.js
-  else
-      mongo --ipv6 $BK_REPO_MONGODB_URI init-data-tenant.js
-  fi
+     mongo $mongo_args $BK_REPO_MONGODB_URI init-data-ext.js
 fi
 
