67fa4a8d88034722b909a2d4bded28f1cf38ab38
yaoxuwan <yaoxuwan@tencent.com>
2025-05-15 09:54:06 +0800

    feat: 升级jdk17和spring框架 #3154 修复服务启动错误

--- src/backend/driver/boot-driver/src/main/kotlin/com/tencent/bkrepo/fs/server/handler/NodeOperationsHandler.kt
+++ src/backend/driver/boot-driver/src/main/kotlin/com/tencent/bkrepo/fs/server/handler/NodeOperationsHandler.kt
@@ -65,6 +65,7 @@ import com.tencent.bkrepo.repository.pojo.node.service.NodeRenameRequest
 import com.tencent.bkrepo.repository.pojo.node.service.NodeSetLengthRequest
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.buildAndAwait
@@ -75,6 +76,7 @@ import reactivefeign.client.ReadTimeoutException
  *
  * 处理节点操作的请求
  * */
+@Component
 class NodeOperationsHandler(
     private val fileNodeService: FileNodeService,
     private val fsService: FsService,
