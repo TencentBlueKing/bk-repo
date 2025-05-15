e71d7974ff15cb6d6ef958bd872b8ba92021f8c1
yaoxuwan <yaoxuwan@tencent.com>
2025-05-15 09:54:06 +0800

    feat: 升级jdk17和spring框架 #3154 修复服务启动错误

--- src/backend/driver/boot-driver/src/main/kotlin/com/tencent/bkrepo/fs/server/config/properties/PropertiesConfiguration.kt
+++ src/backend/driver/boot-driver/src/main/kotlin/com/tencent/bkrepo/fs/server/config/properties/PropertiesConfiguration.kt
@@ -1,5 +1,7 @@
 package com.tencent.bkrepo.fs.server.config.properties
 
+import com.tencent.bkrepo.common.security.http.jwt.JwtAuthProperties
+import com.tencent.bkrepo.common.security.interceptor.devx.DevXProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
@@ -7,5 +9,7 @@ import org.springframework.context.annotation.Configuration
 @EnableConfigurationProperties(
     IoaProperties::class,
     StreamProperties::class,
+    DevXProperties::class,
+    JwtAuthProperties::class,
 )
 class PropertiesConfiguration
