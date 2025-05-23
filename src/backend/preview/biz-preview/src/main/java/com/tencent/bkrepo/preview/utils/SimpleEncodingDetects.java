d0ee8b1576a55ccb9cce059e900b0ee172d7e109
yaoxuwan <yaoxuwan@tencent.com>
2025-05-23 16:17:02 +0800

    feat: 升级jdk17和spring框架 #3154 支持发布biz包

--- src/backend/preview/biz-preview/src/main/java/com/tencent/bkrepo/preview/utils/SimpleEncodingDetects.java
+++ src/backend/preview/biz-preview/src/main/java/com/tencent/bkrepo/preview/utils/SimpleEncodingDetects.java
@@ -1,22 +1,14 @@
 package com.tencent.bkrepo.preview.utils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 /**
- * <Detect encoding .>
- *  Copyright (C) <2009>  <Fluck,ACC http://androidos.cc/dev>
- *
- *   This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
  * EncodingDetect.java<br>
  * 自动获取文件的编码
  * @author Billows.Van
@@ -106,7 +98,7 @@ class BytesEncodingDetect extends Encoding {
 
 
     /**
-     * Function : detectEncoding Aruguments: URL Returns : One of the encodings from the Encoding enumeration (GB2312, HZ, BIG5,
+     * Function: detectEncoding Aruguments: URL Returns : One of the encodings from the Encoding enumeration (GB2312, HZ, BIG5,
      * EUC_TW, ASCII, or OTHER) Description: This function looks at the URL contents and assigns it a probability score for each
      * encoding type. The encoding type with the highest probability is returned.
      */
@@ -132,7 +124,7 @@ class BytesEncodingDetect extends Encoding {
 
 
     /**
-     * Function : detectEncoding Aruguments: byte array Returns : One of the encodings from the Encoding enumeration (GB2312, HZ,
+     * Function: detectEncoding Aruguments: byte array Returns : One of the encodings from the Encoding enumeration (GB2312, HZ,
      * BIG5, EUC_TW, ASCII, or OTHER) Description: This function looks at the byte array and assigns it a probability score for
      * each encoding type. The encoding type with the highest probability is returned.
      */
