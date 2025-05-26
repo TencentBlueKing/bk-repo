58688355cda83f884d6d775a8b561464aaa963e6
yaoxuwan <yaoxuwan@tencent.com>
2025-05-26 12:10:53 +0800

    feat: 升级jdk17和spring框架 #3154 增加snapshots仓库

--- src/backend/settings.gradle.kts
+++ src/backend/settings.gradle.kts
@@ -33,18 +33,20 @@ rootProject.name = "bk-repo-backend"
 
 pluginManagement {
     repositories {
+        val snapshotsRepo = maven {
+            name = "MavenSnapshot"
+            url = java.net.URI("https://oss.sonatype.org/content/repositories/snapshots/")
+            mavenContent {
+                snapshotsOnly()
+            }
+        }
         if (System.getenv("GITHUB_WORKFLOW") == null) {
+            snapshotsRepo
             maven(url = "https://mirrors.tencent.com/nexus/repository/gradle-plugins/")
             maven(url = "https://mirrors.tencent.com/nexus/repository/maven-public")
             maven(url = "https://repo.spring.io/milestone")
         } else {
-            maven {
-                name = "MavenSnapshot"
-                url = java.net.URI("https://oss.sonatype.org/content/repositories/snapshots/")
-                mavenContent {
-                    snapshotsOnly()
-                }
-            }
+            snapshotsRepo
             mavenCentral()
             maven(url = "https://repo.spring.io/milestone")
             gradlePluginPortal()
