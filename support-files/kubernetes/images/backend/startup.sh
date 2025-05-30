d1c982207ffa0355617261e04ca4f1dd92070270
yaoxuwan <yaoxuwan@tencent.com>
2025-05-30 09:48:36 +0800

    feat: 升级jdk17和spring框架 #3154 支持容器化

--- support-files/kubernetes/images/backend/startup.sh
+++ support-files/kubernetes/images/backend/startup.sh
@@ -6,14 +6,10 @@ chmod 777 $BK_REPO_LOGS_DIR
 java -server \
      -Dsun.jnu.encoding=UTF-8 \
      -Dfile.encoding=UTF-8 \
-     -Xloggc:$BK_REPO_LOGS_DIR/gc.log \
-     -XX:+PrintTenuringDistribution \
-     -XX:+PrintGCDetails \
-     -XX:+PrintGCDateStamps \
+     -Xlog:gc*,gc+age=trace:file=$BK_REPO_LOGS_DIR/gc.log:time,uptime,level,tags \
      -XX:+HeapDumpOnOutOfMemoryError \
      -XX:HeapDumpPath=oom.hprof \
      -XX:ErrorFile=$BK_REPO_LOGS_DIR/error_sys.log \
-     -XX:+UseContainerSupport \
      -Dspring.profiles.active=$BK_REPO_PROFILE \
      -Dservice.prefix=$BK_REPO_SERVICE_PREFIX \
      $BK_REPO_JVM_OPTION \
