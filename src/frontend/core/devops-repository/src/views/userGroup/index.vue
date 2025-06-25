83dab8d1102893b2ad8c3e9c6481d61552c8276e
lannoy0523 <935275025@qq.com>
2025-06-25 16:02:37 +0800

    feat:制品库适配日语 #3265

--- src/frontend/core/devops-repository/src/views/userGroup/index.vue
+++ src/frontend/core/devops-repository/src/views/userGroup/index.vue
@@ -50,7 +50,7 @@
             @cancel="cancel">
             <bk-form :label-width="80" :model="editRoleConfig" :rules="rules" ref="roleForm">
                 <bk-form-item :label="$t('roleName')" :required="true" property="name" error-display-type="normal">
-                    <bk-input v-model.trim="editRoleConfig.name" maxlength="32" show-word-limit></bk-input>
+                    <bk-input v-model.trim="editRoleConfig.name" maxlength="32" show-word-limit :placeholder="$t('pleaseInput')"></bk-input>
                 </bk-form-item>
                 <bk-form-item :label="$t('description')">
                     <bk-input type="textarea" v-model.trim="editRoleConfig.description" maxlength="200" :placeholder="$t('pleaseInput')"></bk-input>
