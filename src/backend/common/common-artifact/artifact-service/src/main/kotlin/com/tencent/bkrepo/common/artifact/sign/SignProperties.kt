ccc4b305051a373851e9ed31444c2afd8803b458
yaoxuwan <yaoxuwan@tencent.com>
2025-07-30 17:59:10 +0800

    feat: 插件signed-apk-forward代码合入主库 #3313

--- src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/sign/SignProperties.kt
+++ src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/sign/SignProperties.kt
@@ -34,6 +34,4 @@ data class SignProperties(
     var host: String = "http://localhost",
     var config: MutableMap<String, SignConfig> = mutableMapOf(),
     var signedRepoName: String = "bkrepo-signed-repo",
-    var oldSignedProjectId: String = "",
-    var oldSignedRepoName: String= "",
 )
