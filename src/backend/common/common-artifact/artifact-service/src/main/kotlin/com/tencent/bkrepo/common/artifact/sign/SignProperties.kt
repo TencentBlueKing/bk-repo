dfcfeba084c2c9b6eb41ac6a65218cdb9eae6e41
yaoxuwan <yaoxuwan@tencent.com>
2025-07-30 11:28:20 +0800

    feat: 插件apk-defender代码合入主库 #3313

--- src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/sign/SignProperties.kt
+++ src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/sign/SignProperties.kt
@@ -30,10 +30,10 @@ package com.tencent.bkrepo.common.artifact.manager.sign
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "sign")
-data class SignProperties(
-    var host: String = "http://localhost",
-    var config: MutableMap<String, SignConfig> = mutableMapOf(),
-    var signedRepoName: String = "bkrepo-signed-repo",
-    var oldSignedProjectId: String = "",
-    var oldSignedRepoName: String= "",
-)
+class SignProperties {
+    var host: String = "http://localhost"
+    var config: MutableMap<String, SignConfig> = mutableMapOf()
+    var signedRepoName: String = "bkrepo-signed-repo"
+    var oldSignedProjectId: String = ""
+    var oldSignedRepoName: String = ""
+}
