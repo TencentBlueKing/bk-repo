365055de8678c7b5f48af53995e52c90f389ef5d
ruotiantang <ruotiantang@tencent.com>
2025-08-29 10:50:25 +0800

    feat: 音视频绘制鼠标 #3378 add job

--- src/backend/media/biz-media-job/src/main/kotlin/com/tencent/bkrepo/media/job/pojo/ResourceLimit.kt
+++ src/backend/media/biz-media-job/src/main/kotlin/com/tencent/bkrepo/media/job/pojo/ResourceLimit.kt
@@ -1,10 +1,10 @@
 package com.tencent.bkrepo.media.job.pojo
 
 data class ResourceLimit(
-    val limitMem: String = "4GB",
-    val limitStorage: String = "128GB",
+    val limitMem: String = "4G",
+    val limitStorage: String = "128G",
     val limitCpu: String = "16",
-    val requestMem: String = "2GB",
-    val requestStorage: String = "16GB",
+    val requestMem: String = "2G",
+    val requestStorage: String = "16G",
     val requestCpu: String = "4",
 )
