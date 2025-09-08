c8e6d30c31f48a7d96a1604ef3a48e380428485f
kunlongli <kunlongli@tencent.com>
2025-09-08 11:32:55 +0800

    feat: 支持修改BlockNode ShardingKey #3523

--- src/backend/common/common-mongo-api/src/main/kotlin/com/tencent/bkrepo/common/mongo/api/util/sharding/HashShardingUtils.kt
+++ src/backend/common/common-mongo-api/src/main/kotlin/com/tencent/bkrepo/common/mongo/api/util/sharding/HashShardingUtils.kt
@@ -71,6 +71,8 @@ object HashShardingUtils : ShardingUtils {
         require(values.isNotEmpty())
         var h = 0
         values.forEach { value ->
+            // 仅支持重写了hashCode()方法，且hashCode值不变的类型，避免在用于分表场景时每次sharding结果不一致
+            require(value is String)
             h = 31 * h + value.hashCode()
         }
         return h and shardingCount - 1
