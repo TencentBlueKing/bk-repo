d4f39f978037f18d50bb293c83bbba63745fcf86
kunlongli <kunlongli@tencent.com>
2025-09-25 20:14:40 +0800

    fix: 修复制品加水印时下载制品返回403 #3642

--- src/backend/analyst/biz-analyst/src/main/kotlin/com/tencent/bkrepo/analyst/controller/ScanController.kt
+++ src/backend/analyst/biz-analyst/src/main/kotlin/com/tencent/bkrepo/analyst/controller/ScanController.kt
@@ -41,8 +41,8 @@ import com.tencent.bkrepo.analyst.service.SpdxLicenseService
 import com.tencent.bkrepo.analyst.service.TemporaryScanTokenService
 import com.tencent.bkrepo.common.api.pojo.Response
 import com.tencent.bkrepo.common.security.exception.AuthenticationException
-import com.tencent.bkrepo.common.security.util.SecurityUtils
 import com.tencent.bkrepo.common.service.util.ResponseBuilder
+import com.tencent.bkrepo.repository.constant.SYSTEM_USER
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.RestController
 
@@ -56,7 +56,7 @@ class ScanController @Autowired constructor(
 
     override fun scan(scanRequest: ScanRequest): Response<ScanTask> {
         return ResponseBuilder.success(
-            scanService.scan(scanRequest, ScanTriggerType.ON_NEW_ARTIFACT, SecurityUtils.getUserId())
+            scanService.scan(scanRequest, ScanTriggerType.ON_NEW_ARTIFACT, SYSTEM_USER)
         )
     }
 
