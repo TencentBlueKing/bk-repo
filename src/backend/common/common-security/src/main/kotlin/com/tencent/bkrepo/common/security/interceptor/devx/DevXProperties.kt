3f0163e3941fc35038d7dcc673d007386724e9bf
yaoxuwan <yaoxuwan@tencent.com>
2025-10-21 09:47:24 +0800

    feat: 分享文件的审批阈值支持按项目配置 #3684

--- src/backend/common/common-security/src/main/kotlin/com/tencent/bkrepo/common/security/interceptor/devx/DevXProperties.kt
+++ src/backend/common/common-security/src/main/kotlin/com/tencent/bkrepo/common/security/interceptor/devx/DevXProperties.kt
@@ -36,151 +36,177 @@ import java.time.Duration
  * 云研发配置
  * */
 @ConfigurationProperties("devx")
-data class DevXProperties(
+class DevXProperties {
     /**
      * 是否开启云研发相关配置
      * */
-    var enabled: Boolean = false,
+    var enabled: Boolean = false
+
     /**
      * apigw app code
      * */
-    var appCode: String = "",
+    var appCode: String = ""
+
     /**
      * apigw app secret
      * */
-    var appSecret: String = "",
+    var appSecret: String = ""
+
     /**
      * 查询云研发工作空间的URL
      * */
-    var workspaceUrl: String = "",
+    var workspaceUrl: String = ""
+
     /**
      * 查询cvm workspace的url
      */
-    var cvmWorkspaceUrl: String = "",
+    var cvmWorkspaceUrl: String = ""
+
     /**
      * 查询cvm workspace时使用的用户名
      */
-    var cvmWorkspaceUid: String = "bkrepo",
+    var cvmWorkspaceUid: String = "bkrepo"
+
     /**
      * 查询cvm workspace 页大小
      */
-    var cvmWorkspacePageSize: Int = 500,
+    var cvmWorkspacePageSize: Int = 500
+
     /**
      * 缓存的项目ip过期时间
      */
-    var cacheExpireTime: Duration = Duration.ofMinutes(1L),
+    var cacheExpireTime: Duration = Duration.ofMinutes(1L)
+
     /**
      * 缓存的项目数量
      */
-    var cacheSize: Long = 1000L,
+    var cacheSize: Long = 1000L
+
     /**
      * 配置属于项目的CVM
      * key 为项目ip， value为CVM配置
      */
-    var projectCvmWhiteList: Map<String, Set<String>> = emptyMap(),
+    var projectCvmWhiteList: Map<String, Set<String>> = emptyMap()
+
     /**
      * 配置可以被访问的项目
      * key 为项目id， value为可被访问的项目id
      */
-    var projectWhiteList: Map<String, Set<String>> = emptyMap(),
+    var projectWhiteList: Map<String, Set<String>> = emptyMap()
+
     /**
      * 可以从任意来源访问的用户
      */
-    var userWhiteList: Set<String> = emptySet(),
+    var userWhiteList: Set<String> = emptySet()
+
     /**
      * 访问受限的用户ID前缀
      */
-    var restrictedUserPrefix: Set<String> = emptySet(),
+    var restrictedUserPrefix: Set<String> = emptySet()
+
     /**
      * 访问受限的用户ID后缀
      */
-    var restrictedUserSuffix: Set<String> = emptySet(),
+    var restrictedUserSuffix: Set<String> = emptySet()
+
     /**
      * 请求来源区分header-name
      */
-    var srcHeaderName: String? = null,
+    var srcHeaderName: String? = null
+
     /**
      * 请求来源区分header-value
      */
-    var srcHeaderValues: List<String> = emptyList(),
+    var srcHeaderValues: List<String> = emptyList()
+
     /**
      * devX拦截器优先级，
      * 如果需要取用户信息优先级需要比[com.tencent.bkrepo.common.security.http.core.HttpAuthInterceptor]拦截器低
      */
-    var interceptorOrder: Int = Ordered.LOWEST_PRECEDENCE - 100,
+    var interceptorOrder: Int = Ordered.LOWEST_PRECEDENCE - 100
+
     /**
      * 不应用devX拦截器的接口
      */
-    var excludePatterns: List<String> = emptyList(),
+    var excludePatterns: List<String> = emptyList()
+
     /**
      * 远程制品库集群url
      */
-    var remoteBkRepoUrl: String = "",
+    var remoteBkRepoUrl: String = ""
+
     /**
      * 将host解析到指定ip，不指定时则使用默认dns
      */
-    var remoteBkRepoIp: String = "",
+    var remoteBkRepoIp: String = ""
+
     /**
      * 远程制品库集群平台账号
      */
-    var remoteBkRepoAccessKey: String = "",
+    var remoteBkRepoAccessKey: String = ""
+
     /**
      * 远程制品库集群平台账号密钥
      */
-    var remoteBkRepoSecretKey: String = "",
+    var remoteBkRepoSecretKey: String = ""
+
     /**
      * 使用的远程制品库集群用户身份
      */
-    var remoteBkRepoUser: String = "",
+    var remoteBkRepoUser: String = ""
 
     /**
      * 应用devX拦截器的接口
      */
-    var includePatterns: List<String> = emptyList(),
+    var includePatterns: List<String> = emptyList()
 
     /**
      * 校验devx token接口url
      */
-    var validateTokenUrl: String = "",
+    var validateTokenUrl: String = ""
 
     /**
      * 校验devx token接口的认证token
      */
-    var authToken: String = "",
+    var authToken: String = ""
 
     /**
      * 查询团队云桌面接口url
      */
-    var groupWorkspaceUrl: String = "",
+    var groupWorkspaceUrl: String = ""
 
     /**
      * 查询个人云桌面接口url
      */
-    var personalWorkspaceUrl: String = "",
+    var personalWorkspaceUrl: String = ""
 
     /**
      * 查询云桌面环境使用权限
      */
-    var workspaceEnvUsePermissionUrlFormat: String = "",
+    var workspaceEnvUsePermissionUrlFormat: String = ""
 
     /**
      * 校验devx access token接口url
      */
-    var validateAccessTokenUrl: String = "",
+    var validateAccessTokenUrl: String = ""
 
     /**
      * 蓝盾应用商店应用code
      */
-    var devopsStoreCode: String = "",
+    var devopsStoreCode: String = ""
 
     /**
      * 获取云研发项目管理员
      */
-    var projectManagerUrl: String = "",
+    var projectManagerUrl: String = ""
 
     /**
      * 分享文件大小限制
      */
-    var shareFileSizeLimit: DataSize = DataSize.ofMegabytes(50),
+    var shareFileSizeLimit: DataSize = DataSize.ofMegabytes(50)
 
-)
+    /**
+     * 项目级分享文件大小限制，key 为项目ID
+     */
+    var projectShareFileSizeLimit: Map<String, DataSize> = emptyMap()
+}
