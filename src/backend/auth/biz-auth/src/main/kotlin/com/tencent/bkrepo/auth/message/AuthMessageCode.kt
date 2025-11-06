e8ccf7a76962e2b5a0de0b03f4e14710c2031653
owenlxu <owenlxu@tencent.com>
2025-11-06 15:26:56 +0800

    feat: 优化平台账号限制报错 #3717

--- src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/message/AuthMessageCode.kt
+++ src/backend/auth/biz-auth/src/main/kotlin/com/tencent/bkrepo/auth/message/AuthMessageCode.kt
@@ -75,6 +75,7 @@ enum class AuthMessageCode(private val businessCode: Int, private val key: Strin
     AUTH_ACCOUT_FORAUTH_NOT_PERM(37, "auth.account.forauth.not-perm"),
     AUTH_ENTITY_USER_NOT_EXIST(38, "auth.entity.user.not-exist"),
     AUTH_PROXY_NOT_EXIST(39,"auth.proxy.not-exist"),
+    AUTH_PLATFORM_ONLY(40, "auth.platform.only"),
     ;
 
     override fun getBusinessCode() = businessCode
