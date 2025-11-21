0ee7b04d14e71ece7a32103c99d54c2193744785
yaoxuwan <yaoxuwan@tencent.com>
2025-11-21 10:07:46 +0800

    feat: 支持加密存储并转存 #3757

--- src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/crypt/ArtifactCryptConfiguration.kt
+++ src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/crypt/ArtifactCryptConfiguration.kt
@@ -1,22 +1,15 @@
 package com.tencent.bkrepo.common.artifact.crypt
 
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration(proxyBeanMethods = false)
 class ArtifactCryptConfiguration {
 
-    @Value("artifact.crypt.key")
-    private val key: String = "bkrepo@secret"
-
     @Bean
+    @ConditionalOnProperty(name = ["artifact.crypt.enabled"], havingValue = "true")
     fun cryptFilter(): CryptFilter {
         return CryptFilter()
-    }
-
-    @Bean
-    fun secretKeyProvider(): SecretKeyProvider {
-        return SecretKeyProvider(key)
     }
 }
--- src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/crypt/CryptFilter.kt
+++ src/backend/common/common-artifact/artifact-service/src/main/kotlin/com/tencent/bkrepo/common/artifact/crypt/CryptFilter.kt
@@ -1,8 +1,14 @@
 package com.tencent.bkrepo.common.artifact.crypt
 
+import com.tencent.bkrepo.common.api.constant.MediaTypes
+import com.tencent.bkrepo.common.api.exception.ErrorCodeException
+import com.tencent.bkrepo.common.api.message.CommonMessageCode
 import com.tencent.bkrepo.common.api.util.readJsonString
+import com.tencent.bkrepo.common.api.util.toJsonString
 import com.tencent.bkrepo.common.artifact.constant.HEADER_CRYPT_KEY
 import com.tencent.bkrepo.common.security.util.RsaUtils
+import com.tencent.bkrepo.common.service.util.LocaleMessageUtils
+import com.tencent.bkrepo.common.service.util.ResponseBuilder
 import jakarta.servlet.Filter
 import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletRequest
@@ -18,18 +24,27 @@ class CryptFilter : Filter {
         response: ServletResponse,
         chain: FilterChain
     ) {
-        if (request !is HttpServletRequest) {
+        if (request !is HttpServletRequest || response !is HttpServletResponse) {
             chain.doFilter(request, response)
             return
         }
-        val key = getKey(request)
+        val key = try {
+            getKey(request)
+        } catch (e: ErrorCodeException) {
+            response.status = e.status.value
+            val errorMessage = LocaleMessageUtils.getLocalizedMessage(e.messageCode, e.params)
+            val res = ResponseBuilder.fail(e.messageCode.getCode(), errorMessage)
+            response.contentType = MediaTypes.APPLICATION_JSON
+            response.writer.print(res.toJsonString())
+            return
+        }
         if (key.isNullOrEmpty()) {
             chain.doFilter(request, response)
             return
         }
         if (request.method == HttpMethod.GET.name()) {
             // 下载请求
-            val responseWrapper = HttpDecryptResponseWrapper(key, response as HttpServletResponse)
+            val responseWrapper = HttpDecryptResponseWrapper(key, response)
             val requestWrapper = HttpDownloadRequestWrapper(request)
             chain.doFilter(requestWrapper, responseWrapper)
         } else if (request.method == HttpMethod.PUT.name()) {
@@ -46,10 +61,12 @@ class CryptFilter : Filter {
             data.readJsonString<CryptKey>()
         } catch (e: Exception) {
             logger.warn("Crypt key[$keyHeader] is invalid", e)
-            null
-        } ?: return null
+            throw ErrorCodeException(CommonMessageCode.PARAMETER_INVALID, "crypt key")
+        }
         if (cryptKey.timestamp + cryptKey.expiredSeconds < System.currentTimeMillis()/1000L) {
-            return null
+            logger.info("Crypt key[$keyHeader] expired, timestamp is ${cryptKey.timestamp}, " +
+                    "expired seconds is ${cryptKey.expiredSeconds}")
+            throw ErrorCodeException(CommonMessageCode.RESOURCE_EXPIRED, "crypt key")
         }
         return cryptKey.key
     }
