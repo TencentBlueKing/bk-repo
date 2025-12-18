c5895baf5187264793645d281af57ec965ef666c
lannoy0523 <935275025@qq.com>
2025-12-18 09:42:22 +0800

    feat:前端支持子路径部署 #3848

--- src/frontend/core/devops-repository/src/App.vue
+++ src/frontend/core/devops-repository/src/App.vue
@@ -57,7 +57,7 @@
                 if (window.BK_SUBPATH === '/') {
                     urlProjectId = (location.pathname.match(/^\/[a-zA-Z0-9]+\/([^/]+)/) || [])[1]
                 } else {
-                    urlProjectId = location.pathname.split(window.BK_REPO_SUBPATH)[1].match(/^[a-zA-Z0-9]+\/([^/]+)/)[1]
+                    urlProjectId = location.pathname.split(window.BK_SUBPATH)[1].match(/^[a-zA-Z0-9]+\/([^/]+)/)[1]
                 }
                 const localProjectId = localStorage.getItem('projectId')
                 // 查询路由,取项目名称后半截。
