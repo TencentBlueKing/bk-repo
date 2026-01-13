80e31553d721722c5c4227ddc53bd1c00f1d9ce1
liuliaozhong <liuliaozhong@canway.net>
2026-01-13 10:17:13 +0800

    fix: 查看csv文件中文乱码，且不能上下滑动 #3928
    1.命名规范

--- src/backend/buildSrc/src/main/kotlin/Versions.kt
+++ src/backend/buildSrc/src/main/kotlin/Versions.kt
@@ -67,7 +67,7 @@ object Versions {
     const val Notice = "1.0.0"
     const val Audit = "2.0.1"
     const val JodConverter = "4.4.6"
-    const val ApacheCSV = "1.10.0"
+    const val ApacheCsv = "1.10.0"
     const val ApachePoi = "5.2.3"
     const val Galimatias = "0.2.1"
     const val CommonsNet = "3.9.0"
