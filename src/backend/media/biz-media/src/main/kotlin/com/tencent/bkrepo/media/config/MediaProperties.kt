0b746dfbf4bc1b1b3d2adbd03465686824504243
ruotiantang <ruotiantang@tencent.com>
2026-03-10 19:59:33 +0800

    feat: 新增推流到srs

--- src/backend/media/biz-media/src/main/kotlin/com/tencent/bkrepo/media/config/MediaProperties.kt
+++ src/backend/media/biz-media/src/main/kotlin/com/tencent/bkrepo/media/config/MediaProperties.kt
@@ -12,5 +12,6 @@ class MediaProperties {
     var transcodeConfig: Map<String, TranscodeConfig> = mutableMapOf()
     var repoHost: String = ""
     var storageCredentialsKey: String? = null
+    var enabledLiveProjects: List<String> = mutableListOf()
     var rtcSecret: String = "rtc-stream-pull-secret-2m98cx37yr21"
 }
