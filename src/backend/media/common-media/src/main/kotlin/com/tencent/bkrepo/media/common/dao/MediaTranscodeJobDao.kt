a108df10cd38b0f6478ab91f9899abbe19559efe
ruotiantang <ruotiantang@tencent.com>
2026-03-12 10:18:34 +0800

    feat: 修改转码任务按项目分配

--- src/backend/media/common-media/src/main/kotlin/com/tencent/bkrepo/media/common/dao/MediaTranscodeJobDao.kt
+++ src/backend/media/common-media/src/main/kotlin/com/tencent/bkrepo/media/common/dao/MediaTranscodeJobDao.kt
@@ -9,7 +9,6 @@ import org.bson.types.ObjectId
 import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.FindAndModifyOptions
 import org.springframework.data.mongodb.core.aggregation.Aggregation
-import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Update
 import org.springframework.data.mongodb.core.query.and
