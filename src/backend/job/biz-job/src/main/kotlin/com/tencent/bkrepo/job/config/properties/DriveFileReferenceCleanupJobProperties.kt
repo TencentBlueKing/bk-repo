a9ed76ed746cf4947677e5c28a7bbcc18298554f
kunlongli <kunlongli@tencent.com>
2026-03-26 18:00:59 +0800

    feat: 支持清理DriveFileReference #3999

---
+++ src/backend/job/biz-job/src/main/kotlin/com/tencent/bkrepo/job/config/properties/DriveFileReferenceCleanupJobProperties.kt
@@ -0,0 +1,26 @@
+package com.tencent.bkrepo.job.config.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties("job.drive-file-reference-cleanup")
+class DriveFileReferenceCleanupJobProperties : MongodbJobProperties() {
+    override var cron: String = "0 0 4/6 * * ?"
+    override var sharding: Boolean = true
+
+    /**
+     * 预期drive block node数量
+     */
+    var expectedBlockNodes: Long = 100_000_000
+
+    /**
+     * 布隆过滤器误报率
+     */
+    var fpp: Double = 0.0001
+
+    /**
+     * 忽略的存储凭据，这些存储的缓存将不执行清理
+     */
+    var ignoredStorageCredentialsKeys: Set<String> = emptySet()
+}
