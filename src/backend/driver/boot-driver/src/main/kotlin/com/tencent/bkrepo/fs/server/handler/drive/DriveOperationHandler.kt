3622a8a457966423b15a22b622274a5ba4477130
kunlongli <kunlongli@tencent.com>
2026-04-08 15:17:34 +0800

    feat: 支持限制write请求总数 #3999

--- src/backend/driver/boot-driver/src/main/kotlin/com/tencent/bkrepo/fs/server/handler/drive/DriveOperationHandler.kt
+++ src/backend/driver/boot-driver/src/main/kotlin/com/tencent/bkrepo/fs/server/handler/drive/DriveOperationHandler.kt
@@ -2,8 +2,10 @@ package com.tencent.bkrepo.fs.server.handler.drive
 
 import com.tencent.bkrepo.common.api.constant.HttpStatus
 import com.tencent.bkrepo.common.api.exception.ErrorCodeException
+import com.tencent.bkrepo.common.api.exception.TooManyRequestsException
 import com.tencent.bkrepo.common.api.message.CommonMessageCode
 import com.tencent.bkrepo.common.artifact.exception.ArtifactNotFoundException
+import com.tencent.bkrepo.fs.server.config.properties.drive.DriveProperties
 import com.tencent.bkrepo.fs.server.bodyToArtifactFile
 import com.tencent.bkrepo.fs.server.request.drive.DriveBlockRequest
 import com.tencent.bkrepo.fs.server.request.drive.DriveBlockWriteRequest
@@ -21,6 +23,7 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.buildAndAwait
 import java.time.format.DateTimeFormatter
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Drive 文件操作处理器
@@ -31,6 +34,7 @@ import java.time.format.DateTimeFormatter
 class DriveOperationHandler(
     private val driveFileOperationService: DriveFileOperationService,
     private val driveNodeService: DriveNodeService,
+    private val driveProperties: DriveProperties,
 ) {
 
     /**
@@ -58,29 +62,48 @@ class DriveOperationHandler(
      * 写入的文件块立即可被读取
      */
     suspend fun write(request: ServerRequest): ServerResponse {
-        val user = ReactiveSecurityUtils.getUser()
-        val artifactFile = request.bodyToArtifactFile()
-        val blockRequest = DriveBlockWriteRequest(request)
+        val hasAcquired = tryAcquireWriteQuota()
+        try {
+            val user = ReactiveSecurityUtils.getUser()
+            val artifactFile = request.bodyToArtifactFile()
+            val blockRequest = DriveBlockWriteRequest(request)
 
-        // 检查node是否已经被变更
-        request.headers().firstHeader(HEADER_IF_MATCH)?.let { lastModifiedDate ->
-            val node = driveNodeService.getNodeByIno(blockRequest.projectId, blockRequest.repoName, blockRequest.ino)
-            if (node.lastModifiedDate.format(DateTimeFormatter.ISO_DATE_TIME) != lastModifiedDate) {
-                throw ErrorCodeException(
-                    status = HttpStatus.PRECONDITION_FAILED,
-                    messageCode = CommonMessageCode.PRECONDITION_FAILED,
-                    params = arrayOf(HEADER_IF_MATCH),
-                )
+            // 检查node是否已经被变更
+            request.headers().firstHeader(HEADER_IF_MATCH)?.let { lastModifiedDate ->
+                val node = driveNodeService.getNodeByIno(blockRequest.projectId, blockRequest.repoName, blockRequest.ino)
+                if (node.lastModifiedDate.format(DateTimeFormatter.ISO_DATE_TIME) != lastModifiedDate) {
+                    throw ErrorCodeException(
+                        status = HttpStatus.PRECONDITION_FAILED,
+                        messageCode = CommonMessageCode.PRECONDITION_FAILED,
+                        params = arrayOf(HEADER_IF_MATCH),
+                    )
+                }
+            }
+
+            // 写入数据
+            val blockNode = driveFileOperationService.write(artifactFile, blockRequest, user)
+            return ReactiveResponseBuilder.success(blockNode)
+        } finally {
+            if (hasAcquired) {
+                ongoingWriteRequestCount.decrementAndGet()
             }
         }
+    }
 
-        // 写入数据
-        val blockNode = driveFileOperationService.write(artifactFile, blockRequest, user)
-        return ReactiveResponseBuilder.success(blockNode)
+    private fun tryAcquireWriteQuota(): Boolean {
+        val limit = driveProperties.writeRequestLimit
+        if (limit <= 0) return false
+        val currentCount = ongoingWriteRequestCount.incrementAndGet()
+        if (currentCount > limit) {
+            ongoingWriteRequestCount.decrementAndGet()
+            throw TooManyRequestsException("drive.writeRequestLimit[$limit]")
+        }
+        return true
     }
 
     companion object {
         private const val HEADER_IF_MATCH = "X-BKRepo-If-Match"
         private val logger = LoggerFactory.getLogger(DriveOperationHandler::class.java)
+        private val ongoingWriteRequestCount = AtomicInteger(0)
     }
 }
