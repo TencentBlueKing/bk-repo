e749ed5d87aa9941069a15694dfc445e34761ee8
kunlongli <16629885+cnlkl@users.noreply.github.com>
2026-04-09 19:18:45 +0800

    feat: 支持配置CosClient分块上传下载阈值 #4080

--- src/backend/common/common-metadata/metadata-service/src/main/kotlin/com/tencent/bkrepo/common/metadata/service/repo/impl/InnerCosStorageCredentialsUpdater.kt
+++ src/backend/common/common-metadata/metadata-service/src/main/kotlin/com/tencent/bkrepo/common/metadata/service/repo/impl/InnerCosStorageCredentialsUpdater.kt
@@ -44,6 +44,7 @@ class InnerCosStorageCredentialsUpdater : StorageCredentialsUpdater {
         }
         old.slowLogSpeed = new.slowLogSpeed
         old.slowLogTimeInMillis = new.slowLogTimeInMillis
+        old.multipartThreshold = new.multipartThreshold
         old.download = new.download
         old.modId = new.modId
         old.cmdId = new.cmdId
